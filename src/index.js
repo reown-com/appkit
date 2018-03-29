544a17403ae7297894a1ef416b0f245b7274e3ef
Arron <arronhunt.mac@gmail.com>
2018-03-29 10:22:42 -0700

    Changed file structure

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var modal = document.querySelector('.modal');
+var trigger = document.querySelector('.trigger');
+var closeButton = document.querySelector('.close-button');
+
+function toggleModal() {
+  modal.classList.toggle('show-modal');
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
