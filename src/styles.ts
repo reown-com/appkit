8e4612a4db38a253daf6050f2273d3517cd2bc69
Pedro Gomes <pedrogomes94@gmail.com>
2019-04-05 17:31:34 +0200

    add styles.ts

---
+++ src/styles.ts
@@ -0,0 +1,59 @@
+export const colors = {
+  white: "255, 255, 255",
+  black: "0, 0, 0",
+  dark: "12, 12, 13",
+  grey: "169, 169, 188",
+  darkGrey: "113, 119, 138",
+  lightGrey: "212, 212, 212",
+  blue: "101, 127, 230",
+  lightBlue: "64, 153, 255",
+  yellow: "250, 188, 45",
+  orange: "246, 133, 27",
+  green: "84, 209, 146",
+  pink: "255, 51, 102",
+  red: "214, 75, 71",
+  purple: "110, 107, 233"
+};
+
+export const fonts = {
+  size: {
+    tiny: "10px",
+    small: "14px",
+    medium: "16px",
+    large: "18px",
+    h1: "60px",
+    h2: "50px",
+    h3: "40px",
+    h4: "32px",
+    h5: "24px",
+    h6: "20px"
+  },
+  weight: {
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800
+  },
+  family: {
+    OpenSans: '"Open Sans", sans-serif'
+  }
+};
+
+export const transitions = {
+  short: "all 0.1s ease-in-out",
+  base: "all 0.2s ease-in-out",
+  long: "all 0.3s ease-in-out",
+  button: "all 0.15s ease-in-out"
+};
+
+export const shadows = {
+  soft:
+    "0 4px 6px 0 rgba(50, 50, 93, 0.11), 0 1px 3px 0 rgba(0, 0, 0, 0.08), inset 0 0 1px 0 rgba(0, 0, 0, 0.06)",
+  medium:
+    "0 3px 6px 0 rgba(0, 0, 0, 0.06), 0 0 1px 0 rgba(50, 50, 93, 0.02), 0 5px 10px 0 rgba(59, 59, 92, 0.08)",
+  big:
+    "0 15px 35px 0 rgba(50, 50, 93, 0.06), 0 5px 15px 0 rgba(50, 50, 93, 0.15)",
+  hover:
+    "0 7px 14px 0 rgba(50, 50, 93, 0.1), 0 3px 6px 0 rgba(0, 0, 0, 0.08), inset 0 0 1px 0 rgba(0, 0, 0, 0.06)"
+};
