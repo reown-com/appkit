bf5a112706c16f3d7d503f60414a4d43c9c44a33
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-12 16:20:03 +0200

    replace walletconnect banner

--- example/src/components/Banner.tsx
+++ example/src/components/Banner.tsx
@@ -1,25 +1,33 @@
-import * as React from "react";
-import styled from "styled-components";
-import banner from "../assets/walletconnect-banner.png";
+import * as React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/web3connect-logo.png'
+import { fonts, colors } from '../styles'
 
 const SBannerWrapper = styled.div`
   display: flex;
   align-items: center;
   position: relative;
-`;
+  & span {
+    color: rgb(${colors.lightBlue});
+    font-weight: ${fonts.weight.bold};
+    font-size: ${fonts.size.h5};
+    margin-left: 12px;
+  }
+`
 
 const SBanner = styled.div`
-  width: 275px;
+  width: 45px;
   height: 45px;
-  background: url(${banner}) no-repeat;
+  background: url(${logo}) no-repeat;
   background-size: cover;
   background-position: center;
-`;
+`
 
 const Banner = () => (
   <SBannerWrapper>
     <SBanner />
+    <span>{`Web3Connect`}</span>
   </SBannerWrapper>
-);
+)
 
-export default Banner;
+export default Banner
