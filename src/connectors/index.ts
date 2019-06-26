76b57bb43319012736109d0b4bfe1abe5eb51204
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-26 15:06:17 +0200

    add connectors

---
+++ src/connectors/index.ts
@@ -0,0 +1,11 @@
+import ConnectToWalletConnect from "./walletconnect";
+import ConnectToPortis from "./portis";
+import ConnectToInjected from "./injected";
+import ConnectToFortmatic from "./fortmatic";
+
+export default {
+  ConnectToWalletConnect,
+  ConnectToPortis,
+  ConnectToInjected,
+  ConnectToFortmatic
+};
