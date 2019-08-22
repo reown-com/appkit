68b014a0312d863b925a661b573bc775a7cb0515
David Sato <david.sato64@gmail.com>
2019-08-22 10:12:36 +0200

    create an off() eventManager event for unsubscribing
    1. useful if different web3connect options are available
    2. unsubscribable i.e in react hooks

--- src/core/events.ts
+++ src/core/events.ts
@@ -13,6 +13,16 @@ class EventManager {
     this._eventCallbacks.push(eventCallback);
   }
 
+  public off(event?: string) {
+    if (event) {
+      this._eventCallbacks.filter(
+        (eventCallback: IEventCallback) => eventCallback.event !== event
+      );
+    } else {
+      this._eventCallbacks = [];
+    }
+  }
+
   public trigger(event: string, result?: any): void {
     let eventCallbacks: IEventCallback[] = this._eventCallbacks.filter(
       (eventCallback: IEventCallback) => eventCallback.event === event
