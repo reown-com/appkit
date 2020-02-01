c1d8ed9a298b1e1624d39fe3eece16d3d9f25725
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-01 12:22:56 +0100

    fix caching provider on failed connection

--- src/core/controllers/providers.ts
+++ src/core/controllers/providers.ts
@@ -150,15 +150,15 @@ class ProviderController {
     id: string,
     connector: (providerPackage: any, opts: any) => Promise<any>
   ) => {
-    if (this.shouldCacheProvider) {
-      this.setCachedProvider(id);
-    }
     try {
       const providerPackage = this.getProviderOption(id, "package");
       const providerOptions = this.getProviderOption(id, "options");
       const opts = { network: this.network || undefined, ...providerOptions };
       const provider = await connector(providerPackage, opts);
       this.eventController.trigger(CONNECT_EVENT, provider);
+      if (this.shouldCacheProvider) {
+        this.setCachedProvider(id);
+      }
     } catch (error) {
       this.eventController.trigger(ERROR_EVENT);
     }
