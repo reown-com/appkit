abd673a9ed3a54280337bdee82e8484378899bbc
getUnrekt <75529443+getunrekt@users.noreply.github.com>
2022-03-22 10:03:09 +0800

    add cronos and harmony chain

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -85,6 +85,12 @@ export const CHAIN_DATA_LIST: ChainDataList = {
     network: "lukso-l15-testnet",
     networkId: 23
   },
+  25: {
+    chainId: 25,
+    chain: "CRO",
+    network: "cronos",
+    networkId: 25
+  },
   30: {
     chainId: 30,
     chain: "RSK",
@@ -318,5 +324,11 @@ export const CHAIN_DATA_LIST: ChainDataList = {
     chain: "ETH",
     network: "arbitrum-rinkeby",
     networkId: 421611
+  },
+  1666600000: {
+    chainId: 1666600000,
+    chain: "ONE",
+    network: "harmony-shard1",
+    networkId: 1666600000
   }
 };
