f27ee165f4117f518302d5455b539ddcd3350fad
Ilja <idaderko@gmail.com>
2022-08-16 13:58:50 +0300

    remove unused css

--- packages/core/src/components/w3m-modal-container/styles.ts
+++ packages/core/src/components/w3m-modal-container/styles.ts
@@ -33,46 +33,6 @@ export default css`
     max-height: 90vh;
   }
 
-  .w3m-modal-media, .w3m-modal-highlight, #w3m-transparent-noise {
-    inset: 0;
-    position: absolute;
-    display: block;
-    pointer-events: none;
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    transform: translate3d(-5px, -5px, 0);
-  }
-
-  .w3m-modal-media {
-    backface-visibility: none;
-    --gradient-color-1: #cad8f2;
-    --gradient-color-2: #be3620;
-    --gradient-color-3: #a6208c;
-    --gradient-color-4: #06968f;
-  }
-
-  #w3m-transparent-noise {
-    mix-blend-mode: multiply;
-    opacity: 0.35;
-  }
-
-
-  .w3m-modal-toolbar {
-    height: 28px;
-    display: flex;
-    position: relative;
-    margin: 5px 15px 5px 5px;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .w3m-modal-toolbar svg, .w3m-modal-toolbar img {
-    height: 28px;
-    object-position: left center;
-    object-fit: contain;
-  } 
-
   .w3m-modal-content {
     width: 400px;
     height: 80vh;
@@ -88,10 +48,6 @@ export default css`
       background-color: ${contentBgDark}
       box-shadow: 0 0 0 1px ${contentShadowDark};
     }
-
-    .w3m-modal-highlight {
-      border: 1px solid ${contentShadowDark};
-    }
   }
 
   @media (prefers-color-scheme: light) {
@@ -99,10 +55,6 @@ export default css`
       color: ${contentColorLight};
       background-color: ${contentBgLight}
       box-shadow: 0 0 0 1px ${contentShadowLight};
-    }
-
-    .w3m-modal-highlight {
-      border: 1px solid ${contentShadowLight};
     }
   }
 `
