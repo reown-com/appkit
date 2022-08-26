68c7da0040ceb3bd93829a4fc384aebefeaa6e78
Celine Sarafa <celinesarafa@gmail.com>
2022-08-26 05:12:22 +0300

    Add logic to detect user preferences

--- packages/ui/src/utils/ThemedElement.ts
+++ packages/ui/src/utils/ThemedElement.ts
@@ -3,41 +3,45 @@ import { LitElement, html } from 'lit'
 import { state } from 'lit/decorators.js'
 import { color } from './Theme'
 
+type ThemeType = typeof ConfigCtrl['state']['theme']
+
 export default class ThemedElement extends LitElement {
   private readonly configUnsub?: () => void = undefined
-  @state() public configured = false
+  @state() public theme = 'light'
 
   protected dynamicStyles() {
     const { foreground, background, overlay } = color()
 
-    return html` <style>
-      button {
-        color: ${foreground.inverse};
-        background-color: ${foreground.accent};
-        box-shadow: inset 0 0 0 1px ${overlay.thin};
-      }
+    return html`
+      <style>
+        button {
+          color: ${foreground.inverse};
+          background-color: ${foreground.accent};
+          box-shadow: inset 0 0 0 1px ${overlay.thin};
+        }
 
-      button::before {
-        background-color: ${overlay.thin};
-      }
+        button::before {
+          background-color: ${overlay.thin};
+        }
 
-      .w3m-button-loading:disabled {
-        background-color: ${background.accent};
-      }
+        .w3m-button-loading:disabled {
+          background-color: ${background.accent};
+        }
 
-      button:disabled {
-        background-color: ${background[3]};
-        color: ${foreground[3]};
-      }
+        button:disabled {
+          background-color: ${background[3]};
+          color: ${foreground[3]};
+        }
 
-      svg path {
-        fill: ${foreground.inverse};
-      }
+        svg path {
+          fill: ${foreground.inverse};
+        }
 
-      button:disabled svg path {
-        fill: ${foreground[3]};
-      }
-    </style>`
+        button:disabled svg path {
+          fill: ${foreground[3]};
+        }
+      </style>
+    `
   }
 
   public disconnectedCallback() {
@@ -47,8 +51,20 @@ export default class ThemedElement extends LitElement {
   // -- lifecycle ---------------------------------------------------- //
   public constructor() {
     super()
+
     this.configUnsub = ConfigCtrl.subscribe(configState => {
-      this.configured = configState.configured
+      this.theme = configState.theme ?? 'dark'
     })
+
+    if (typeof window !== 'undefined') {
+      const prefersDarkModePreference = window.matchMedia('(prefers-color-scheme: dark)')
+      const initialTheme: ThemeType = prefersDarkModePreference.matches ? 'dark' : 'light'
+      ConfigCtrl.setConfig({ theme: initialTheme, projectId: ConfigCtrl.state.projectId })
+
+      prefersDarkModePreference.addEventListener('change', ({ matches: prefersDarkMode }) => {
+        const theme: ThemeType = prefersDarkMode ? 'dark' : 'light'
+        ConfigCtrl.setConfig({ theme, projectId: ConfigCtrl.state.projectId })
+      })
+    }
   }
 }
