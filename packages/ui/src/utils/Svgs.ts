e27e5f2ee0b47d09a7275ad3adf462f9fb71733a
Ilja <idaderko@gmail.com>
2022-08-30 15:35:31 +0300

    Add header back button

--- packages/ui/src/utils/Svgs.ts
+++ packages/ui/src/utils/Svgs.ts
@@ -180,3 +180,14 @@ export const COPY_ICON = svg`
     />
   </svg>
 `
+
+export const BACK_ICON = svg`
+  <svg width="10" height="18" viewBox="0 0 10 18">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8.735.179a.75.75 0 0 1 .087 1.057L2.92 8.192a1.25 1.25 0 0 0 0 1.617l5.902 6.956a.75.75 0 1 1-1.144.97L1.776 10.78a2.75 2.75 0 0 1 0-3.559L7.678.265A.75.75 0 0 1 8.735.18Z"
+      fill="#fff"
+    />
+  </svg>
+`
