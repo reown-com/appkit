babc1dc71630eba38336bfc10da2e02fcc8d928e
Ilja <idaderko@gmail.com>
2022-09-06 12:21:15 +0300

    Fix state update issue

--- examples/react/src/pages/index.tsx
+++ examples/react/src/pages/index.tsx
@@ -3,5 +3,5 @@ import { AccountButton, ConnectButton, useAccount } from '@web3modal/react'
 export default function HomePage() {
   const { chainId, connected, address } = useAccount()
 
-  return connected ? <AccountButton /> : <ConnectButton />
+  return <div>{connected ? <AccountButton /> : <ConnectButton />}</div>
 }
