cedb25eadeddb09cced440c7719a3ac3a4e9007f
Ilja <idaderko@gmail.com>
2022-09-11 17:33:35 +0300

    save

--- packages/ui/src/components/w3m-modal/index.ts
+++ packages/ui/src/components/w3m-modal/index.ts
@@ -67,9 +67,10 @@ export class W3mModal extends ThemedElement {
       ...defaultWallets.map(async url => preloadImage(url))
     ])
     this.open = true
-    animate(this.overlayEl, { opacity: [0, 1] }, { duration: 0.2 })
+    animate(this.overlayEl, { opacity: [0, 1] }, { duration: 0.2, delay: 0.1 })
     animate(this.containerEl, isMobileAnimation() ? { y: [15, 0] } : { scale: [0.98, 1] }, {
-      duration: 0.2
+      duration: 0.2,
+      delay: 0.1
     })
     document.addEventListener('keydown', this.onKeyDown)
     this.firstOpen = false
