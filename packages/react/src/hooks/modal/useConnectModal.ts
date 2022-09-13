83d5f23ce136871f869a0f59f6cb7db04fa0031c
Celine Sarafa <celinesarafa@gmail.com>
2022-09-13 10:33:33 +0300

    Add open state

--- packages/react/src/hooks/modal/useConnectModal.ts
+++ packages/react/src/hooks/modal/useConnectModal.ts
@@ -2,6 +2,7 @@ import { ConnectModalCtrl } from '@web3modal/core'
 
 export function useConnectModal() {
   return {
+    isOpen: ConnectModalCtrl.state.open,
     open: ConnectModalCtrl.openModal,
     close: ConnectModalCtrl.closeModal
   }
