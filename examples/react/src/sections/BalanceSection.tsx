95f5281f985463df3da139e7fa30245918feefb3
Celine Sarafa <celinesarafa@gmail.com>
2022-09-14 17:32:39 +0300

    Fix typo

--- examples/react/src/sections/BalanceSection.tsx
+++ examples/react/src/sections/BalanceSection.tsx
@@ -4,14 +4,14 @@ export default function BalanceSection() {
   const { balance, refetch, isLoading } = useBalance()
   const account = useAccount()
 
-  function onSignMessage() {
+  function onFetchBalance() {
     refetch({ addressOrName: account.address, chainId: account.chainId, formatUnits: 'wei' })
   }
 
   return (
     <section>
       <h1>Balance</h1>
-      <button type="button" disabled={isLoading} onClick={onSignMessage}>
+      <button type="button" disabled={isLoading} onClick={onFetchBalance}>
         Get Balance
       </button>
       {balance ? <p>{balance}</p> : null}
