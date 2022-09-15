0504bfa2b5d492eca6389f0eaeefb8e04ce058ad
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-09-15 12:04:11 +0200

    put back to normal order

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,3 @@
-export * from './src/components/w3m-modal'
 export * from './src/partials/w3m-account-button'
 export * from './src/partials/w3m-connect-button'
+export * from './src/components/w3m-modal'
