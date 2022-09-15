64da477aadda4973e16fcf2c722191d0055d1f83
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2022-09-15 12:01:40 +0200

    rm unfinished account-view content

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,4 +1,3 @@
-export * from './src/components/w3m-account-modal'
 export * from './src/components/w3m-modal'
 export * from './src/partials/w3m-account-button'
 export * from './src/partials/w3m-connect-button'
