3848f92f6ca6b85fc7b999bf3994edd6f1825aea
Celine Sarafa <celinesarafa@gmail.com>
2022-09-17 13:49:24 +0300

    Make initialOpts optional for useContract

--- packages/react/src/hooks/contract/useContract.ts
+++ packages/react/src/hooks/contract/useContract.ts
@@ -2,7 +2,7 @@ import { ClientCtrl } from '@web3modal/core'
 import type { GetContractOpts } from '@web3modal/ethereum'
 import { useSyncHookBuilder } from '../../utils/useSyncHookBuilder'
 
-export function useContract(initialOpts: GetContractOpts) {
+export function useContract(initialOpts?: GetContractOpts) {
   const { data: contract, ...fetchResult } = useSyncHookBuilder(
     (opts: GetContractOpts) => ClientCtrl.ethereum().getContract(opts),
     initialOpts
