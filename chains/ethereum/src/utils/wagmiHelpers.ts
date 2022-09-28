b88c6b07501c14dfea54947aa9ea0a279f2a2293
Ilja <idaderko@gmail.com>
2022-09-28 10:10:54 +0300

    update deps

--- chains/ethereum/src/utils/wagmiHelpers.ts
+++ chains/ethereum/src/utils/wagmiHelpers.ts
@@ -1,6 +1,5 @@
 import type * as WagmiTypes from '@wagmi/core'
 import * as WagmiCore from '@wagmi/core'
-import type { State } from '@wagmi/core/dist/declarations/src/client'
 import { publicProvider } from '@wagmi/core/providers/public'
 import { Buffer } from 'buffer'
 import type { EthereumOptions } from '../../types/apiTypes'
@@ -14,42 +13,6 @@ let client = undefined as WagmiTypes.Client<any, any> | undefined
 
 export function getClient() {
   return client
-}
-
-function onConnectorChange(event: WagmiTypes.ConnectorData) {
-  if (event.chain) {
-    /* TODO: Set this in NetworkCtrl*/
-  }
-}
-
-function onConnectorMessage(event: { type: string; data?: unknown }) {
-  // TODO: Set this in ConnectorCtrl
-}
-
-function onConnectorError(event: Error) {
-  // TODO: Handle this in ConnectorCtrl
-}
-
-function onClientConnected() {
-  const connector = getClient()?.connector
-  const provider = getClient()?.provider
-  if (connector && provider) {
-    connector.on('change', onConnectorChange)
-    connector.on('message', onConnectorMessage)
-    connector.on('error', onConnectorError)
-  }
-}
-
-function onClientDisconnected() {
-  getClient()?.connector?.removeAllListeners()
-}
-
-function onClientChange(state: State, prevState: State) {
-  if (state.status !== prevState.status) {
-    const { status } = state
-    if (status === 'connected') onClientConnected()
-    if (status === 'disconnected') onClientDisconnected()
-  }
 }
 
 export function initializeClient(options: EthereumOptions) {
@@ -70,5 +33,4 @@ export function initializeClient(options: EthereumOptions) {
   })
 
   client = wagmiClient
-  getClient()?.subscribe(onClientChange)
 }
