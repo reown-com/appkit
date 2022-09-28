c18aed0b8d07513b257f97739e0ee176a05f0bcf
theo-ahnlabio <107841222+theo-ahnlabio@users.noreply.github.com>
2022-09-28 17:04:51 +0900

    add abcwallet
    https://myabcwallet.io/en/

--- src/providers/injected/index.ts
+++ src/providers/injected/index.ts
@@ -62,6 +62,8 @@ import TokenPocketLogo from "../logos/tokenpocket.svg";
 import CoreLogo from "../logos/core.svg";
 // @ts-ignore
 import GameStopLogo from "../logos/gamestopwallet.svg";
+// @ts-ignore
+import ABCWallet from "../logos/abcwallet.svg";
 
 export const FALLBACK: IProviderInfo = {
   id: "injected",
@@ -309,4 +311,12 @@ export const GAMESTOP: IProviderInfo = {
   logo: GameStopLogo,
   type: "injected",
   check: "isGamestop"
+};
+
+export const ABCWALLET: IProviderInfo = {
+  id: "injected",
+  name: "ABC Wallet",
+  logo: GameStopLogo,
+  type: "injected",
+  check: "isABC"
 };
