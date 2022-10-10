24ff130934f24e649077e81e7feaa1dca8ddabbb
Ilja <idaderko@gmail.com>
2022-10-10 17:27:09 -0500

    vanilla atempt 1

--- chains/ethereum/index.ts
+++ chains/ethereum/index.ts
@@ -2,3 +2,21 @@ export type { Chain, ChainProviderFn, Connector, Provider, WebSocketProvider } f
 export { Web3ModalEthereum } from './src/api'
 export { chains, providers } from './src/utils/wagmiTools'
 export * from './types/apiTypes'
+
+// -- vanilla ----------------------------------------------- //
+import { Web3ModalEthereum as Web3ModalEthereumClient } from './src/api'
+import { chains, providers } from './src/utils/wagmiTools'
+
+const Web3ModalEthereum = {
+  ...Web3ModalEthereumClient,
+  chains,
+  providers
+}
+
+if (typeof window !== 'undefined') window.Web3ModalEth = Web3ModalEthereum
+
+declare global {
+  interface Window {
+    Web3ModalEth: typeof Web3ModalEthereum
+  }
+}
