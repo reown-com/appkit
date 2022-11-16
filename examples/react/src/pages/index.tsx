2af86578308c1266f990839a173f97d873f1a73a
Ilja <idaderko@gmail.com>
2022-11-16 10:51:40 +0200

    update deps

--- examples/react/src/pages/index.tsx
+++ examples/react/src/pages/index.tsx
@@ -2,14 +2,14 @@ import { useWeb3Modal, Web3Button } from '@web3modal/react'
 import { useAccount } from 'wagmi'
 
 export default function HomePage() {
-  const { isDisconnected } = useAccount()
+  const { isConnected } = useAccount()
   const { open } = useWeb3Modal()
 
   return (
     <>
       <Web3Button />
       <p>--- or ---</p>
-      {isDisconnected && <button onClick={() => open()}>Custom Button</button>}
+      {!isConnected && <button onClick={() => open()}>Custom Button</button>}
     </>
   )
 }
