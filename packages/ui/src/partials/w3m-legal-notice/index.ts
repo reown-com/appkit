9b5c52a37ecb1dd78a2a45ab71091310abf6584a
Ilja <idaderko@gmail.com>
2022-12-23 12:35:51 +0200

    Add terms conditions

--- packages/ui/src/partials/w3m-legal-notice/index.ts
+++ packages/ui/src/partials/w3m-legal-notice/index.ts
@@ -19,7 +19,20 @@ export class W3mLegalNotice extends LitElement {
 
     return html`
       <div>
-        <p>hello legal</p>
+        <w3m-text variant="small-normal" color="secondary">
+          By connecting your wallet, you agree to our <br />
+          ${termsOfServiceUrl
+            ? html`<a href=${termsOfServiceUrl} target="_blank" rel="noopener noreferrer">
+                Terms of Service
+              </a>`
+            : null}
+          ${termsOfServiceUrl && privacyPolicyUrl ? 'and' : null}
+          ${privacyPolicyUrl
+            ? html`<a href=${privacyPolicyUrl} target="_blank" rel="noopener noreferrer">
+                Privacy Policy
+              </a>`
+            : null}
+        </w3m-text>
       </div>
     `
   }
