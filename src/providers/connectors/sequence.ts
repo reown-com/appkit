8590bc6dc6e47fe4e775a505a06f1b25f8f6db46
Samuele Agostinelli <batonenbois@gmail.com>
2023-01-05 02:40:32 -0500

    Fixes to Sequence connector integration (#726)
    * connector update for sequence wallet
    
    * await sequence wallet init
    
    * minor fix
    
    * additional sequence connector options
    
    * added more wallet options
    
    * new params fallback
    
    Co-authored-by: Peter Kieltyka <peter.kieltyka@gmail.com>

--- src/providers/connectors/sequence.ts
+++ src/providers/connectors/sequence.ts
@@ -1,8 +1,13 @@
 import { IAbstractConnectorOptions } from "../../helpers";
 
 export interface ISequenceConnectorOptions extends IAbstractConnectorOptions {
-  appName: string;
-  defaultNetwork?: string;
+  appName: string
+  defaultNetwork?: string
+  networkRpcUrl?: string
+  getSequenceWallet?: (sequenceWallet: any) => void
+  onConnect?: (connectDetails: any) => void
+  authorize?: boolean
+  keepWalletOpened?: boolean
 }
 
 const ConnectToSequence = async (
@@ -20,21 +25,41 @@ const ConnectToSequence = async (
     }
   }
 
-  const wallet = new sequence.Wallet(opts?.defaultNetwork || 'mainnet');
+  let wallet;
+
+  try {
+    wallet = sequence.getWallet();
+  } catch (err) {
+    let walletConfig = {};
+    if (opts?.networkRpcUrl) {
+      walletConfig = { networkRpcUrl: opts.networkRpcUrl }
+    }
+
+    wallet = await sequence.initWallet(opts?.defaultNetwork || 'mainnet', walletConfig);
+  }
 
   if (!wallet.isConnected()) {
     const connectDetails = await wallet.connect({
       app: opts?.appName,
-      authorize: true
+      authorize: opts?.authorize || false,
+      keepWalletOpeneed: opts?.keepWalletOpened || false
     });
-
+    
     if (!connectDetails.connected) {
       throw new Error("Failed to connect");
     }
+    
+    if (opts?.onConnect) {
+      opts.onConnect(connectDetails)
+    }
   }
-
+  
   provider = wallet.getProvider();
   provider.sequence = wallet;
+
+  if (opts?.getSequenceWallet) {
+    opts.getSequenceWallet(wallet)
+  }
 
   return provider;
 };
