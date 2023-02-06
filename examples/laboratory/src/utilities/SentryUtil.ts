1f131d70f0b41e254aa75cae2058b2730cf1d1ba
Derek <alexanderderekrein@gmail.com>
2023-02-05 22:14:36 -0300

    fix: lint

--- examples/laboratory/src/utilities/SentryUtil.ts
+++ examples/laboratory/src/utilities/SentryUtil.ts
@@ -7,10 +7,6 @@ export function bootstrap() {
     Sentry.init({
       dsn,
       integrations: [new BrowserTracing()],
-
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
       tracesSampleRate: 1.0
     })
   }
