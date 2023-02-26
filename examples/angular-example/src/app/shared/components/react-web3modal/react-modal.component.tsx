48aaab435616c1e2f7fdee13d9dd5d8b296369b5
bws9000 <snyder.burt@gmail.com>
2023-02-26 08:30:24 -0500

    strip angular/ add html package

--- examples/angular-example/src/app/shared/components/react-web3modal/react-modal.component.tsx
+++ examples/angular-example/src/app/shared/components/react-web3modal/react-modal.component.tsx
@@ -1,17 +1,42 @@
-import { Web3Modal } from '@web3modal/standalone'
-import React from 'react'
-// eslint-disable-next-line capitalized-comments
-// import SignClient from '@walletconnect/sign-client'
+import { arbitrum, mainnet, polygon } from "@wagmi/chains";
+import { configureChains, createClient } from "@wagmi/core";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/html";
+import React from "react";
 
 // eslint-disable-next-line func-style
 export const ReactModalComponent: React.FC = () => {
-  // eslint-disable-next-line capitalized-comments
-  // const [signClient, setSignClient] = useState<SignClient | undefined>(undefined)
-  const web3Modal = new Web3Modal(Object({ projectId: '5eec3e3ff91ddea25b76615da1e8b668' }))
 
-  // eslint-disable-next-line func-style
+  const chains = [arbitrum, mainnet, polygon];
+
+  // Wagmi Core Client
+  const { provider } = configureChains(chains, [
+    walletConnectProvider({ projectId: "5eec3e3ff91ddea25b76615da1e8b668" }),
+  ]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: modalConnectors({
+      projectId: "5eec3e3ff91ddea25b76615da1e8b668",
+      version: "2", // or "2"
+      appName: "web3Modal",
+      chains,
+    }),
+    provider,
+  });
+
+  // Web3Modal and Ethereum Client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
+  const web3modal = new Web3Modal(
+    { projectId: "5eec3e3ff91ddea25b76615da1e8b668" },
+    ethereumClient
+  );
+
   const open = () => {
-    web3Modal.openModal(undefined)
+    web3modal.openModal(undefined)
   }
 
   return (
@@ -21,4 +46,5 @@ export const ReactModalComponent: React.FC = () => {
       </div>
     </div>
   )
+
 }
