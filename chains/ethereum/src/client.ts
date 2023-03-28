10737c0371c158cd394c0affcd1e9da196a0e9cd
Ilja <idaderko@gmail.com>
2023-03-28 11:58:59 +0300

    Remove log

--- chains/ethereum/src/client.ts
+++ chains/ethereum/src/client.ts
@@ -116,7 +116,6 @@ export class EthereumClient {
   public async reconnectWalletConnect() {
     const { connector, isV2 } = this.getWalletConnectConnectors()
     if (isV2) {
-      console.log('halloooo')
       const provider = await connector.getProvider()
       provider.signer.client.core.relayer.transportOpen()
     }
