2e276c67eaa62925a59144ef9a1df635cf28482c
Ilja <idaderko@gmail.com>
2023-04-03 16:03:15 +0300

    Update search label to be more general

--- packages/ui/src/components/w3m-search-input/index.ts
+++ packages/ui/src/components/w3m-search-input/index.ts
@@ -1,4 +1,3 @@
-import { CoreUtil } from '@web3modal/core'
 import { html, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { SvgUtil } from '../../utils/SvgUtil'
@@ -13,7 +12,7 @@ export class W3mSearchInput extends LitElement {
 
   // -- render ------------------------------------------------------- //
   protected render() {
-    const placeholder = CoreUtil.isMobile() ? 'Search mobile wallets' : 'Search desktop wallets'
+    const placeholder = 'Search wallets'
 
     return html`
       <input type="text" @input=${this.onChange} placeholder=${placeholder} />
