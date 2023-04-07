7c48c80c8bbf142304738c83411afa44347b1729
Ilja <idaderko@gmail.com>
2023-04-07 15:34:59 +0300

    fix

--- packages/ui/src/views/w3m-mobile-connecting-view/index.ts
+++ packages/ui/src/views/w3m-mobile-connecting-view/index.ts
@@ -89,7 +89,10 @@ export class W3mMobileConnectingView extends LitElement {
 
         <w3m-platform-selection .isWeb=${isWeb}>
           <div>
-            <w3m-button .onClick=${this.openMobileApp.bind(this)} .iconRight=${SvgUtil.RETRY_ICON}>
+            <w3m-button
+              .onClick=${() => this.openMobileApp(false)}
+              .iconRight=${SvgUtil.RETRY_ICON}
+            >
               Retry
             </w3m-button>
 
