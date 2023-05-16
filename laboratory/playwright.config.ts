a2eac41bafc0641ca6170f2cbafb5643c259a61b
Chris Smith <chris@walletconnect.com>
2023-05-16 16:16:59 -0400

    chore: initial tests

--- laboratory/playwright.config.ts
+++ laboratory/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -24,28 +24,28 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      use: { ...devices['Desktop Safari'] }
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+  webServer: {
+    command: 'yarn dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI
+  }
+})
---
+++ laboratory/tests/laboratory.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test'
+
+test('has title', async ({ page }) => {
+  await page.goto('./')
+  await expect(page.locator('h1')).toHaveText(/Web3Modal Lab/)
+})
+
+test('can navigate to ManagedReact playground', async ({ page }) => {
+  await page.goto('./')
+  await page.getByText('Go to playground').first().click()
+  expect(page.url()).toBe('http://127.0.0.1:3000/ManagedReact')
+  await expect(page.locator('h1')).toHaveText(/Web3Modal Lab/)
+})
+
+test('can open modal with Connect Wallet', async ({ page }) => {
+  await page.goto('./ManagedReact')
+  await expect(page.getByText('Connect your wallet')).not.toBeVisible()
+  await page.getByText('Connect Wallet').click({ force: true })
+  await expect(page.getByText('Connect your wallet')).toBeVisible()
+})
+
+test('can click View All', async ({ page }) => {
+  await page.goto('./ManagedReact')
+  await expect(page.getByText('Connect your wallet')).not.toBeVisible()
+  await page.getByText('Connect Wallet').click({ force: true })
+  await expect(page.getByText('Connect your wallet')).toBeVisible()
+  await page.getByText('View All').click()
+  await expect(page.locator('[placeholder="Search wallets"]')).toBeVisible()
+})
