c83cce991cac2404676f172c92aef6e8ccd5c07e
Chris Smith <chris@walletconnect.com>
2023-05-30 11:32:21 -0400

    chore: fmt

--- laboratory/tests/openModal.ts
+++ laboratory/tests/openModal.ts
@@ -1,4 +1,4 @@
-import type { Page} from '@playwright/test';
+import type { Page } from '@playwright/test'
 import { expect } from '@playwright/test'
 
 export async function openModal(page: Page) {
