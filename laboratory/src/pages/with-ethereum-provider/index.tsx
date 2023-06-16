e338f8694396b0d5289d667582ad4eac6fc56d27
Sven Voskamp <fr.sven.fr@gmail.com>
2023-06-16 12:40:45 +0200

    [:feat] listen to disconnect events

--- laboratory/src/pages/with-ethereum-provider/index.tsx
+++ laboratory/src/pages/with-ethereum-provider/index.tsx
@@ -56,6 +56,14 @@ export default function WithEthereumProvider() {
   }
 
   useEffect(() => {
+    if (providerClient) {
+      providerClient.on('disconnect', () => {
+        setSession(false)
+      })
+    }
+  }, [providerClient])
+
+  useEffect(() => {
     onInitializeProviderClient()
   }, [])
 
