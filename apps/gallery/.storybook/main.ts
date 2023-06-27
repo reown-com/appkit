9be4a5e4edc19b91c181b8fc3dac3a45481d2814
Ilja <idaderko@gmail.com>
2023-06-27 09:16:54 +0300

    Add storybook dependency

--- apps/gallery/.storybook/main.ts
+++ apps/gallery/.storybook/main.ts
@@ -3,7 +3,6 @@ const config = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
-    builder: '@storybook/builder-vite',
     disableTelemetry: true
   },
   framework: {
