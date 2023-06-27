f9b39c08d6490057dbb294e8bccc073baaca176b
Ilja <idaderko@gmail.com>
2023-06-27 14:58:41 +0300

    add danger

---
+++ dangerfile.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-await-in-loop */
+import { danger, warn, fail } from 'danger'
+
+// -- Data --------------------------------------------------------------------
+const { modified_files, created_files, deleted_files, diffForFile } = danger.git
+const updated_files = [...modified_files, ...created_files, ...deleted_files]
+const packageJsons = updated_files.filter(f => f.includes('package.json'))
+const packageLock = updated_files.find(f => f.includes('package-lock.json'))
+const yarnLock = updated_files.find(f => f.includes('yarn.lock'))
+const pnpmLock = updated_files.find(f => f.includes('pnpm-lock.yaml'))
+
+// -- Dependency Checks -------------------------------------------------------
+if (packageJsons.length && !packageLock) {
+  warn('Changes were made to one or more package.json(s), but not to package-lock.json')
+}
+
+if (yarnLock || pnpmLock) {
+  fail('yarn or pnpm lockfile(s) detected, please use npm')
+}
+
+for (const f of packageJsons) {
+  const diff = await diffForFile(f)
+  if (diff) {
+    const { added } = diff
+    if (added.includes('^') || added.includes('~')) {
+      fail(`${f} should use strict dependency versions`)
+    }
+  }
+}
