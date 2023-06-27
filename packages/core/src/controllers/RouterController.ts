90ceef8ccec3583f8e342dae4e4370ab57db4753
0xAsimetriq <3154053+0xAsimetriq@users.noreply.github.com>
2023-06-27 15:42:55 +0300

    Add first controllers, tests and danger (#1101)
    * setup core
    
    * save progress
    
    * Add router tests
    
    * add test step to github actions
    
    * add danger
    
    * fix async usage
    
    * Cleanup dangerfile
    
    * Update text in danger messages
    
    * better router test titles
    
    * update actions title

---
+++ packages/core/src/controllers/RouterController.ts
@@ -0,0 +1,42 @@
+import { proxy } from 'valtio/vanilla'
+
+// -- Types --------------------------------------------------------------------
+export interface RouterControllerState {
+  view: 'Account' | 'Connect' | 'Networks'
+  history: RouterControllerState['view'][]
+}
+
+// -- Controller ---------------------------------------------------------------
+export class RouterController {
+  public state = proxy<RouterControllerState>({
+    view: 'Connect',
+    history: ['Connect']
+  })
+
+  public push(view: RouterControllerState['view']) {
+    if (view !== this.state.view) {
+      this.state.view = view
+      this.state.history.push(view)
+    }
+  }
+
+  public reset(view: RouterControllerState['view']) {
+    this.state.view = view
+    this.state.history = [view]
+  }
+
+  public replace(view: RouterControllerState['view']) {
+    if (this.state.history.length > 1 && this.state.history.at(-1) !== view) {
+      this.state.view = view
+      this.state.history[this.state.history.length - 1] = view
+    }
+  }
+
+  public goBack() {
+    if (this.state.history.length > 1) {
+      this.state.history.pop()
+      const [last] = this.state.history.slice(-1)
+      this.state.view = last
+    }
+  }
+}
