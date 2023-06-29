759886f90c41f965451594b45163fbd300b4f068
Sven <38101365+svenvoskamp@users.noreply.github.com>
2023-06-29 12:07:56 +0200

    [:feat] add theme switch (#1114)
    Co-authored-by: Ilja <idaderko@gmail.com>

--- apps/gallery/.storybook/preview.tsx
+++ apps/gallery/.storybook/preview.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { Controls, Description, Primary, Source, Stories, Subtitle, Title } from '@storybook/blocks'
 import { themes } from '@storybook/theming'
+import { setColorTheme, initializeTheming } from '@web3modal/ui/src/utils/ThemeUtil'
+import { addons } from '@storybook/preview-api'
+import { GLOBALS_UPDATED, SET_GLOBALS } from '@storybook/core-events'
 
+// -- Utilities ------------------------------------------------------------
+initializeTheming()
+
+const backgroundChangeListener = args => {
+  const bgColor = args.globals.backgrounds?.value
+  if (bgColor) {
+    const theme = bgColor === '#272A2A' ? 'dark' : 'light'
+    setColorTheme(theme)
+  } else {
+    setColorTheme('dark')
+  }
+}
+
+const channel = addons.getChannel()
+channel.addListener(SET_GLOBALS, backgroundChangeListener)
+channel.addListener(GLOBALS_UPDATED, backgroundChangeListener)
+
+// -- Configuration --------------------------------------------------------
 /** @type { import('@storybook/web-components').Preview } */
-const preview = {
+export default {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     layout: 'centered',
@@ -26,6 +47,7 @@ const preview = {
         }
       ]
     },
+
     docs: {
       theme: themes.dark,
       page: () => (
@@ -42,5 +64,3 @@ const preview = {
     }
   }
 }
-
-export default preview
--- packages/ui/src/utils/ThemeUtil.ts
+++ packages/ui/src/utils/ThemeUtil.ts
@@ -1,6 +1,19 @@
 import { css } from 'lit'
 
-export const globalStyles = css`
+// -- Utilities ---------------------------------------------------------------
+export function initializeTheming() {
+  const styleTag = document.createElement('style')
+  styleTag.dataset.wui = 'theme'
+  styleTag.textContent = rootStyles.cssText
+  document.head.appendChild(styleTag)
+}
+
+export function setColorTheme(theme: string) {
+  document.documentElement.setAttribute('data-wui-theme', theme)
+}
+
+// -- Presets -----------------------------------------------------------------
+export const rootStyles = css`
   :root {
     --wui-font-family: 'SF Pro Text', 'Segoe UI', Roboto, Ubuntu, 'Helvetica Neue', sans-serif;
     --wui-font-size-xxs: 10px;
@@ -21,7 +34,9 @@ export const globalStyles = css`
     --wui-line-height-lg: 130%;
 
     --wui-color-inherit: inherit;
+  }
 
+  [data-wui-theme='dark'] {
     --wui-color-blue-100: #47a1ff;
     --wui-color-blue-090: #59aaff;
     --wui-color-blue-080: #6cb4ff;
@@ -94,8 +109,8 @@ export const globalStyles = css`
     --wui-color-bg-275: #dce7e7;
     --wui-color-bg-300: #d8e3e3;
 
-    --wui-color-inverse-100: #000;
-    --wui-color-inverse-000: #fff;
+    --wui-color-inverse-100: #fff;
+    --wui-color-inverse-000: #000;
 
     --wui-color-success-100: #26b562;
     --wui-color-error-100: #f05142;
@@ -116,7 +131,9 @@ export const globalStyles = css`
 
     --wui-box-shadow-blue: rgba(51, 150, 255, 0.16);
   }
+`
 
+export const globalStyles = css`
   *,
   *::after,
   *::before,
@@ -130,47 +147,6 @@ export const globalStyles = css`
     -moz-osx-font-smoothing: grayscale;
     -webkit-tap-highlight-color: transparent;
     backface-visibility: hidden;
-
-    --wui-font-family: 'SF Pro Text', 'Segoe UI', Roboto, Ubuntu, 'Helvetica Neue', sans-serif;
-    --wui-font-size-xxs: 10px;
-    --wui-font-size-sm: 14px;
-    --wui-font-size-md: 16px;
-    --wui-font-size-lg: 20px;
-
-    --wui-font-weight-medium: 500;
-    --wui-font-weight-semibold: 600;
-    --wui-font-weight-bold: 700;
-
-    --wui-letter-spacing-sm: -0.02em;
-    --wui-letter-spacing-md: -0.03em;
-    --wui-letter-spacing-lg: -0.05em;
-
-    --wui-line-height-sm: 120%;
-    --wui-line-height-md: 125%;
-    --wui-line-height-lg: 130%;
-
-    --wui-color-inherit: inherit;
-    --wui-color-blue-100: #47a1ff;
-    --wui-color-blue-090: #59aaff;
-    --wui-color-blue-080: #6cb4ff;
-    --wui-color-inverse-100: #fff;
-    --wui-color-inverse-000: #000;
-    --wui-color-error-100: #f25a67;
-    --wui-color-success-100: #26d962;
-    --wui-color-fg-100: #e4e7e7;
-    --wui-color-fg-200: #949e9e;
-    --wui-color-fg-300: #6e7777;
-    --wui-color-bg-300: #3b4040;
-
-    --wui-overlay-002: rgba(255, 255, 255, 0.02);
-    --wui-overlay-005: rgba(255, 255, 255, 0.05);
-    --wui-overlay-010: rgba(255, 255, 255, 0.1);
-    --wui-overlay-015: rgba(255, 255, 255, 0.15);
-    --wui-overlay-020: rgba(255, 255, 255, 0.2);
-    --wui-overlay-025: rgba(255, 255, 255, 0.25);
-    --wui-overlay-030: rgba(255, 255, 255, 0.3);
-
-    --wui-box-shadow-blue: rgba(71, 161, 255, 0.16);
   }
 
   button {
