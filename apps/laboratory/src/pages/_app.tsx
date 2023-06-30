a6d74e83a43a11737c58fe577491f22bcd2551af
0xAsimetriq <3154053+0xAsimetriq@users.noreply.github.com>
2023-06-30 15:27:55 +0300

    [v3] Set up apps/laboratory (#1123)

---
+++ apps/laboratory/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
