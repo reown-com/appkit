587dcb8cc767ed5f87f76ddf6d0535b3735acda0
Ilja <idaderko@gmail.com>
2023-06-30 15:01:48 +0300

    setup chakra

--- apps/laboratory/src/pages/_app.tsx
+++ apps/laboratory/src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
--- apps/laboratory/src/pages/_document.tsx
+++ apps/laboratory/src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode="system" />
         <Main />
         <NextScript />
       </body>
