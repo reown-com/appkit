c213cb5b23aea51015fd4cd9b89e6572a78cfdcf
Ilja <idaderko@gmail.com>
2023-06-30 15:42:48 +0300

    Prepare scafold client

--- packages/scaffold-html/src/client.ts
+++ packages/scaffold-html/src/client.ts
@@ -1,3 +1,19 @@
 export class Web3ModalScaffoldHtml {
-  public constructor() {}
+  #initPromise?: Promise<void> = undefined
+
+  public constructor() {
+    this.#initOrContinue()
+  }
+
+  async #initOrContinue() {
+    if (!this.#initPromise) {
+      this.#initPromise = new Promise<void>(async resolve => {
+        const Web3ModalUi = await import('@web3modal/ui')
+        Web3ModalUi.initializeTheming()
+        resolve()
+      })
+    }
+
+    return this.#initPromise
+  }
 }
