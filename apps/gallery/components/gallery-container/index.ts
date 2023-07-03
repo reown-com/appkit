0df0e6b842e6a55ab924b57cbe96bdeb0307b9f6
Sven Voskamp <fr.sven.fr@gmail.com>
2023-07-03 11:24:44 +0200

    fix lint

--- apps/gallery/components/gallery-container/index.ts
+++ apps/gallery/components/gallery-container/index.ts
@@ -5,12 +5,14 @@ import styles from './styles'
 @customElement('gallery-container')
 export class GalleryContainer extends LitElement {
   public static styles = [styles]
+
   // -- state & properties ------------------------------------------- //
   @property() public width = '0'
 
   // -- render ------------------------------------------------------- //
   public render() {
     const maxWidth = `--container-width: ${this.width}px;`
+
     return html`<div style=${maxWidth}><slot></slot></div>`
   }
 }
