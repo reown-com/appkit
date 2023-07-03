c10b10649d3460f5c8d8602f109826308f6ad3e8
Sven Voskamp <fr.sven.fr@gmail.com>
2023-07-03 18:00:36 +0200

    [:fix] button cleanup & css truncate fix

--- apps/gallery/stories/composites/wui-card-select.stories.ts
+++ apps/gallery/stories/composites/wui-card-select.stories.ts
@@ -3,14 +3,14 @@ import '@web3modal/ui/src/composites/wui-card-select'
 import '../../components/gallery-container'
 import type { WuiCardSelect } from '@web3modal/ui/src/composites/wui-card-select'
 import { html } from 'lit'
+import { walletImageSrc } from '../../utils/PresetUtils'
 
 type Component = Meta<WuiCardSelect>
 
 export default {
   title: 'Composites/wui-card-select',
   args: {
-    imageSrc:
-      'https://explorer-api.walletconnect.com/w3m/v1/getWalletImage/7a33d7f1-3d12-4b5c-f3ee-5cd83cb1b500?projectId=a8d876c6f91c3748db621583fad358f1',
+    imageSrc: walletImageSrc,
     name: 'Rainbow',
     disabled: false
   }
--- packages/ui/src/composites/wui-link/styles.ts
+++ packages/ui/src/composites/wui-link/styles.ts
@@ -3,32 +3,12 @@ import { css } from 'lit'
 export default css`
   button {
     padding: 2px 6px;
-    column-gap: var(--wui-spacing-3xs);
-    transition: all 200ms ease-in-out;
     border-radius: var(--wui-border-radius-3xs);
     background-color: transparent;
-    border: 1px solid transparent;
   }
 
   button:disabled > wui-text,
   button:disabled > wui-icon {
     color: var(--wui-color-bg-300);
-  }
-
-  button:focus {
-    border: 1px solid var(--wui-color-blue-100);
-    -webkit-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
-    -moz-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
-    box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
-  }
-
-  @media (hover: hover) and (pointer: fine) {
-    button:hover:enabled {
-      background-color: var(--wui-overlay-005);
-    }
-
-    button:active:enabled {
-      background-color: var(--wui-overlay-010);
-    }
   }
 `
