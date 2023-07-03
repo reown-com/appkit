78ac6ef305fdd6514719192f221f27e31e73d906
Chris Smith <chris@walletconnect.com>
2023-07-03 18:11:57 -0400

    fix: lint

--- packages/core/src/utils/BlockchainApiUtil.ts
+++ packages/core/src/utils/BlockchainApiUtil.ts
@@ -18,10 +18,10 @@ export const BlockchainApiUtil = {
     if (response.status === 404) {
       // Profile not found
       return null
-    } else if (response.status !== 200) {
-      throw new Error('Problem resolving profile: ' + response.status)
-    } else {
+    } else if (response.status === 200) {
       return response.json()
+    } else {
+      throw new Error(`Problem resolving profile: ${response.status}`)
     }
   }
 }
