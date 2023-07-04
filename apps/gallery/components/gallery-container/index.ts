392b7aec6642c2252dfd79c75153835a4bb3aac7
Sven <fr.sven.fr@gmail.com>
2023-07-04 13:00:42 +0200

    fix lint

--- apps/gallery/components/gallery-container/index.ts
+++ apps/gallery/components/gallery-container/index.ts
@@ -9,13 +9,13 @@ export class GalleryContainer extends LitElement {
   // -- state & properties ------------------------------------------- //
   @property() public width = '0'
 
-  @property() public height? = 'auto'
+  @property() public height = 'auto'
 
   // -- render ------------------------------------------------------- //
   public render() {
-    const styles = `--container-width: ${this.width}px; --container-height: ${this.height}px;`
+    const inlineStyles = `--container-width: ${this.width}px; --container-height: ${this.height}px;`
 
-    return html`<div style=${styles}><slot></slot></div>`
+    return html`<div style=${inlineStyles}><slot></slot></div>`
   }
 }
 
