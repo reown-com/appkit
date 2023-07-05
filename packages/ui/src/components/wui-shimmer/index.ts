92f01dd749610b4fc44c3b0df6b258f9519ad531
Sven <fr.sven.fr@gmail.com>
2023-07-05 12:53:22 +0200

    remove html

--- packages/ui/src/components/wui-shimmer/index.ts
+++ packages/ui/src/components/wui-shimmer/index.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit'
+import { LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import styles from './styles'
 import type { BorderRadiusType } from '../../utils/TypesUtil'
