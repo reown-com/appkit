07ad95730e55a4158ab0cfae6ae9becf2c03c972
Sven <38101365+svenvoskamp@users.noreply.github.com>
2023-07-05 09:57:05 +0200

    add wui-input-cancel component (#1149)

---
+++ packages/ui/src/composites/wui-input-element/index.ts
@@ -0,0 +1,32 @@
+import type { TemplateResult } from 'lit'
+import { html, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { closeSvg } from '../../assets/svg/close'
+import { globalStyles } from '../../utils/ThemeUtil'
+import '../../components/wui-icon'
+import styles from './styles'
+
+@customElement('wui-input-element')
+export class WuiInputElement extends LitElement {
+  public static styles = [globalStyles, styles]
+
+  // -- state & properties ------------------------------------------- //
+  @property({ type: Object }) public icon: TemplateResult<2> = closeSvg
+
+  @property() public onClick: (event: PointerEvent) => void = () => null
+
+  // -- render ------------------------------------------------------- //
+  public render() {
+    return html`
+      <button @click=${this.onClick.bind(this)}>
+        <wui-icon color="bg-200" size="xxs">${this.icon}</wui-icon>
+      </button>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wui-input-element': WuiInputElement
+  }
+}
