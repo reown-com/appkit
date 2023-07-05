2770da694cc14c21de1c5df8d945568d3a304a98
Ilja <idaderko@gmail.com>
2023-07-05 11:10:43 +0300

    Update deps, organise dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,21 +5,22 @@ import { danger, fail, warn } from 'danger'
 const { modified_files, created_files, deleted_files, diffForFile } = danger.git
 const updated_files = [...modified_files, ...created_files]
 const all_files = [...updated_files, ...created_files, ...deleted_files]
-const packageJsons = all_files.filter(f => f.includes('package.json'))
-const packageLock = updated_files.find(f => f.includes('package-lock.json'))
-const yarnLock = updated_files.find(f => f.includes('yarn.lock'))
-const pnpmLock = updated_files.find(f => f.includes('pnpm-lock.yaml'))
 
 // -- Dependency Checks -------------------------------------------------------
-if (packageJsons.length && !packageLock) {
-  warn('Changes were made to one or more package.json(s), but not to package-lock.json')
-}
-
-if (yarnLock || pnpmLock) {
-  fail('Non npm lockfile(s) detected (yarn / pnpm), please use npm')
-}
-
 async function checkPackageJsons() {
+  const packageJsons = all_files.filter(f => f.includes('package.json'))
+  const packageLock = updated_files.find(f => f.includes('package-lock.json'))
+  const yarnLock = updated_files.find(f => f.includes('yarn.lock'))
+  const pnpmLock = updated_files.find(f => f.includes('pnpm-lock.yaml'))
+
+  if (packageJsons.length && !packageLock) {
+    warn('Changes were made to one or more package.json(s), but not to package-lock.json')
+  }
+
+  if (yarnLock || pnpmLock) {
+    fail('Non npm lockfile(s) detected (yarn / pnpm), please use npm')
+  }
+
   for (const f of packageJsons) {
     const diff = await diffForFile(f)
     if (diff && (diff.added.includes('^') || diff.added.includes('~'))) {
