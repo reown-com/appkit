d86c179b679cb242f48416418de9e86572b773db
Ilja <idaderko@gmail.com>
2023-07-05 17:07:50 +0300

    glue it together

--- packages/core/src/controllers/ModalController.ts
+++ packages/core/src/controllers/ModalController.ts
@@ -1,9 +1,12 @@
+import { subscribeKey } from 'valtio/utils'
 import { proxy } from 'valtio/vanilla'
 
 // -- Types --------------------------------------------------------------------
 export interface ModalControllerState {
   open: boolean
 }
+
+type StateKey = keyof ModalControllerState
 
 // -- State --------------------------------------------------------------------
 const state = proxy<ModalControllerState>({
@@ -13,6 +16,10 @@ const state = proxy<ModalControllerState>({
 // -- Controller ---------------------------------------------------------------
 export const ModalController = {
   state,
+
+  subscribe<K extends StateKey>(key: K, callback: (value: ModalControllerState[K]) => void) {
+    subscribeKey(state, key, callback)
+  },
 
   open() {
     state.open = true
--- packages/scaffold-html/src/modal/index.ts
+++ packages/scaffold-html/src/modal/index.ts
@@ -1,25 +1,32 @@
+import { ModalController } from '@web3modal/core'
 import '@web3modal/ui'
 import { initializeTheming, setColorTheme } from '@web3modal/ui'
 import { LitElement, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, state } from 'lit/decorators.js'
 import styles from './styles'
 
 @customElement('w3m-modal')
 export class W3mModal extends LitElement {
   public static styles = [styles]
 
+  @state() private open = ModalController.state.open
+
   public constructor() {
     super()
     initializeTheming()
     setColorTheme('dark')
+
+    ModalController.subscribe('open', open => (this.open = open))
   }
 
   public render() {
-    return html`
-      <wui-overlay>
-        <wui-card>Hello</wui-card>
-      </wui-overlay>
-    `
+    return this.open
+      ? html`
+          <wui-overlay @click=${ModalController.close}>
+            <wui-card>Hello</wui-card>
+          </wui-overlay>
+        `
+      : null
   }
 }
 
