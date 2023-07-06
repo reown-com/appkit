0c8508c520ad6dfbb9084b49697ded26d3120118
Sven <fr.sven.fr@gmail.com>
2023-07-06 17:49:06 +0200

    small fixes

--- packages/ui/src/assets/svg/checkmark.ts
+++ packages/ui/src/assets/svg/checkmark.ts
@@ -1,9 +1,10 @@
 import { svg } from 'lit'
 
 export const checkmarkSvg = svg`<svg viewBox="0 0 12 12">
-	<path
-		fill="currentColor"
-		fill-rule="evenodd"
-		d="M10.82.92c.4.25.5.77.25 1.16l-5.62 8.75a.83.83 0 0 1-1.34.08L.98 7.16a.83.83 0 1 1 1.28-1.07l2.4 2.88 5.01-7.8a.83.83 0 0 1 1.15-.25Z"
-		clip-rule="evenodd"/>
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M10.82.92c.4.25.5.77.25 1.16l-5.62 8.75a.83.83 0 0 1-1.34.08L.98 7.16a.83.83 0 1 1 1.28-1.07l2.4 2.88 5.01-7.8a.83.83 0 0 1 1.15-.25Z"
+    clip-rule="evenodd"
+  />
 </svg>`
--- packages/ui/src/composites/wui-snackbar/styles.ts
+++ packages/ui/src/composites/wui-snackbar/styles.ts
@@ -6,7 +6,7 @@ export default css`
     column-gap: var(--wui-spacing-xs);
     align-items: center;
     padding: 9px 16px 10px 8px;
-    border-radius: 1000px;
+    border-radius: var(--wui-border-radius-3xl);
     border: 1px solid var(--wui-overlay-005);
     background-color: var(--wui-color-bg-275);
   }
