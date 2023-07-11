71d8546e6cf46cd5a0396bbef961cdd30c1d724c
Sven <fr.sven.fr@gmail.com>
2023-07-11 16:02:43 +0200

    hover/focus refactor

--- packages/ui/src/utils/ThemeUtil.ts
+++ packages/ui/src/utils/ThemeUtil.ts
@@ -273,10 +273,6 @@ export const elementStyles = css`
     button:active:enabled {
       transition: all var(--wui-ease-out-power-4) var(--wui-duration-sm);
       background-color: var(--wui-overlay-010);
-      /* border: 1px solid var(--wui-color-blue-100);
-      -webkit-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
-      -moz-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
-      box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue); */
     }
   }
 
