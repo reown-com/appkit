27c23adf916bfa478157e5acb1c7a1b949410143
Sven <fr.sven.fr@gmail.com>
2023-07-11 15:21:49 +0200

    add wui-tag

---
+++ packages/ui/src/composites/wui-tag/styles.ts
@@ -0,0 +1,11 @@
+import { css } from 'lit'
+
+export default css`
+  :host {
+    display: block;
+    padding: 3.5px 5px 4.5px 5px !important;
+    background-color: var(--local-bg-value);
+    border-radius: var(--wui-border-radius-5xs);
+    color: var(--local-color-value);
+  }
+`
