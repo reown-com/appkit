3728c74f8cd0b40627982900714a1906080e2a21
Sven <fr.sven.fr@gmail.com>
2023-07-11 15:28:19 +0200

    adjusted padding

--- packages/ui/src/composites/wui-tag/styles.ts
+++ packages/ui/src/composites/wui-tag/styles.ts
@@ -3,7 +3,7 @@ import { css } from 'lit'
 export default css`
   :host {
     display: block;
-    padding: 3.5px 5px 4.5px 5px !important;
+    padding: 2px 5px 3px 5px !important;
     background-color: var(--local-bg-value);
     border-radius: var(--wui-border-radius-5xs);
     color: var(--local-color-value);
