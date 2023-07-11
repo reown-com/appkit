198adb3671b79b44049cc715c28fd99e95e8fe9e
Sven <38101365+svenvoskamp@users.noreply.github.com>
2023-07-11 12:44:21 +0200

    V 3/icon box transparency (#1181)

--- packages/ui/src/composites/wui-icon-box/index.ts
+++ packages/ui/src/composites/wui-icon-box/index.ts
@@ -2,7 +2,7 @@ import { html, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import '../../components/wui-icon'
 import { elementStyles, resetStyles } from '../../utils/ThemeUtil'
-import type { ColorType, IconType, SizeType } from '../../utils/TypesUtil'
+import type { BackgroundType, ColorType, IconType, SizeType } from '../../utils/TypesUtil'
 import styles from './styles'
 
 @customElement('wui-icon-box')
@@ -15,6 +15,8 @@ export class WuiIconBox extends LitElement {
   @property() public backgroundColor: ColorType = 'blue-100'
 
   @property() public iconColor: ColorType = 'blue-100'
+
+  @property() public background: BackgroundType = 'transparent'
 
   @property({ type: Boolean }) public border? = false
 
@@ -36,10 +38,18 @@ export class WuiIconBox extends LitElement {
     const isLg = this.size === 'lg'
     const bgMix = isLg ? '12%' : '16%'
     const borderRadius = isLg ? 'xxs' : '3xl'
+    const isOpaque = this.background === 'opaque'
+    const isColorChange =
+      (this.backgroundColor === 'blue-100' && isOpaque) ||
+      (this.backgroundColor === 'error-100' && isOpaque)
 
     this.style.cssText = `
-       --local-bg-value: var(--wui-color-${this.backgroundColor});
-       --local-bg-mix: ${bgMix};
+       --local-bg-value: ${
+         isColorChange
+           ? `var(--wui-icon-box-bg-${this.backgroundColor})`
+           : `var(--wui-color-${this.backgroundColor})`
+       };
+       --local-bg-mix: ${isColorChange ? `100%` : bgMix};
        --local-border-radius: var(--wui-border-radius-${borderRadius});
        --local-size: var(--wui-icon-box-size-${this.size});
        --local-border: 2px solid ${this.border ? `var(--wui-color-bg-125)` : `transparent`}
