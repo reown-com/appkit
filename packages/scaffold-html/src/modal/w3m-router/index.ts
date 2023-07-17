ce159559330679cff15e5a325039a62d6b02f485
Ilja <idaderko@gmail.com>
2023-07-17 17:14:03 +0300

    adjust animations

--- packages/scaffold-html/src/modal/w3m-router/index.ts
+++ packages/scaffold-html/src/modal/w3m-router/index.ts
@@ -13,6 +13,8 @@ export class W3mRouter extends LitElement {
 
   private prevHeight = '0px'
 
+  private prevHistoryLength = 1
+
   private unsubscribe: (() => void)[] = []
 
   // -- State & Properties -------------------------------- //
@@ -62,9 +64,17 @@ export class W3mRouter extends LitElement {
   }
 
   private async onViewChange(newView: RouterControllerState['view']) {
-    await animate(this, { opacity: [1, 0], scale: [1, 1.02] }, { duration: 0.15 }).finished
+    const { history } = RouterController.state
+    let xOut = -10
+    let xIn = 10
+    if (history.length < this.prevHistoryLength) {
+      xOut = 10
+      xIn = -10
+    }
+    this.prevHistoryLength = history.length
+    await animate(this, { opacity: [1, 0], x: [0, xOut] }, { duration: 0.15 }).finished
     this.view = newView
-    animate(this, { opacity: [0, 1], scale: [0.98, 1] }, { duration: 0.15, delay: 0.05 })
+    animate(this, { opacity: [0, 1], x: [xIn, 0] }, { duration: 0.15, delay: 0.05 })
   }
 
   private getWrapper() {
--- packages/scaffold-html/src/partials/w3m-header/index.ts
+++ packages/scaffold-html/src/partials/w3m-header/index.ts
@@ -1,9 +1,21 @@
+import type { RouterControllerState } from '@web3modal/core'
 import { ModalController, RouterController } from '@web3modal/core'
 import { LitElement, html } from 'lit'
 import { customElement, state } from 'lit/decorators.js'
 import { animate } from 'motion'
 
 // -- Helpers ------------------------------------------- //
+function headings() {
+  const connectorName = RouterController.state.data?.connector.name
+
+  return {
+    Connect: 'Connect Wallet',
+    Account: 'Account',
+    ConnectingExternal: connectorName ?? 'Connect Wallet',
+    ConnectingWalletConnect: connectorName ?? 'WalletConnect',
+    Networks: 'Choose Network'
+  }
+}
 
 @customElement('w3m-header')
 export class W3mHeader extends LitElement {
@@ -13,9 +25,18 @@ export class W3mHeader extends LitElement {
   // -- State & Properties --------------------------------- //
   @state() private heading = 'Connect Wallet'
 
+  @state() private showBack = false
+
   public constructor() {
     super()
-    this.unsubscribe.push(RouterController.subscribeKey('view', val => this.onViewChange(val)))
+    this.unsubscribe.push(
+      ...[
+        RouterController.subscribeKey('view', val => {
+          this.onViewChange(val)
+          this.onHistoryChange()
+        })
+      ]
+    )
   }
 
   disconnectCallback() {
@@ -30,7 +51,7 @@ export class W3mHeader extends LitElement {
         justifyContent="space-between"
         alignItems="center"
       >
-        <wui-icon-link icon="copy" @click=${RouterController.goBack}></wui-icon-link>
+        <wui-icon-link id="hmm" icon="clock" @click=${RouterController.goBack}></wui-icon-link>
         <wui-text variant="paragraph-700" color="fg-100">${this.heading}</wui-text>
         <wui-icon-link icon="close" @click=${ModalController.close}></wui-icon-link>
       </wui-flex>
@@ -39,11 +60,23 @@ export class W3mHeader extends LitElement {
   }
 
   // -- Private ------------------------------------------- //
-  private async onViewChange(view: string) {
+  private async onViewChange(view: RouterControllerState['view']) {
     const headingEl = this.shadowRoot?.querySelector('wui-text')
     if (headingEl) {
-      await animate(headingEl, { opacity: 0 }, { duration: 0.15 }).finished
-      animate(headingEl, { opacity: 1 }, { duration: 0.15, delay: 0.05 })
+      const preset = headings()[view]
+      await animate(headingEl, { opacity: 0 }, { duration: 0.2 }).finished
+      this.heading = preset
+      animate(headingEl, { opacity: 1 }, { duration: 0.2 })
+    }
+  }
+
+  private async onHistoryChange() {
+    const { history } = RouterController.state
+    const buttonEl = this.shadowRoot?.querySelector('#hmm')
+    if (history.length > 1 && !this.showBack && buttonEl) {
+      await animate(buttonEl, { opacity: [0, 1] }, { duration: 0.2 }).finished
+      this.showBack = true
+      animate(buttonEl, { opacity: [1, 0] }, { duration: 0.2 })
     }
   }
 }
