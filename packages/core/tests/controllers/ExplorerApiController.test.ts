9fa60d7cd7d189e09b7e683268ef7688a937d2d0
Ilja <idaderko@gmail.com>
2023-07-19 17:02:44 +0300

    hesuuuu

--- packages/core/tests/controllers/ExplorerApiController.test.ts
+++ packages/core/tests/controllers/ExplorerApiController.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest'
-import { ConnectorController } from '../../index'
+import { ExplorerApiController } from '../../index'
 
 // -- Tests --------------------------------------------------------------------
 describe('ExplorerApiController', () => {
   it('should have valid default state', () => {
-    expect(ConnectorController.state).toEqual({
+    expect(ExplorerApiController.state).toEqual({
       projectId: '',
       page: 1,
       total: 0,
