4961ae30fe13270716bd4c78cb760382aa34c576
Ilja <idaderko@gmail.com>
2023-07-31 14:40:43 +0300

    mobile layout

--- packages/scaffold-html/src/modal/w3m-modal/styles.ts
+++ packages/scaffold-html/src/modal/w3m-modal/styles.ts
@@ -1,6 +1,24 @@
 import { css } from 'lit'
 
 export default css`
+  @keyframes zoom-in {
+    0% {
+      transform: scale(0.95) translateY(0);
+    }
+    100% {
+      transform: scale(1) translateY(0);
+    }
+  }
+
+  @keyframes slide-in {
+    0% {
+      transform: scale(1) translateY(50px);
+    }
+    100% {
+      transform: scale(1) translateY(0);
+    }
+  }
+
   wui-overlay {
     display: flex;
     align-items: center;
@@ -11,11 +29,23 @@ export default css`
     max-width: 360px;
     width: 100%;
     position: relative;
+    animation-duration: 0.2s;
+    animation-name: zoom-in;
+    animation-fill-mode: backwards;
+    animation-timing-function: var(--wui-ease-out-power-4);
   }
 
-  @media (max-width: 360px) {
+  @media (max-width: 430px) {
     wui-overlay {
       align-items: flex-end;
+    }
+
+    wui-card {
+      max-width: 100%;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      border-bottom: none;
+      animation-name: slide-in;
     }
   }
 `
