0daa4bb186476b575c48857019d01047a8061d98
Jonathan Conn <jonathansconn@gmail.com>
2023-08-01 17:34:42 -0400

    fix: removed unused older test

--- laboratory/tests/shared/fixtures/fixture.spec.ts
+++
@@ -1,6 +0,0 @@
-import { testMW, expect } from './w3m-wallet-fixture'
-
-testMW('Open pages', ({ modalPage, walletPage }) => {
-  expect(modalPage).toBeDefined()
-  expect(walletPage).toBeDefined()
-})
