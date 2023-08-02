076e97a7d21fa20fc431d01490077fef1d105ffc
Ilja <idaderko@gmail.com>
2023-08-02 14:21:07 +0300

    stash

--- apps/laboratory/next.config.js
+++ apps/laboratory/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  transpilePackages: [
+    '@web3modal/core',
+    '@web3modal/ui',
+    '@web3modal/scaffold-html',
+    '@web3modal/wagmi'
+  ]
 }
 
 module.exports = nextConfig
