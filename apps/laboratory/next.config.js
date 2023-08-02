1e645c0fe8b5134a3f504fc83704f43032a81c97
Ilja <idaderko@gmail.com>
2023-08-02 13:44:47 +0300

    update deps, create watch command

--- apps/laboratory/next.config.js
+++ apps/laboratory/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  transpilePackages: [
-    '@web3modal/core',
-    '@web3modal/ui',
-    '@web3modal/scaffold-html',
-    '@web3modal/wagmi',
-    '@web3modal/wagmi/react'
-  ]
+  output: 'export'
 }
 
 module.exports = nextConfig
