3fab2e569282f8dad8056471effc1941684b27e2
Ilja <idaderko@gmail.com>
2023-08-15 13:41:59 +0300

    update jsonrpc provider implementation

--- packages/wagmi/src/utils/provider.ts
+++ packages/wagmi/src/utils/provider.ts
@@ -1,29 +1,36 @@
-import type { Chain } from '@wagmi/core'
-import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc'
+import type { Chain, ChainProviderFn } from '@wagmi/core'
 import { BLOCKCHAIN_API, NAMESPACE } from './constants.js'
 
 interface Options {
   projectId: string
 }
 
-export function walletConnectProvider<C extends Chain>({ projectId }: Options) {
-  return jsonRpcProvider<C>({
-    rpc: chain => {
-      const supported = [
-        1, 3, 4, 5, 10, 42, 56, 69, 97, 100, 137, 280, 324, 420, 42161, 42220, 43114, 80001, 421611,
-        421613, 1313161554, 1313161555
-      ]
+export function walletConnectProvider<C extends Chain = Chain>({
+  projectId
+}: Options): ChainProviderFn<C> {
+  return function provider(chain) {
+    const supported = [
+      1, 3, 4, 5, 10, 42, 56, 69, 97, 100, 137, 280, 324, 420, 42161, 42220, 43114, 80001, 421611,
+      421613, 1313161554, 1313161555, 7777777, 8453
+    ]
 
-      if (supported.includes(chain.id)) {
-        return {
-          http: `${BLOCKCHAIN_API}/v1/?chainId=${NAMESPACE}:${chain.id}&projectId=${projectId}`
+    if (!supported.includes(chain.id)) {
+      return null
+    }
+
+    const baseHttpUrl = `${BLOCKCHAIN_API}/v1/?chainId=${NAMESPACE}:${chain.id}&projectId=${projectId}`
+
+    return {
+      chain: {
+        ...chain,
+        rpcUrls: {
+          ...chain.rpcUrls,
+          default: { http: [baseHttpUrl] }
         }
-      }
-
-      return {
-        http: chain.rpcUrls.default.http[0] ?? '',
-        webSocket: chain.rpcUrls.default.webSocket?.[0]
+      } as C,
+      rpcUrls: {
+        http: [baseHttpUrl]
       }
     }
-  })
+  }
 }
