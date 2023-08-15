dbd2249067387c35ab755e627af7cc2935985af3
Ilja <idaderko@gmail.com>
2023-08-15 12:25:05 +0300

    add api controller tests

---
+++ packages/core/tests/controllers/ApiController.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest'
+import { ApiController } from '../../index.js'
+
+// -- Tests --------------------------------------------------------------------
+describe('ApiController', () => {
+  it('should have valid default state', () => {
+    expect(ApiController.state).toEqual({
+      projectId: '',
+      page: 1,
+      count: 0,
+      recommended: [],
+      wallets: [],
+      search: [],
+      images: {}
+    })
+  })
+})
