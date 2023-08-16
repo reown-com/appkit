45661dbc7a49bc681764b841eb740ab7f02982cf
Ilja <idaderko@gmail.com>
2023-08-16 14:29:16 +0300

    update from v3

--- packages/scaffold/src/views/w3m-get-wallet-view/index.ts
+++ packages/scaffold/src/views/w3m-get-wallet-view/index.ts
@@ -1,4 +1,4 @@
-import { ApiController, CoreHelperUtil } from '@web3modal/core'
+import { ApiController, AssetController, AssetUtil, CoreHelperUtil } from '@web3modal/core'
 import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import { ifDefined } from 'lit/directives/if-defined.js'
@@ -8,11 +8,10 @@ export class W3mGetWalletView extends LitElement {
   // -- Members ------------------------------------------- //
   private recommendedWallets = ApiController.state.recommended
 
-  private images = ApiController.state.images
-
   // -- Render -------------------------------------------- //
   public override render() {
-    const walletImages = Object.values(this.images).map(src => ({ src }))
+    const { walletImages } = AssetController.state
+    const walletImagesSrc = Object.values(walletImages).map(src => ({ src }))
 
     return html`
       <wui-flex flexDirection="column" padding="s" gap="xs">
@@ -20,7 +19,7 @@ export class W3mGetWalletView extends LitElement {
         <wui-list-wallet
           name="Explore all"
           showAllWallets
-          .walletImages=${walletImages}
+          .walletImages=${walletImagesSrc}
           icon="externalLink"
           @click=${() => {
             CoreHelperUtil.openHref('https://walletconnect.com/explorer?type=wallet', '_blank')
@@ -32,15 +31,13 @@ export class W3mGetWalletView extends LitElement {
 
   // -- Private ------------------------------------------- //
   private recommendedWalletsTemplate() {
-    const { images } = ApiController.state
-
     return this.recommendedWallets.map(
       wallet => html`
         <wui-list-wallet
           name=${wallet.name ?? 'Unknown'}
           tagLabel="get wallet"
           tagVariant="main"
-          imageSrc=${ifDefined(images[wallet.image_id])}
+          imageSrc=${ifDefined(AssetUtil.getWalletImage(wallet.image_id))}
           @click=${() => {
             CoreHelperUtil.openHref(wallet.homepage, '_blank')
           }}
