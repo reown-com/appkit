150192d56c597e1b92fa4941e982191fe5de2bf4
Ilja <idaderko@gmail.com>
2023-08-21 11:34:23 +0300

    remove wss rpc endpoint

--- packages/wagmi/src/utils/provider.ts
+++ packages/wagmi/src/utils/provider.ts
@@ -1,5 +1,5 @@
 import type { Chain, ChainProviderFn } from '@wagmi/core'
-import { BLOCKCHAIN_HTTP_API, BLOCKCHAIN_WSS_API, NAMESPACE } from './constants.js'
+import { BLOCKCHAIN_HTTP_API, NAMESPACE } from './constants.js'
 
 interface Options {
   projectId: string
@@ -9,37 +9,16 @@ export function walletConnectProvider<C extends Chain = Chain>({
   projectId
 }: Options): ChainProviderFn<C> {
   return function provider(chain) {
-    const supportedWss = [1, 5, 10, 69, 420, 42161, 42220, 421613, 1313161554, 1313161555]
-
-    const supportedHttp = [
-      ...supportedWss,
-      3,
-      4,
-      42,
-      56,
-      97,
-      100,
-      137,
-      280,
-      324,
-      8453,
-      43114,
-      80001,
-      421611,
-      7777777,
-      11155111
+    const supported = [
+      1, 5, 10, 69, 420, 42161, 42220, 421613, 1313161554, 1313161555, 3, 4, 42, 56, 97, 100, 137,
+      280, 324, 8453, 43114, 80001, 421611, 7777777, 11155111
     ]
 
-    if (!supportedHttp.includes(chain.id)) {
+    if (!supported.includes(chain.id)) {
       return null
     }
 
     const baseHttpUrl = `${BLOCKCHAIN_HTTP_API}/v1/?chainId=${NAMESPACE}:${chain.id}&projectId=${projectId}`
-    let baseWssUrl = undefined
-
-    if (supportedWss.includes(chain.id)) {
-      baseWssUrl = `${BLOCKCHAIN_WSS_API}/v1/?chainId=${NAMESPACE}:${chain.id}&projectId=${projectId}`
-    }
 
     return {
       chain: {
@@ -50,8 +29,7 @@ export function walletConnectProvider<C extends Chain = Chain>({
         }
       } as C,
       rpcUrls: {
-        http: [baseHttpUrl],
-        webSocket: baseWssUrl ? [baseWssUrl] : undefined
+        http: [baseHttpUrl]
       }
     }
   }
