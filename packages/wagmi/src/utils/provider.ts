19f3179ca27ad1a125bfe9bc59c4009977b6c254
Ilja <idaderko@gmail.com>
2023-08-22 16:12:50 +0300

    Added base and zora testnets

--- packages/wagmi/src/utils/provider.ts
+++ packages/wagmi/src/utils/provider.ts
@@ -10,8 +10,8 @@ export function walletConnectProvider<C extends Chain = Chain>({
 }: Options): ChainProviderFn<C> {
   return function provider(chain) {
     const supported = [
-      1, 5, 10, 69, 420, 42161, 42220, 421613, 1313161554, 1313161555, 3, 4, 42, 56, 97, 100, 137,
-      280, 324, 8453, 43114, 80001, 421611, 7777777, 11155111
+      1, 5, 10, 69, 420, 999, 42161, 42220, 421613, 1313161554, 1313161555, 3, 4, 42, 56, 97, 100,
+      137, 280, 324, 8453, 43114, 84531, 80001, 421611, 7777777, 11155111
     ]
 
     if (!supported.includes(chain.id)) {
