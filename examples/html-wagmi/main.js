71f1d97613c9b4aa555825865e19a3a09b36a878
Ilja <idaderko@gmail.com>
2023-09-08 15:36:44 +0300

    WIP html example

--- examples/html-wagmi/main.js
+++ examples/html-wagmi/main.js
@@ -1,3 +1,32 @@
-import { createWeb3Modal } from 'https://esm.sh/@web3modal/wagmi@canary'
+console.log('helloooo')
 
-const modal = createWeb3Modal({})
+import { arbitrum, mainnet } from 'https://esm.sh/@wagmi/core/chains'
+import { createWeb3Modal, defaultWagmiConfig } from 'https://esm.sh/@web3modal/wagmi@canary'
+
+// 1. Create projectId
+const projectId = 'YOUR_PROJECT_ID'
+
+// 2. Create wagmiConfig
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' })
+
+// 3. Create modal
+const modal = createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  chains,
+  themeMode: 'light',
+  themeVariables: {
+    '--w3m-color-mix': 'orange',
+    '--w3m-color-mix-strength': 10
+  }
+})
+
+// 4. Trigger modal programaticaly
+const openConnectModalBtn = document.getElementById('open-connect-modal')
+const openNetworkModalBtn = document.getElementById('open-network-modal')
+
+openConnectModalBtn.addEventListener('click', () => modal.open())
+openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
+
+// 5. Alternatively use w3m component buttons (see index.html)
