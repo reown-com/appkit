4ae5809be3cf490dc12e7182cd3afbf690c78393
Ilja <idaderko@gmail.com>
2023-09-14 09:10:46 +0300

    tag beta.1

--- packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
+++ packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
@@ -5,7 +5,6 @@ import { configureChains, createConfig } from '@wagmi/core'
 import { CoinbaseWalletConnector } from '@wagmi/core/connectors/coinbaseWallet'
 import { InjectedConnector } from '@wagmi/core/connectors/injected'
 import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
-import { W3mAnnouncedConnector } from '../connectors/W3mAnnouncedConnector.js'
 import { walletConnectProvider } from './provider.js'
 
 export interface ConfigOptions {
@@ -23,7 +22,7 @@ export function defaultWagmiConfig({ projectId, chains, appName }: ConfigOptions
     autoConnect: true,
     connectors: [
       new WalletConnectConnector({ chains, options: { projectId, showQrModal: false } }),
-      new W3mAnnouncedConnector({ chains }),
+
       new InjectedConnector({ chains, options: { shimDisconnect: true } }),
       new CoinbaseWalletConnector({ chains, options: { appName } })
     ],
--- packages/wagmi/src/utils/defaultWagmiReactConfig.ts
+++ packages/wagmi/src/utils/defaultWagmiReactConfig.ts
@@ -4,7 +4,6 @@ import { configureChains, createConfig } from 'wagmi'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { W3mAnnouncedConnector } from '../connectors/W3mAnnouncedConnector.js'
 import type { ConfigOptions } from './defaultWagmiCoreConfig.js'
 import { walletConnectProvider } from './provider.js'
 
@@ -17,7 +16,6 @@ export function defaultWagmiConfig({ projectId, chains, appName }: ConfigOptions
     autoConnect: true,
     connectors: [
       new WalletConnectConnector({ chains, options: { projectId, showQrModal: false } }),
-      new W3mAnnouncedConnector({ chains }),
       new InjectedConnector({ chains, options: { shimDisconnect: true } }),
       new CoinbaseWalletConnector({ chains, options: { appName } })
     ],
