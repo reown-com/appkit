c1c5a73b2e53257f9aa6104defa190305f58816a
Ilja <idaderko@gmail.com>
2023-09-20 14:38:59 +0300

    another one

--- packages/ui/src/layout/wui-overlay/styles.ts
+++ packages/ui/src/layout/wui-overlay/styles.ts
@@ -4,7 +4,7 @@ export default css`
   :host {
     overflow-x: hidden;
     overflow-y: auto;
-    background-color: rgba(0, 0, 0, 0.3);
+    background-color: var(--wui-cover);
     display: flex;
     align-items: center;
     justify-content: center;
