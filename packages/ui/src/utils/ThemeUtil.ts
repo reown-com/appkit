af68d472972a4dbcfd731a4e0c7a8e88af6307c6
Ilja <idaderko@gmail.com>
2023-09-20 13:53:05 +0300

    fix flicker on ios overlay

--- packages/ui/src/utils/ThemeUtil.ts
+++ packages/ui/src/utils/ThemeUtil.ts
@@ -545,7 +545,6 @@ export const resetStyles = css`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     -webkit-tap-highlight-color: transparent;
-    backface-visibility: hidden;
     font-family: var(--wui-font-family);
   }
 `
