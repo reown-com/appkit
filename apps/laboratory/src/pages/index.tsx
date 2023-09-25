f75e0620c40560a1ae767567ea47ac0e78b65942
Ilja <idaderko@gmail.com>
2023-09-25 17:11:01 +0300

    rebump vercel build

--- apps/laboratory/src/pages/index.tsx
+++ apps/laboratory/src/pages/index.tsx
@@ -45,7 +45,7 @@ export const wagmiConfig = defaultWagmiConfig({
   projectId,
   metadata: {
     name: 'Web3Modal',
-    description: 'Web3Modal Example',
+    description: 'Web3Modal Laboratory',
     url: 'https://web3modal.com',
     icons: ['https://avatars.githubusercontent.com/u/37784886']
   }
