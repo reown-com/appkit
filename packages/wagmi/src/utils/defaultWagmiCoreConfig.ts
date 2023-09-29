175635ff500bd38b792c7c4a3fcae5308edb30f3
0xAsimetriq <idaderko@gmail.com>
2023-09-29 09:10:57 +0300

    update html example

--- packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
+++ packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
@@ -6,6 +6,7 @@ import { CoinbaseWalletConnector } from '@wagmi/core/connectors/coinbaseWallet'
 import { InjectedConnector } from '@wagmi/core/connectors/injected'
 import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
 import { publicProvider } from '@wagmi/core/providers/public'
+import { EIP6963Connector } from '../connectors/EIP6963Connector.js'
 import { walletConnectProvider } from './provider.js'
 
 export interface ConfigOptions {
@@ -30,6 +31,7 @@ export function defaultWagmiConfig({ projectId, chains, metadata }: ConfigOption
     autoConnect: true,
     connectors: [
       new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+      new EIP6963Connector({ chains }),
       new InjectedConnector({ chains, options: { shimDisconnect: true } }),
       new CoinbaseWalletConnector({ chains, options: { appName: metadata?.name ?? 'Unknown' } })
     ],
