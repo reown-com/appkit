91785c4e868338b5e03c14bfea0fe8bc2655c83c
Sven <fr.sven.fr@gmail.com>
2023-10-06 12:00:11 +0200

    fix type

--- packages/ethers-5/src/utils/defaultEthersCoreConfig.ts
+++ packages/ethers-5/src/utils/defaultEthersCoreConfig.ts
@@ -2,7 +2,7 @@ import '@web3modal/polyfills'
 import { EthereumProvider } from '@walletconnect/ethereum-provider'
 import { ethers } from 'ethers'
 import type { EthereumProviderOptions } from 'node_modules/@walletconnect/ethereum-provider/dist/types/EthereumProvider.js'
-import type { ProviderType } from './types.js'
+import type { ExternalProvider, ProviderType } from './types.js'
 import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk'
 
 type ArrayOneOrMore<T> = {
@@ -33,7 +33,10 @@ export async function defaultEthersConfig({ projectId, chains, optionalChains }:
     darkMode: false
   })
 
-  const coinbaseProvider = coinbaseWallet.makeWeb3Provider('https://cloudflare-eth.com', 1)
+  const coinbaseProvider = coinbaseWallet.makeWeb3Provider(
+    'https://cloudflare-eth.com',
+    1
+  ) as unknown as ExternalProvider
 
   providers.coinbase = new ethers.providers.Web3Provider(coinbaseProvider, 'any')
 
