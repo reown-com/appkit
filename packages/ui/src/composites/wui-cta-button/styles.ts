d1f01975533a25c18cf60015996884d8d56b09f5
0xAsimetriq <idaderko@gmail.com>
2023-10-09 13:18:09 +0300

    wui-cta-button component

---
+++ packages/ui/src/composites/wui-cta-button/styles.ts
@@ -0,0 +1,9 @@
+import { css } from 'lit'
+
+export default css`
+  wui-flex {
+    width: 100%;
+    background-color: var(--wui-gray-glass-002);
+    border-radius: var(--wui-border-radius-xs);
+  }
+`
