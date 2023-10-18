833004b8296e2fe525a0289bfcb0aa85edb36346
Sven <fr.sven.fr@gmail.com>
2023-10-18 18:31:01 +0200

    new canary

--- packages/ethers-5/exports/index.ts
+++ packages/ethers-5/exports/index.ts
@@ -3,7 +3,6 @@ import { Web3Modal } from '../src/client.js'
 import { ConstantsUtil } from '@web3modal/utils'
 
 export type { Web3ModalOptions } from '../src/client.js'
-
 export { defaultConfig } from '../src/utils/defaultConfig.js'
 
 export function createWeb3Modal(options: Web3ModalOptions) {
