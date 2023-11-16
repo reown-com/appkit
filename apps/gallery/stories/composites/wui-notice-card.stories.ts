fb614bd565edf0652da4ffb1ecd04ace1cd76100
0xAsimetriq <idaderko@gmail.com>
2023-11-16 12:11:54 +0200

    wui-notice-card component

--- apps/gallery/stories/composites/wui-notice-card.stories.ts
+++ apps/gallery/stories/composites/wui-notice-card.stories.ts
@@ -3,19 +3,33 @@ import '@web3modal/ui/src/composites/wui-notice-card'
 import type { WuiNoticeCard } from '@web3modal/ui/src/composites/wui-notice-card'
 import { html } from 'lit'
 import '../../components/gallery-container'
+import { iconOptions } from '../../utils/PresetUtils'
 
 type Component = Meta<WuiNoticeCard>
 
 export default {
   title: 'Composites/wui-notice-card',
-  args: {},
-  argTypes: {}
+  args: {
+    label: 'Enjoy all your wallet potential',
+    description: 'Switch to a Non Custodial Wallet in a minute',
+    icon: 'wallet'
+  },
+  argTypes: {
+    icon: {
+      options: iconOptions,
+      control: { type: 'select' }
+    }
+  }
 } as Component
 
 export const Default: Component = {
   render: args => html`
-    <gallery-container width="336">
-      <wui-notice-card></wui-notice-card>
+    <gallery-container width="340">
+      <wui-notice-card
+        label=${args.label}
+        description=${args.description}
+        icon=${args.icon}
+      ></wui-notice-card>
     </gallery-container>
   `
 }
