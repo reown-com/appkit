d740e0f8b9a78592e0f248938120d6c17b96f307
Glitch <glitch.txs@gmail.com>
2023-12-01 01:16:42 -0300

    fix imports

--- packages/connectors/index.ts
+++ packages/connectors/index.ts
@@ -1,3 +1,3 @@
-export type { EthereumHelpers, Web3ModalOptions, Web3ModalClientOptions } from './src/client'
-export { Web3Modal } from './src/client'
-export { defaultConfig, type ConfigOptions } from './src/utils/defaultConfig'
+export type { EthereumHelpers, Web3ModalOptions, Web3ModalClientOptions } from './src/client.js'
+export { Web3Modal } from './src/client.js'
+export { defaultConfig, type ConfigOptions } from './src/utils/defaultConfig.js'
