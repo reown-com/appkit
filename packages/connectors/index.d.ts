e87b73666f18618e9d98736f10498ca2f82ddd3c
Glitch <glitch.txs@gmail.com>
2023-12-01 00:58:51 -0300

    remove ts files

--- packages/connectors/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { EthereumHelpers, Web3ModalOptions, Web3ModalClientOptions } from './src/client';
-export { Web3Modal } from './src/client';
-export { defaultConfig, type ConfigOptions } from './src/utils/defaultConfig';
--- packages/connectors/index.js
+++
@@ -1,3 +0,0 @@
-export { Web3Modal } from './src/client';
-export { defaultConfig } from './src/utils/defaultConfig';
-//# sourceMappingURL=index.js.map
