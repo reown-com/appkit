d79344cae142301bfe8f13a3f86b198ff8c86817
tomiir <rocchitomas@gmail.com>
2023-12-20 08:34:29 -0600

    feat: rename wagmi -> wagmi1

--- packages/wagmi1/src/utils/defaultWagmiReactConfig.ts
+++ packages/wagmi1/src/utils/defaultWagmiReactConfig.ts
@@ -1,6 +1,6 @@
 import '@web3modal/polyfills'
 
-import { configureChains, createConfig } from 'wagmi'
+import { configureChains, createConfig, type Config } from 'wagmi'
 import type { Connector } from 'wagmi'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
@@ -20,7 +20,7 @@ export function defaultWagmiConfig({
   enableEIP6963,
   enableEmail,
   enableWalletConnect
-}: ConfigOptions) {
+}: ConfigOptions): Config {
   const { publicClient } = configureChains(chains, [
     walletConnectProvider({ projectId }),
     publicProvider()
@@ -58,5 +58,5 @@ export function defaultWagmiConfig({
     autoConnect: true,
     connectors,
     publicClient
-  })
+  }) as Config
 }
