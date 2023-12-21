67eb87f5fd3a286e7d9187264b300e518ee0dafb
Derek <alexanderderekrein@gmail.com>
2023-12-21 11:53:56 -0500

    fix: localhost

--- apps/laboratory/tests/shared/constants/index.ts
+++ apps/laboratory/tests/shared/constants/index.ts
@@ -2,6 +2,7 @@ import type { SessionParams } from '../types'
 
 // Allow localhost
 export const BASE_URL = 'http://localhost:3000/library/'
+// Allow localhost
 export const LOCAL_LABS_URL = `${BASE_URL}wagmi/`
 export const WALLET_URL = 'https://react-wallet.walletconnect.com/'
 export const DEFAULT_SESSION_PARAMS: SessionParams = {
