08f3909d89a5e44d20ed4251379418083182ae83
Derek <alexanderderekrein@gmail.com>
2023-12-21 14:37:23 -0500

    feat(ui-tests): cover ethers (#1646)

--- apps/laboratory/tests/shared/fixtures/w3m-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-fixture.ts
@@ -3,15 +3,17 @@ import { ModalPage } from '../pages/ModalPage'
 import { ModalValidator } from '../validators/ModalValidator'
 
 // Declare the types of fixtures to use
-interface ModalFixture {
+export interface ModalFixture {
   modalPage: ModalPage
   modalValidator: ModalValidator
+  library: string
 }
 
 // M -> test Modal
 export const testM = base.extend<ModalFixture>({
-  modalPage: async ({ page }, use) => {
-    const modalPage = new ModalPage(page)
+  library: ['wagmi', { option: true }],
+  modalPage: async ({ page, library }, use) => {
+    const modalPage = new ModalPage(page, library)
     await modalPage.load()
     await use(modalPage)
   },
--- apps/laboratory/tests/shared/pages/ModalPage.ts
+++ apps/laboratory/tests/shared/pages/ModalPage.ts
@@ -1,21 +1,24 @@
 import type { Locator, Page } from '@playwright/test'
-import { LOCAL_LABS_URL } from '../constants'
+import { BASE_URL } from '../constants'
 
 export class ModalPage {
-  private readonly baseURL = LOCAL_LABS_URL
+  private readonly baseURL = BASE_URL
 
   private readonly connectButton: Locator
 
-  constructor(public readonly page: Page) {
+  constructor(
+    public readonly page: Page,
+    public readonly library: string
+  ) {
     this.connectButton = this.page.getByText('Connect Wallet')
   }
 
   async load() {
-    await this.page.goto(this.baseURL)
+    await this.page.goto(`${this.baseURL}library/${this.library}/`)
   }
 
   async copyConnectUriToClipboard() {
-    await this.page.goto(this.baseURL)
+    await this.page.goto(`${this.baseURL}library/${this.library}/`)
     await this.connectButton.click()
     await this.page.getByText('WalletConnect').click()
     await this.page.waitForTimeout(2000)
