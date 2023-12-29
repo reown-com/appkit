3820961ffe233900b63d171433528c1a9cd36318
Derek <alexanderderekrein@gmail.com>
2023-12-29 09:22:35 -0500

    feat: speed up

--- apps/laboratory/playwright.config.ts
+++ apps/laboratory/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig<ModalFixture>({
 
   fullyParallel: true,
   retries: process.env['CI'] ? 2 : 0,
-  workers: process.env['CI'] ? 1 : undefined,
+  workers: process.env['CI'] ? 8 : 1,
   reporter: [['list'], ['html']],
 
   expect: {
