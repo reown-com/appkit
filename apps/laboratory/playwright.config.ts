a2eacfde93531736a8459d429f5d37a36a964358
Derek <alexanderderekrein@gmail.com>
2024-01-01 12:36:37 -0500

    fix: format

--- apps/laboratory/playwright.config.ts
+++ apps/laboratory/playwright.config.ts
@@ -9,8 +9,8 @@ export default defineConfig<ModalFixture>({
   testDir: './tests',
 
   fullyParallel: true,
-  retries: process.env['CI'] ? 2 : 0,
-  workers: process.env['CI'] ? 1 : undefined,
+  retries: 0,
+  workers: 1,
   reporter: [['list'], ['html']],
 
   expect: {
