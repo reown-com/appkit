f2e27b26dce2d52ddf9032b68b5574c5797894ff
tomiir <rocchitomas@gmail.com>
2024-01-04 08:28:23 -0600

    fix: set max height to 360px to keep in sync with connection view. Removed scrollbar

--- packages/scaffold/src/views/w3m-networks-view/styles.ts
+++ packages/scaffold/src/views/w3m-networks-view/styles.ts
@@ -2,7 +2,11 @@ import { css } from 'lit'
 
 export default css`
   :host > wui-grid {
-    max-height: 70vh;
+    max-height: 360px;
     overflow: auto;
+  }
+
+  wui-grid::-webkit-scrollbar {
+    display: none;
   }
 `
