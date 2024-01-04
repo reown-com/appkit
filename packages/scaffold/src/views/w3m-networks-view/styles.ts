080d222bbc1f2812d5b031580a9a4beda390738d
tomiir <rocchitomas@gmail.com>
2024-01-04 09:17:09 -0600

    fix: capped networks view grid height to 70vh (#1701)

---
+++ packages/scaffold/src/views/w3m-networks-view/styles.ts
@@ -0,0 +1,12 @@
+import { css } from 'lit'
+
+export default css`
+  :host > wui-grid {
+    max-height: 360px;
+    overflow: auto;
+  }
+
+  wui-grid::-webkit-scrollbar {
+    display: none;
+  }
+`
