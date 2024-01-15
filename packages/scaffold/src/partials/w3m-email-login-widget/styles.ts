ed86d0bb00bf2486df2a05218d11cb90886c1e48
tomiir <rocchitomas@gmail.com>
2024-01-15 15:02:03 -0600

    fix: w3m email widget spacing with alpha banner

--- packages/scaffold/src/partials/w3m-email-login-widget/styles.ts
+++ packages/scaffold/src/partials/w3m-email-login-widget/styles.ts
@@ -1,10 +1,6 @@
 import { css } from 'lit'
 
 export default css`
-  :host {
-    padding: var(--wui-spacing-3xs) 0;
-  }
-
   wui-separator {
     margin: var(--wui-spacing-s) calc(var(--wui-spacing-s) * -1);
     width: calc(100% + var(--wui-spacing-s) * 2);
@@ -39,6 +35,6 @@ export default css`
     padding: 10px 12px 10px 10px;
     border-radius: var(--wui-border-radius-s);
     background: var(--wui-accent-glass-010);
-    margin-bottom: var(--wui-spacing-xxs);
+    margin-bottom: var(--wui-spacing-s);
   }
 `
