ee567023f7fff68b5d245d19b09b15d10fd5a481
tomiir <rocchitomas@gmail.com>
2024-01-24 12:49:00 -0600

    fix: wagmi-siwe params issue

--- apps/laboratory/src/pages/library/wagmi-siwe.tsx
+++ apps/laboratory/src/pages/library/wagmi-siwe.tsx
@@ -15,7 +15,8 @@ const queryClient = new QueryClient()
 export const wagmiConfig = defaultWagmiConfig({
   chains: WagmiConstantsUtil.chains,
   projectId: ConstantsUtil.ProjectId,
-  metadata: ConstantsUtil.Metadata
+  metadata: ConstantsUtil.Metadata,
+  ssr: true
 })
 
 const modal = createWeb3Modal({
@@ -23,8 +24,7 @@ const modal = createWeb3Modal({
   projectId: ConstantsUtil.ProjectId,
   enableAnalytics: true,
   metadata: ConstantsUtil.Metadata,
-  siweConfig,
-  ssr: true
+  siweConfig
 })
 
 ThemeStore.setModal(modal)
