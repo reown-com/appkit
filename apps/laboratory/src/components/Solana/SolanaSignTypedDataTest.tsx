8d3068bb7392de08103e28255b057b8bae14372a
gooxp <gooxpf@gmail.com>
2024-01-29 18:45:27 +0400

    Temporary removed unused types.

--- apps/laboratory/src/components/Solana/SolanaSignTypedDataTest.tsx
+++ apps/laboratory/src/components/Solana/SolanaSignTypedDataTest.tsx
@@ -1,33 +1,9 @@
 import { Button, useToast } from '@chakra-ui/react'
 import { useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/solana/react'
 
-const types: Record<string, object> = {
-  Person: [
-    { name: 'name', type: 'string' },
-    { name: 'wallet', type: 'address' }
-  ],
-  Mail: [
-    { name: 'from', type: 'Person' },
-    { name: 'to', type: 'Person' },
-    { name: 'contents', type: 'string' }
-  ]
-}
-
-const message = {
-  from: {
-    name: 'Cow',
-    wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
-  },
-  to: {
-    name: 'Bob',
-    wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
-  },
-  contents: 'Hello, Bob!'
-} as const
-
 export function SolanaSignTypedDataTest() {
   const toast = useToast()
-  const { address, chainId } = useWeb3ModalAccount()
+  const { address } = useWeb3ModalAccount()
   const { walletProvider } = useWeb3ModalProvider()
 
   async function onSignTypedData() {
