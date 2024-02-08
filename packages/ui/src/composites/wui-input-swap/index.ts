0abd225bd48635a6bdfbc8cc818cdca03731343f
tomiir <rocchitomas@gmail.com>
2024-02-08 15:59:05 -0600

    feat: base swap input bootstrap

---
+++ packages/ui/src/composites/wui-input-swap/index.ts
@@ -0,0 +1,81 @@
+import { html, LitElement } from 'lit'
+import { property } from 'lit/decorators.js'
+import { ifDefined } from 'lit/directives/if-defined.js'
+import { createRef, ref } from 'lit/directives/ref.js'
+import '../../components/wui-icon/index.js'
+import { elementStyles, resetStyles } from '../../utils/ThemeUtil.js'
+import type { IconType, InputType, SizeType } from '../../utils/TypeUtil.js'
+import { customElement } from '../../utils/WebComponentsUtil.js'
+import styles from './styles.js'
+
+@customElement('wui-input-swap')
+export class WuiInputSwap extends LitElement {
+  public static override styles = [resetStyles, elementStyles, styles]
+
+  // -- Members ------------------------------------------- //
+  public inputElementRef = createRef<HTMLInputElement>()
+
+  // -- State & Properties -------------------------------- //
+  @property() public size: Exclude<SizeType, 'inherit' | 'xl' | 'xs' | 'xxs'> = 'md'
+
+  @property() public icon?: IconType
+
+  @property({ type: Boolean }) public disabled = false
+
+  @property() public placeholder = ''
+
+  @property() public type: InputType = 'text'
+
+  @property() public keyHint?: HTMLInputElement['enterKeyHint']
+
+  @property() public value?: string
+
+  // -- Render -------------------------------------------- //
+  public override render() {
+    const sizeClass = `wui-size-${this.size}`
+
+    return html`${this.templateIcon()}
+      <input
+        ${ref(this.inputElementRef)}
+        class=${sizeClass}
+        type=${this.type}
+        enterkeyhint=${ifDefined(this.enterKeyHint)}
+        ?disabled=${this.disabled}
+        placeholder=${this.placeholder}
+        @input=${this.dispatchInputChangeEvent.bind(this)}
+        value=${ifDefined(this.value)}
+        .value=${this.value || ''}
+      />
+      <slot></slot>`
+  }
+
+  // -- Private ------------------------------------------- //
+  private templateIcon() {
+    if (this.icon) {
+      return html`<wui-icon
+        data-input=${this.size}
+        size=${this.size}
+        color="inherit"
+        name=${this.icon}
+      ></wui-icon>`
+    }
+
+    return null
+  }
+
+  private dispatchInputChangeEvent() {
+    this.dispatchEvent(
+      new CustomEvent('inputChange', {
+        detail: this.inputElementRef.value?.value,
+        bubbles: true,
+        composed: true
+      })
+    )
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wui-input-swap': WuiInputSwap
+  }
+}
---
+++ packages/ui/src/composites/wui-input-swap/styles.ts
@@ -0,0 +1,133 @@
+import { css } from 'lit'
+
+export default css`
+  :host {
+    position: relative;
+    width: 100%;
+    display: inline-block;
+    color: var(--wui-color-fg-275);
+  }
+
+  input {
+    width: 100%;
+    border-radius: var(--wui-border-radius-xs);
+    border: 1px solid var(--wui-gray-glass-005);
+    background: var(--wui-gray-glass-005);
+    font-size: var(--wui-font-size-paragraph);
+    font-weight: var(--wui-font-weight-light);
+    letter-spacing: var(--wui-letter-spacing-paragraph);
+    color: var(--wui-color-fg-100);
+    transition: all var(--wui-ease-inout-power-1) var(--wui-duration-lg);
+    caret-color: var(--wui-color-accent-100);
+  }
+
+  input:disabled {
+    cursor: not-allowed;
+    border: 1px solid var(--wui-gray-glass-010);
+  }
+
+  input:disabled::placeholder,
+  input:disabled + wui-icon {
+    color: var(--wui-color-fg-300);
+  }
+
+  input::placeholder {
+    color: var(--wui-color-fg-275);
+  }
+
+  input:focus:enabled {
+    transition: all var(--wui-ease-out-power-2) var(--wui-duration-sm);
+    background-color: var(--wui-gray-glass-010);
+    border: 1px solid var(--wui-color-accent-100);
+    -webkit-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+    -moz-box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+    box-shadow: 0px 0px 0px 4px var(--wui-box-shadow-blue);
+  }
+
+  input:hover:enabled {
+    background-color: var(--wui-gray-glass-010);
+  }
+
+  wui-icon {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    pointer-events: none;
+  }
+
+  .wui-size-sm {
+    padding: 9px var(--wui-spacing-m) 10px var(--wui-spacing-s);
+  }
+
+  wui-icon + .wui-size-sm {
+    padding: 9px var(--wui-spacing-m) 10px 36px;
+  }
+
+  wui-icon[data-input='sm'] {
+    left: var(--wui-spacing-s);
+  }
+
+  .wui-size-md {
+    padding: 15px var(--wui-spacing-m) var(--wui-spacing-l) var(--wui-spacing-m);
+  }
+
+  wui-icon + .wui-size-md,
+  wui-loading-spinner + .wui-size-md {
+    padding: 10.5px var(--wui-spacing-3xl) 10.5px 40px;
+  }
+
+  wui-icon[data-input='md'] {
+    left: var(--wui-spacing-l);
+  }
+
+  .wui-size-lg {
+    padding: var(--wui-spacing-s) var(--wui-spacing-s) var(--wui-spacing-s) var(--wui-spacing-l);
+    letter-spacing: var(--wui-letter-spacing-medium-title);
+    font-size: var(--wui-font-size-medium-title);
+    font-weight: var(--wui-font-weight-light);
+    line-height: 130%;
+    color: var(--wui-color-fg-100);
+    height: 64px;
+  }
+
+  wui-icon + .wui-size-lg,
+  wui-loading-spinner + .wui-size-lg {
+    padding-left: 50px;
+  }
+
+  wui-icon[data-input='lg'] {
+    left: var(--wui-spacing-l);
+  }
+
+  input:placeholder-shown ~ ::slotted(wui-input-element),
+  input:placeholder-shown ~ ::slotted(wui-icon) {
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  ::slotted(wui-input-element),
+  ::slotted(wui-icon) {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    transition: all var(--wui-ease-in-power-2) var(--wui-duration-md);
+  }
+
+  ::slotted(wui-input-element) {
+    right: var(--wui-spacing-m);
+  }
+
+  ::slotted(wui-icon) {
+    right: 0px;
+  }
+`
