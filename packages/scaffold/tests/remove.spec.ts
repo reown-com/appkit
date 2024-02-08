3caf067cbaf0d70c709490f029929631a7533b10
Derek <alexanderderekrein@gmail.com>
2024-02-08 06:27:09 -0500

    fix(ui-test): tests not passing anymore

--- packages/scaffold/tests/remove.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-// -- Tests --------------------------------------------------------------------
-describe('Scaffold', () => {
-  it('does nothing', () => {
-    expect(true).toEqual(true)
-  })
-})
