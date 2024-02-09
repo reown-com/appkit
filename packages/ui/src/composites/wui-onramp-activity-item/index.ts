6cf0d131f0829759eb8bef18a39d06fa405a424d
tomiir <rocchitomas@gmail.com>
2024-02-09 12:36:00 -0600

    fix: activity status icons

--- packages/ui/src/composites/wui-onramp-activity-item/index.ts
+++ packages/ui/src/composites/wui-onramp-activity-item/index.ts
@@ -41,7 +41,7 @@ export class WuiOnRampActivityItem extends LitElement {
         ${this.imageTemplate()}
         <wui-flex flexDirection="column" gap="4xs" flexGrow="1">
           <wui-flex gap="xxs" alignItems="center" justifyContent="flex-start">
-            ${this.completed ? this.boughtIconTemplate() : null}
+            ${this.statusIconTemplate()}
             <wui-text variant="paragraph-500" color="fg-100"> ${this.label}</wui-text>
           </wui-flex>
           <wui-text variant="small-400" color="fg-200">
@@ -56,10 +56,28 @@ export class WuiOnRampActivityItem extends LitElement {
   }
 
   // -- Private ------------------------------------------- //
+  private statusIconTemplate() {
+    if (this.inProgress) {
+      return null
+    }
+
+    return this.completed ? this.boughtIconTemplate() : this.errorIconTemplate()
+  }
+
+  private errorIconTemplate() {
+    return html`<wui-icon-box
+      size="xxs"
+      iconColor="error-100"
+      backgroundColor="error-100"
+      background="opaque"
+      icon="close"
+      borderColor="wui-color-bg-125"
+    ></wui-icon-box>`
+  }
+
   private imageTemplate() {
     return html`<wui-flex class="purchase-image-container">
       <wui-image src=${this.icon}></wui-image>
-      ${this.completed ? null : this.boughtIconTemplate()}
     </wui-flex>`
   }
 
@@ -70,7 +88,6 @@ export class WuiOnRampActivityItem extends LitElement {
       backgroundColor="success-100"
       background="opaque"
       icon="arrowBottom"
-      ?border=${true}
       borderColor="wui-color-bg-125"
     ></wui-icon-box>`
   }
