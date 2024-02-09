9a8a9d069caacf17e50543ac353626f1ecaa210d
tomiir <rocchitomas@gmail.com>
2024-02-09 09:17:36 -0600

    feat: onramp activity styles improvement + types prune (#1887)

--- packages/ui/src/composites/wui-transaction-list-item/index.ts
+++ packages/ui/src/composites/wui-transaction-list-item/index.ts
@@ -28,11 +28,11 @@ export class WuiTransactionListItem extends LitElement {
 
   @property({ type: Array }) public images: TransactionImage[] = []
 
-  @property() public price: TransactionImage[] = []
+  @property({ type: Array }) public price: TransactionImage[] = []
 
-  @property() public amount: TransactionImage[] = []
+  @property({ type: Array }) public amount: TransactionImage[] = []
 
-  @property() public symbol: TransactionImage[] = []
+  @property({ type: Array }) public symbol: TransactionImage[] = []
 
   // -- Render -------------------------------------------- //
   public override render() {
