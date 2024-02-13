2308a530e98400533849c9d8c094b3a57a380dc9
Luka Isailovic <lukaisailovic@gmail.com>
2024-02-13 19:36:22 +0100

    feat: reduce the amout of workers and change runners

--- apps/laboratory/playwright.config.ts
+++ apps/laboratory/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig<ModalFixture>({
 
   fullyParallel: true,
   retries: 2,
-  workers: 6,
+  workers: 3,
   reporter: process.env['CI']
     ? [['list'], ['html', { open: 'never' }]]
     : [['list'], ['html', { host: '0.0.0.0' }]],
