899ff34e171751862887bc370a095be8b56a8e08
Luka Isailovic <lukaisailovic@gmail.com>
2024-02-14 09:44:02 +0100

    fix: lint

--- apps/laboratory/tests/shared/utils/device.ts
+++ apps/laboratory/tests/shared/utils/device.ts
@@ -2,7 +2,9 @@ import { DEVICES } from '../constants/devices'
 
 export function getAvailableDevices(): string[] {
   if (!process.env['CI']) {
+
     return DEVICES
   }
+  
   return DEVICES.filter(d => d !== 'Desktop Safari')
 }
