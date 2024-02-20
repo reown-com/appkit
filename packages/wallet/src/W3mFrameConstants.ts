d974ad570ed42125bd2ffe62094a19484086a939
tomiir <rocchitomas@gmail.com>
2024-02-20 13:13:16 -0600

    fix: secure site url

--- packages/wallet/src/W3mFrameConstants.ts
+++ packages/wallet/src/W3mFrameConstants.ts
@@ -1,5 +1,5 @@
 export const W3mFrameConstants = {
-  SECURE_SITE_SDK: '"https://secure.walletconnect.com/sdk',
+  SECURE_SITE_SDK: 'https://secure.walletconnect.com/sdk',
   APP_EVENT_KEY: '@w3m-app/',
   FRAME_EVENT_KEY: '@w3m-frame/',
   RPC_METHOD_KEY: 'RPC_',
