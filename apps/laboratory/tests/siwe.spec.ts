012242e7f0ae8de75f60439dc85e52da56518dc3
Luka Isailovic <lukaisailovic@gmail.com>
2024-02-21 15:11:56 +0100

    fix: revert deeplinking on siwe

--- apps/laboratory/tests/siwe.spec.ts
+++ apps/laboratory/tests/siwe.spec.ts
@@ -1,35 +1,30 @@
 import { DEFAULT_SESSION_PARAMS } from './shared/constants'
 import { testMWSiwe } from './shared/fixtures/w3m-wallet-fixture'
-import { expectConnection } from './shared/utils/validation'
 
-testMWSiwe.beforeEach(async ({ walletValidator, modalValidator, walletPage, modalPage }) => {
+testMWSiwe.beforeEach(async ({ modalPage, walletPage }) => {
   const uri = await modalPage.getConnectUri()
   await walletPage.connectWithUri(uri)
   await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
-  await expectConnection(modalValidator, walletValidator)
 })
 
-testMWSiwe.afterEach(async ({ modalValidator, walletValidator }) => {
+testMWSiwe.afterEach(async ({ modalValidator, walletValidator, browserName }) => {
+  // Webkit cannot use clipboard.
+  if (browserName === 'webkit') {
+    return
+  }
   await modalValidator.expectDisconnected()
   await walletValidator.expectDisconnected()
 })
 
 testMWSiwe(
   'it should sign in with ethereum',
-  async ({ modalPage, walletPage, modalValidator, walletValidator, browserName }) => {
-    // Webkit cannot use clipboard.
-    if (browserName === 'webkit') {
-      testMWSiwe.skip()
-
-      return
-    }
+  async ({ modalPage, walletPage, modalValidator, walletValidator }) => {
     await modalPage.promptSiwe()
     await walletValidator.expectReceivedSign({})
     await walletPage.handleRequest({ accept: true })
     await modalValidator.expectAuthenticated()
-    // If we don't wait here, we are in disconnected state TODO: check
-    await walletPage.page.waitForTimeout(1000)
-    await walletValidator.expectSessionCard()
+    await modalValidator.expectConnected()
+    await walletValidator.expectConnected()
     await modalPage.disconnect()
   }
 )
