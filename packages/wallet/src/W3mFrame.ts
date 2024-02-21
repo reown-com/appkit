b44a3f8b301b0792ef95456b81ce2dbdd239a68f
tomiir <rocchitomas@gmail.com>
2024-02-21 11:53:17 -0600

    wip: use origin

--- packages/wallet/src/W3mFrame.ts
+++ packages/wallet/src/W3mFrame.ts
@@ -44,7 +44,7 @@ export class W3mFrame {
 
           // Before appending, get the url and fetch the headers
           const url = new URL(iframe.src)
-          fetch(url, { method: 'HEAD' })
+          fetch(url.origin, { method: 'HEAD' })
             .then(response => {
               console.log('fetch response', response)
               console.log('fetch response headers', response.headers)
