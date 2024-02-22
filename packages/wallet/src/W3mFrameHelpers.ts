f6c81f6f5dd7ac3bedfad64fc736394434143e8f
Sven <38101365+svenvoskamp@users.noreply.github.com>
2024-02-22 12:01:03 +0100

    hotfix: better handling for rpc request  (#1958)

--- packages/wallet/src/W3mFrameHelpers.ts
+++ packages/wallet/src/W3mFrameHelpers.ts
@@ -54,6 +54,15 @@ export const W3mFrameHelpers = {
     return 0
   },
 
+  checkIfRequestExists(request: unknown) {
+    const method = (request as { payload: W3mFrameTypes.RPCRequest })?.payload?.method
+
+    return (
+      W3mFrameRpcConstants.NOT_SAFE_RPC_METHODS.includes(method) ||
+      W3mFrameRpcConstants.SAFE_RPC_METHODS.includes(method)
+    )
+  },
+
   checkIfRequestIsAllowed(request: unknown) {
     const method = (request as { payload: W3mFrameTypes.RPCRequest })?.payload?.method
 
