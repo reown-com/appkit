65efc6e6390a718c5658e5aa4ae1d0ef362e7624
Glitch <glitch.txs@gmail.com>
2024-02-27 21:40:33 -0300

    goback

--- packages/siwe/exports/index.ts
+++ packages/siwe/exports/index.ts
@@ -1,14 +1,21 @@
-import type { SIWEConfig } from '../core/utils/TypeUtils.js'
-import { Web3ModalSIWEClient } from '../src/client.js'
-export { SIWEController, type SIWEControllerClient } from '../core/controller/SIWEController.js'
-
-export type {
+import type {
   SIWEConfig,
   SIWESession,
   SIWECreateMessageArgs,
   SIWEVerifyMessageArgs,
   SIWEClientMethods
 } from '../core/utils/TypeUtils.js'
+import { Web3ModalSIWEClient } from '../src/client.js'
+export { SIWEController, type SIWEControllerClient } from '../core/controller/SIWEController.js'
+
+export type {
+  Web3ModalSIWEClient,
+  SIWEConfig,
+  SIWESession,
+  SIWECreateMessageArgs,
+  SIWEVerifyMessageArgs,
+  SIWEClientMethods
+}
 
 export function createSIWEConfig(siweConfig: SIWEConfig) {
   return new Web3ModalSIWEClient(siweConfig)
