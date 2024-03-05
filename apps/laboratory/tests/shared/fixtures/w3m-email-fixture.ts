82a090e82cea1ad6938b7c11dcf023d768d7948f
Luka Isailovic <lukaisailovic@gmail.com>
2024-03-05 14:44:39 +0100

    fix: move MAILSAC_API_KEY check inside the initialization logic

--- apps/laboratory/tests/shared/fixtures/w3m-email-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-email-fixture.ts
@@ -5,17 +5,16 @@ import { ModalValidator } from '../validators/ModalValidator'
 import { DeviceRegistrationPage } from '../pages/DeviceRegistrationPage'
 import { Email } from '../utils/email'
 
-const mailsacApiKey = process.env['MAILSAC_API_KEY']
-if (!mailsacApiKey) {
-  throw new Error('MAILSAC_API_KEY is not set')
-}
-
 export const testMEmail = base.extend<ModalFixture>({
   library: ['wagmi', { option: true }],
   modalPage: async ({ page, library, context }, use, testInfo) => {
     const modalPage = new ModalPage(page, library, 'email')
     await modalPage.load()
 
+    const mailsacApiKey = process.env['MAILSAC_API_KEY']
+    if (!mailsacApiKey) {
+      throw new Error('MAILSAC_API_KEY is not set')
+    }
     const email = new Email(mailsacApiKey)
 
     const tempEmail = email.getEmailAddressToUse(testInfo.parallelIndex)
