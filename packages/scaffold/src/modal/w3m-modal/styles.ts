306eb38e3900135b23944a92849d1a8760839d18
enesozturk <enesozturk.d@gmail.com>
2024-03-25 18:04:12 +0300

    chore: remove css animation delay

--- packages/scaffold/src/modal/w3m-modal/styles.ts
+++ packages/scaffold/src/modal/w3m-modal/styles.ts
@@ -38,7 +38,6 @@ export default css`
     max-width: 360px;
     width: 100%;
     position: relative;
-    animation-delay: 0.3s;
     animation-duration: 0.2s;
     animation-name: zoom-in;
     animation-fill-mode: backwards;
