c5bc11ecb1d9534a8b965e606555cdae6774038b
Chris Smith <chris@walletconnect.com>
2024-03-27 09:31:31 -0400

    fix: lint

--- apps/laboratory/tests/canary.spec.ts
+++ apps/laboratory/tests/canary.spec.ts
@@ -6,8 +6,10 @@ const ENV = process.env['ENVIRONMENT'] || 'dev'
 const REGION = process.env['REGION'] || 'eu-central-1'
 
 testConnectedMW.beforeEach(async ({ modalValidator, walletValidator }) => {
+  // eslint-disable-next-line no-console
   console.time('beforeEach expectConnection')
   await expectConnection(modalValidator, walletValidator)
+  // eslint-disable-next-line no-console
   console.timeEnd('beforeEach expectConnection')
 })
 
@@ -17,6 +19,7 @@ testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
   }
 
   if (ENV !== 'dev') {
+    // eslint-disable-next-line no-console
     console.time('uploadCanaryResultsToCloudWatch')
     const duration: number = testInfo.duration
     await uploadCanaryResultsToCloudWatch(
@@ -27,6 +30,7 @@ testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
       testInfo.status === 'passed',
       duration
     )
+    // eslint-disable-next-line no-console
     console.timeEnd('uploadCanaryResultsToCloudWatch')
   }
 })
@@ -34,26 +38,40 @@ testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
 testConnectedMW(
   'it should sign',
   async ({ modalPage, walletPage, modalValidator, walletValidator }) => {
+    // eslint-disable-next-line no-console
     console.time('modalPage.sign()')
     await modalPage.sign()
+    // eslint-disable-next-line no-console
     console.timeEnd('modalPage.sign()')
+    // eslint-disable-next-line no-console
     console.time('walletValidator.expectReceivedSign')
     await walletValidator.expectReceivedSign({})
+    // eslint-disable-next-line no-console
     console.timeEnd('walletValidator.expectReceivedSign')
+    // eslint-disable-next-line no-console
     console.time('walletPage.handleRequest')
     await walletPage.handleRequest({ accept: true })
+    // eslint-disable-next-line no-console
     console.timeEnd('walletPage.handleRequest')
+    // eslint-disable-next-line no-console
     console.time('modalValidator.expectAcceptedSign')
     await modalValidator.expectAcceptedSign()
+    // eslint-disable-next-line no-console
     console.timeEnd('modalValidator.expectAcceptedSign')
+    // eslint-disable-next-line no-console
     console.time('modalPage.disconnect')
     await modalPage.disconnect()
+    // eslint-disable-next-line no-console
     console.timeEnd('modalPage.disconnect')
+    // eslint-disable-next-line no-console
     console.time('modalValidator.expectDisconnected')
     await modalValidator.expectDisconnected()
+    // eslint-disable-next-line no-console
     console.timeEnd('modalValidator.expectDisconnected')
+    // eslint-disable-next-line no-console
     console.time('walletValidator.expectDisconnected')
     await walletValidator.expectDisconnected()
+    // eslint-disable-next-line no-console
     console.timeEnd('walletValidator.expectDisconnected')
   }
 )
