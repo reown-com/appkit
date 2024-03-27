d43bedda92b6d079140579bec8b4a6f7debd082c
Derek <alexanderderekrein@gmail.com>
2024-03-27 17:22:14 +0900

    feat(canary): use playwright for duration calculation

--- apps/laboratory/tests/canary.spec.ts
+++ apps/laboratory/tests/canary.spec.ts
@@ -5,23 +5,26 @@ import { expectConnection } from './shared/utils/validation'
 const ENV = process.env['ENVIRONMENT'] || 'dev'
 const REGION = process.env['REGION'] || 'eu-central-1'
 
-let startTime = 0
-
 testConnectedMW.beforeEach(async ({ modalValidator, walletValidator }) => {
-  // Give us extra time in a potentially slow canary deployment
-  testConnectedMW.setTimeout(120_000)
-
-  startTime = Date.now()
   await expectConnection(modalValidator, walletValidator)
 })
 
-testConnectedMW.afterEach(async ({ modalPage, modalValidator, walletValidator, browserName }) => {
+testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
   if (browserName === 'firefox') {
     return
   }
-  await modalPage.disconnect()
-  await modalValidator.expectDisconnected()
-  await walletValidator.expectDisconnected()
+  
+  if (ENV !== 'dev') {
+    const duration: number = testInfo.duration
+    await uploadCanaryResultsToCloudWatch(
+      ENV,
+      REGION,
+      'https://lab.web3modal.com/',
+      'HappyPath.sign',
+      true,
+      duration
+    )
+  }
 })
 
 testConnectedMW(
@@ -31,17 +34,8 @@ testConnectedMW(
     await walletValidator.expectReceivedSign({})
     await walletPage.handleRequest({ accept: true })
     await modalValidator.expectAcceptedSign()
-
-    if (ENV !== 'dev') {
-      const duration: number = Date.now() - startTime
-      await uploadCanaryResultsToCloudWatch(
-        ENV,
-        REGION,
-        'https://lab.web3modal.com/',
-        'HappyPath.sign',
-        true,
-        duration
-      )
-    }
+    await modalPage.disconnect()
+    await modalValidator.expectDisconnected()
+    await walletValidator.expectDisconnected()
   }
 )
