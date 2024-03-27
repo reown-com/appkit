2bde00607782fec5226410fcd48275825b67fdf0
Chris Smith <chris@walletconnect.com>
2024-03-27 09:16:29 -0400

    chore: log canary times

--- apps/laboratory/tests/canary.spec.ts
+++ apps/laboratory/tests/canary.spec.ts
@@ -6,7 +6,9 @@ const ENV = process.env['ENVIRONMENT'] || 'dev'
 const REGION = process.env['REGION'] || 'eu-central-1'
 
 testConnectedMW.beforeEach(async ({ modalValidator, walletValidator }) => {
+  console.time("beforeEach expectConnection")
   await expectConnection(modalValidator, walletValidator)
+  console.timeEnd("beforeEach expectConnection")
 })
 
 testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
@@ -15,6 +17,7 @@ testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
   }
 
   if (ENV !== 'dev') {
+    console.time("uploadCanaryResultsToCloudWatch")
     const duration: number = testInfo.duration
     await uploadCanaryResultsToCloudWatch(
       ENV,
@@ -24,18 +27,33 @@ testConnectedMW.afterEach(async ({ browserName }, testInfo) => {
       testInfo.status === 'passed',
       duration
     )
+    console.timeEnd("uploadCanaryResultsToCloudWatch")
   }
 })
 
 testConnectedMW(
   'it should sign',
   async ({ modalPage, walletPage, modalValidator, walletValidator }) => {
+    console.time("modalPage.sign()")
     await modalPage.sign()
+    console.timeEnd("modalPage.sign()")
+    console.time("walletValidator.expectReceivedSign")
     await walletValidator.expectReceivedSign({})
+    console.timeEnd("walletValidator.expectReceivedSign")
+    console.time("walletPage.handleRequest")
     await walletPage.handleRequest({ accept: true })
+    console.timeEnd("walletPage.handleRequest")
+    console.time("modalValidator.expectAcceptedSign")
     await modalValidator.expectAcceptedSign()
+    console.timeEnd("modalValidator.expectAcceptedSign")
+    console.time("modalPage.disconnect")
     await modalPage.disconnect()
+    console.timeEnd("modalPage.disconnect")
+    console.time("modalValidator.expectDisconnected")
     await modalValidator.expectDisconnected()
+    console.timeEnd("modalValidator.expectDisconnected")
+    console.time("walletValidator.expectDisconnected")
     await walletValidator.expectDisconnected()
+    console.timeEnd("walletValidator.expectDisconnected")
   }
 )
