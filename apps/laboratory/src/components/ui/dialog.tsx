9c028e83ed6ed8c425471837a598f0346dc91542
enesozturk <enesozturk.d@gmail.com>
2024-04-02 22:54:36 +0300

    chore: group class names

--- apps/laboratory/src/components/ui/dialog.tsx
+++ apps/laboratory/src/components/ui/dialog.tsx
@@ -47,7 +47,15 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close
+        className={cn(
+          'absolute right-4 top-4',
+          'rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100',
+          'disabled:pointer-events-none',
+          'focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+          'data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'
+        )}
+      >
         <Cross1Icon className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
