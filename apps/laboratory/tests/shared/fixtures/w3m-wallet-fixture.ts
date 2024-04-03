d3627c23cd03cba97e2a3c4ada4612e9f64cab07
Luka Isailovic <lukaisailovic@gmail.com>
2024-04-03 16:00:35 +0200

    chore: add conditional logs for canary (#2110)

--- apps/laboratory/tests/shared/fixtures/w3m-wallet-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-wallet-fixture.ts
@@ -6,6 +6,7 @@ import { WalletValidator } from '../validators/WalletValidator'
 
 import { DEFAULT_SESSION_PARAMS } from '../constants'
 import { doActionAndWaitForNewPage } from '../utils/actions'
+import { timeEnd, timeStart } from '../utils/logs'
 
 // Declare the types of fixtures to use
 interface ModalWalletFixture {
@@ -18,45 +19,45 @@ export const testConnectedMW = base.extend<ModalWalletFixture>({
   walletPage: async ({ context, modalPage }, use) => {
     if (modalPage.library === 'solana') {
       // Because solana doesn't support react-wallet-v2
-      console.time('new WalletPage')
+      timeStart('new WalletPage')
       const walletPage = new WalletPage(await context.newPage())
-      console.timeEnd('new WalletPage')
-      console.time('walletPage.load')
+      timeEnd('new WalletPage')
+      timeStart('walletPage.load')
       await walletPage.load()
-      console.timeEnd('walletPage.load')
-      console.time('walletPage.connectWithUri')
+      timeEnd('walletPage.load')
+      timeStart('walletPage.connectWithUri')
       const walletValidator = new WalletValidator(walletPage.page)
-      console.timeEnd('walletPage.connectWithUri')
-      console.time('modalPage.getConnectUri')
+      timeEnd('walletPage.connectWithUri')
+      timeStart('modalPage.getConnectUri')
       const uri = await modalPage.getConnectUri()
-      console.timeEnd('modalPage.getConnectUri')
-      console.time('walletPage.connectWithUri')
+      timeEnd('modalPage.getConnectUri')
+      timeStart('walletPage.connectWithUri')
       await walletPage.connectWithUri(uri)
-      console.timeEnd('walletPage.connectWithUri')
-      console.time('walletPage.handleSessionProposal')
+      timeEnd('walletPage.connectWithUri')
+      timeStart('walletPage.handleSessionProposal')
       await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
-      console.timeEnd('walletPage.handleSessionProposal')
-      console.time('walletValidator.expectConnected')
+      timeEnd('walletPage.handleSessionProposal')
+      timeStart('walletValidator.expectConnected')
       await walletValidator.expectConnected()
-      console.timeEnd('walletValidator.expectConnected')
+      timeEnd('walletValidator.expectConnected')
       await use(walletPage)
     } else {
-      console.time('doActionAndWaitForNewPage')
+      timeStart('doActionAndWaitForNewPage')
       const page = await doActionAndWaitForNewPage(modalPage.clickWalletDeeplink(), context)
-      console.timeEnd('doActionAndWaitForNewPage')
-      console.time('new WalletPage')
+      timeEnd('doActionAndWaitForNewPage')
+      timeStart('new WalletPage')
       const walletPage = new WalletPage(page)
-      console.timeEnd('new WalletPage')
-      console.time('walletPage.handleSessionProposal')
+      timeEnd('new WalletPage')
+      timeStart('walletPage.handleSessionProposal')
       await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
-      console.timeEnd('walletPage.handleSessionProposal')
+      timeEnd('walletPage.handleSessionProposal')
       await use(walletPage)
     }
   },
   walletValidator: async ({ walletPage }, use) => {
-    console.time('new WalletValidator')
+    timeStart('new WalletValidator')
     const walletValidator = new WalletValidator(walletPage.page)
-    console.timeEnd('new WalletValidator')
+    timeEnd('new WalletValidator')
     await use(walletValidator)
   }
 })
