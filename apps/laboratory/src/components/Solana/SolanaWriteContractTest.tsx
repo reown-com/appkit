12fe48790ac9a48a33d3c93ac980c980f23e312b
gooxp <gooxpf@gmail.com>
2024-04-18 10:55:45 +0400

    Remove unnecessary comment

--- apps/laboratory/src/components/Solana/SolanaWriteContractTest.tsx
+++ apps/laboratory/src/components/Solana/SolanaWriteContractTest.tsx
@@ -24,7 +24,6 @@ export function SolanaWriteContractTest() {
     setLoading(true)
 
     const PROGRAM_ID = new PublicKey(detectProgramId(currentChain?.chainId ?? ''))
-    /* Add your program id */
 
     try {
       if (!walletProvider || !address) {
