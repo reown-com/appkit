b6ea718476aac9fdca147a47e00fdac2ad8f3773
Chris Smith <chris@walletconnect.com>
2024-04-19 13:35:59 -0400

    chore: comment

--- apps/laboratory/docker-canary.sh
+++ apps/laboratory/docker-canary.sh
@@ -1,5 +1,7 @@
 #!/usr/env/bin
 
+# Not adding `set -e` so that S3 upload happens regardless
+
 npm run playwright:canary:test
 
 destination="s3://$TEST_RESULTS_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/"
