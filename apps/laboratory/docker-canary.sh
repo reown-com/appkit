73e4027c3449499375f23fafb6c7d53b8419b159
Chris Smith <chris@walletconnect.com>
2024-04-19 13:30:29 -0400

    fix: env var name

--- apps/laboratory/docker-canary.sh
+++ apps/laboratory/docker-canary.sh
@@ -1,4 +1,7 @@
 #!/usr/env/bin
 
 npm run playwright:canary:test
-aws s3 cp ./test-results/ s3://$S3_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/ --recursive
+
+destination="s3://$TEST_RESULTS_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/"
+echo "Uploading test results to $destination"
+aws s3 cp ./test-results/ $destination --recursive
