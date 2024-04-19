cc1638dcf5ff5a38fa6bb643e83b5b282e2ad3d0
enesozturk <enesozturk.d@gmail.com>
2024-04-20 00:41:56 +0300

    chore: upgrade vitest, refactor test scripts, fix public state controller tests

--- packages/core/tests/controllers/PublicStateController.test.ts
+++ packages/core/tests/controllers/PublicStateController.test.ts
@@ -13,11 +13,13 @@ describe('PublicStateController', () => {
   it('should update state correctly on set()', () => {
     PublicStateController.set({ open: true })
     expect(PublicStateController.state).toEqual({
+      loading: false,
       open: true,
       selectedNetworkId: undefined
     })
     PublicStateController.set({ selectedNetworkId: 'eip155:1' })
     expect(PublicStateController.state).toEqual({
+      loading: false,
       open: true,
       selectedNetworkId: 'eip155:1'
     })
