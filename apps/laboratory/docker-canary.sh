58c4d19b5dcf2f4e4ea473679eb947ae61d3cccc
Chris Smith <1979423+chris13524@users.noreply.github.com>
2024-04-23 12:47:32 -0400

    chore: don't use /usr/bin/env (#2183)

--- apps/laboratory/docker-canary.sh
+++ apps/laboratory/docker-canary.sh
@@ -1,8 +1,6 @@
-#!/usr/env/bin
-
 # Not adding `set -e` so that S3 upload happens regardless
 
-npm run playwright:canary:test
+npm run playwright:test:canary
 
 destination="s3://$TEST_RESULTS_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/"
 echo "Uploading test results to $destination"
