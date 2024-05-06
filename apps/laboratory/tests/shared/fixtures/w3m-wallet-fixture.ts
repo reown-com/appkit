fffaa0125f40adb5cd5988d2c0ea2410e632de78
Chris Smith <chris@walletconnect.com>
2024-05-06 20:12:28 +0100

    chore: more canary timings

--- apps/laboratory/tests/shared/fixtures/w3m-wallet-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-wallet-fixture.ts
@@ -50,13 +50,48 @@ export const testConnectedMW = base.extend<ModalWalletFixture>({
     } else {
       timeStart('doActionAndWaitForNewPage')
       const page = await doActionAndWaitForNewPage(modalPage.clickWalletDeeplink(), context)
+      let pairingCreatedTime: Date | null = null
+      let verificationStartedTime: Date | null = null
+      page.on('console', msg => {
+        if (msg.text().includes('set') && msg.text().includes('core/pairing/pairing')) {
+          pairingCreatedTime = new Date()
+        }
+        if (msg.text().includes('resolving attestation')) {
+          verificationStartedTime = new Date()
+        }
+        if (msg.text().includes('session_proposal') && msg.text().includes('verifyContext')) {
+          // For some reason this log is emitted twice; so only recording the time once
+          if (verificationStartedTime) {
+            const verificationEndedTime = new Date()
+            timingRecords.push({
+              item: 'sessionProposalVerification',
+              timeMs: verificationEndedTime.getTime() - verificationStartedTime.getTime()
+            })
+            verificationStartedTime = null
+          }
+        }
+      })
       timeEnd('doActionAndWaitForNewPage')
+      const connectionInitiated = new Date()
       timeStart('new WalletPage')
       const walletPage = new WalletPage(page)
       timeEnd('new WalletPage')
       timeStart('walletPage.handleSessionProposal')
       await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
       timeEnd('walletPage.handleSessionProposal')
+      const proposalReceived = new Date()
+      timingRecords.push({
+        item: 'deeplinkReceiveSessionProposal',
+        timeMs: proposalReceived.getTime() - connectionInitiated.getTime()
+      })
+      if (pairingCreatedTime) {
+        timingRecords.push({
+          item: 'pairingReceiveSessionProposal',
+          timeMs: proposalReceived.getTime() - (pairingCreatedTime as Date).getTime()
+        })
+      } else {
+        throw new Error('pairingCreatedTime not yet set')
+      }
       await use(walletPage)
     }
   },
