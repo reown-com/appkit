3cddbb727b53fc6596a1cb5ba3d5e6166acc1930
tomiir <rocchitomas@gmail.com>
2024-05-06 15:48:09 +0200

    chore: add email siwe tests

--- apps/laboratory/tests/shared/fixtures/w3m-email-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-email-fixture.ts
@@ -1,5 +1,6 @@
 import { test as base } from '@playwright/test'
 import type { ModalFixture } from './w3m-fixture'
+import { testMSiwe as siwe } from './w3m-fixture'
 import { ModalPage } from '../pages/ModalPage'
 import { ModalValidator } from '../validators/ModalValidator'
 import { Email } from '../utils/email'
@@ -23,5 +24,24 @@ export const testMEmail = base.extend<ModalFixture>({
   modalValidator: async ({ modalPage }, use) => {
     const modalValidator = new ModalValidator(modalPage.page)
     await use(modalValidator)
+  }
+})
+
+export const testMEmailSiwe = siwe.extend<ModalFixture>({
+  modalPage: async ({ page, library, context }, use, testInfo) => {
+    const modalPage = new ModalPage(page, library, 'email')
+    await modalPage.load()
+
+    const mailsacApiKey = process.env['MAILSAC_API_KEY']
+    if (!mailsacApiKey) {
+      throw new Error('MAILSAC_API_KEY is not set')
+    }
+    const email = new Email(mailsacApiKey)
+    const tempEmail = email.getEmailAddressToUse(testInfo.parallelIndex)
+
+    await modalPage.emailFlow(tempEmail, context, mailsacApiKey)
+    await modalPage.promptSiwe()
+    await modalPage.approveSign()
+    await use(modalPage)
   }
 })
