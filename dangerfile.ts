d4b85354294be93032e0c58aaf0a6fcd2721ed96
enesozturk <enesozturk.d@gmail.com>
2024-05-06 12:00:53 +0300

    chore: add tooltip controller test and refactor danger checks

--- dangerfile.ts
+++ dangerfile.ts
@@ -50,7 +50,9 @@ checkPackageJsons()
 async function checkUiPackage() {
   const created_ui_components = created_files.filter(f => f.includes('ui/src/components'))
   const created_ui_composites = created_files.filter(f => f.includes('ui/src/composites'))
+  const deleted_ui_composites = deleted_files.filter(f => f.includes('ui/src/composites'))
   const created_ui_layout = created_files.filter(f => f.includes('ui/src/layout'))
+  const deleted_ui_layout = deleted_files.filter(f => f.includes('ui/src/layout'))
   const created_ui_files = [
     ...created_ui_components,
     ...created_ui_composites,
@@ -82,7 +84,10 @@ async function checkUiPackage() {
       fail(`${f} is missing \`${STATE_PROPERTIES_COMMENT}\` comment`)
     }
 
-    if (diff?.added.includes('private ') && !diff.added.includes(PRIVATE_COMMENT)) {
+    const privateFunctionRegex = /private\s+(\w+)\s*\(\s*\)/g
+    const privateFunctions = diff?.added.match(privateFunctionRegex)
+
+    if (privateFunctions && !diff?.added.includes(PRIVATE_COMMENT)) {
       message(
         `${f} is missing \`${PRIVATE_COMMENT}\` comment, but seems to have private members. Check if this is correct`
       )
@@ -119,13 +124,21 @@ async function checkUiPackage() {
   const jsx_index_diff = jsx_index ? await diffForFile(jsx_index) : undefined
   const created_ui_components_index_ts = created_ui_components.filter(f => f.endsWith('index.ts'))
   const created_ui_composites_index_ts = created_ui_composites.filter(f => f.endsWith('index.ts'))
+  const deleted_ui_composites_index_ts = deleted_ui_composites.filter(f => f.endsWith('index.ts'))
+  const is_new_composites_added =
+    created_ui_composites_index_ts.length > deleted_ui_composites_index_ts.length
   const created_ui_layout_index_ts = created_ui_layout.filter(f => f.endsWith('index.ts'))
+  const deleted_ui_layout_index_ts = deleted_ui_layout.filter(f => f.endsWith('index.ts'))
+  const is_new_layout_added = created_ui_layout_index_ts.length > deleted_ui_layout_index_ts.length
 
   if (created_ui_components_index_ts.length && !ui_index_diff?.added.includes('src/components')) {
     fail('New components were added, but not exported in ui/index.ts')
   }
 
-  if (created_ui_composites_index_ts.length && !ui_index_diff?.added.includes('src/composites')) {
+  if (created_ui_composites_index_ts.length !== deleted_ui_composites_index_ts.length) {
+    fail('Number of created composites does not match number of deleted composites')
+  }
+  if (is_new_composites_added && !ui_index_diff?.added.includes('src/composites')) {
     fail('New composites were added, but not exported in ui/index.ts')
   }
 
@@ -141,11 +154,11 @@ async function checkUiPackage() {
     )
   }
 
-  if (created_ui_composites_index_ts.length && !jsx_index_diff?.added.includes('../composites')) {
+  if (is_new_composites_added && !jsx_index_diff?.added.includes('../composites')) {
     fail('New composites were added, but not exported in ui/utils/JSXTypeUtil.ts')
   }
 
-  if (created_ui_layout_index_ts.length && !jsx_index_diff?.added.includes('../layout')) {
+  if (is_new_layout_added && !jsx_index_diff?.added.includes('../layout')) {
     fail('New layout components were added, but not exported in ui/utils/JSXTypeUtil.ts')
   }
 
@@ -153,7 +166,7 @@ async function checkUiPackage() {
     fail('New components were added, but no stories were created')
   }
 
-  if (created_ui_composites.length && !created_ui_composites_stories.length) {
+  if (is_new_composites_added && !created_ui_composites_stories.length) {
     fail('New composites were added, but no stories were created')
   }
 
