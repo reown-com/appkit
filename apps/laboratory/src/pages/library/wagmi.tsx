a5f22eb824ecc9fe791fa3f7d5281a888b188858
Sven <fr.sven.fr@gmail.com>
2024-05-08 12:04:46 +0200

    add custom wallets

--- apps/laboratory/src/pages/library/wagmi.tsx
+++ apps/laboratory/src/pages/library/wagmi.tsx
@@ -25,6 +25,7 @@ const modal = createWeb3Modal({
   metadata: ConstantsUtil.Metadata,
   termsConditionsUrl: 'https://walletconnect.com/terms',
   privacyPolicyUrl: 'https://walletconnect.com/privacy',
+  customWallets: ConstantsUtil.CustomWallets,
   enableOnramp: true
 })
 
