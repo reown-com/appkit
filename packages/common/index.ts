247d322cbccfefd31827e6088990cc51ec6b54fc
enesozturk <enesozturk.d@gmail.com>
2024-05-08 20:14:00 +0300

    refactor: turn erc20 abi to js module

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,7 +1,8 @@
 // -- Utils -------------------------------------------------------------------
+export { ConstantsUtil } from './src/utils/ConstantsUtil.js'
 export { DateUtil } from './src/utils/DateUtil.js'
 export { NetworkUtil } from './src/utils/NetworkUtil.js'
 export { NumberUtil } from './src/utils/NumberUtil.js'
-export { default as erc20ABI } from './src/contracts/erc20.json'
+export { erc20ABI } from './src/contracts/erc20.js'
 export * from './src/utils/ThemeUtil.js'
 export type * from './src/utils/TypeUtil.js'
