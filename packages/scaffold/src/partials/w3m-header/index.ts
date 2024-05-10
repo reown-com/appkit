6870786aa17a60e9072931516d4169b5792ff61a
tomiir <rocchitomas@gmail.com>
2024-05-10 14:26:41 +0200

    fix: siwe global import (#2240)

--- packages/scaffold/src/partials/w3m-header/index.ts
+++ packages/scaffold/src/partials/w3m-header/index.ts
@@ -11,7 +11,6 @@ import { customElement } from '@web3modal/ui'
 import { LitElement, html } from 'lit'
 import { state } from 'lit/decorators.js'
 import styles from './styles.js'
-import { SIWEController } from '@web3modal/siwe'
 
 // -- Helpers ------------------------------------------- //
 function headings() {
@@ -117,7 +116,7 @@ export class W3mHeader extends LitElement {
   }
 
   private async onClose() {
-    if (OptionsController.state.isSiweEnabled && SIWEController.state.status !== 'success') {
+    if (OptionsController.state.isSiweEnabled) {
       const { SIWEController } = await import('@web3modal/siwe')
       if (SIWEController.state.status !== 'success') {
         await ConnectionController.disconnect()
