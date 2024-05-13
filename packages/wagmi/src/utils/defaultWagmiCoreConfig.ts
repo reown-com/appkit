a352d203ec02a50b85a211e4d7e1619086332ac6
tomiir <rocchitomas@gmail.com>
2024-05-13 12:21:48 +0200

    chore: use wagmis WC connector instead of alpha

--- packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
+++ packages/wagmi/src/utils/defaultWagmiCoreConfig.ts
@@ -2,7 +2,7 @@ import '@web3modal/polyfills'
 
 import type { CreateConfigParameters, CreateConnectorFn } from '@wagmi/core'
 import { createConfig } from '@wagmi/core'
-import { coinbaseWallet } from '@wagmi/connectors'
+import { coinbaseWallet, walletConnect } from '@wagmi/connectors'
 
 import { emailConnector } from '../connectors/EmailConnector.js'
 import { alphaWalletConnect } from '../connectors/alphaWalletConnect.js'
@@ -47,7 +47,7 @@ export function defaultWagmiConfig({
 
   // Enabled by default
   if (enableWalletConnect !== false) {
-    connectors.push(alphaWalletConnect({ projectId, metadata, showQrModal: false }))
+    connectors.push(walletConnect({ projectId, metadata, showQrModal: false }))
   }
 
   if (enableCoinbase !== false) {
