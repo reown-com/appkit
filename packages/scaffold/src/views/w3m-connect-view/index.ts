4fdab8add1eb1b0e4f1f70a8d9ab6987ec250a0a
Valentyn <valentin.vad.sh@gmail.com>
2024-05-16 17:46:46 +0300

    fix: removed no mobile flag

--- packages/scaffold/src/views/w3m-connect-view/index.ts
+++ packages/scaffold/src/views/w3m-connect-view/index.ts
@@ -145,7 +145,7 @@ export class W3mConnectView extends LitElement {
 
   private announcedTemplate() {
     return this.connectors.map(connector => {
-      if (connector.type !== 'ANNOUNCED' || CoreHelperUtil.isMobile()) {
+      if (connector.type !== 'ANNOUNCED') {
         return null
       }
 
