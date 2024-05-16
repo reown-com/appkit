c1d0f6c4a5cf758b36e8b71ed3249128b1657487
Alex <luu.alex98@gmail.com>
2024-05-16 14:05:35 -0400

    Add html, vue, react web3js examples  (#22)
    * chore(lab): dogfooding button (#2206)
    
    * add web3 examples
    
    * update example
    
    ---------
    
    Co-authored-by: Chris Smith <1979423+chris13524@users.noreply.github.com>
    Co-authored-by: Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>

---
+++ examples/html-web3/src/main.js
@@ -0,0 +1,62 @@
+import {
+  createWeb3Modal,
+  defaultConfig
+} from 'web3modal-web3js';
+
+// @ts-expect-error 1. Get projectId from Vite or next 
+const projectId = import.meta.env.PROJECT_ID
+if (!projectId) {
+  throw new Error('VITE_PROJECT_ID is not set')
+}
+
+// 2. Set chains
+const chains = [
+  {
+    chainId: 1,
+    name: 'Ethereum',
+    currency: 'ETH',
+    explorerUrl: 'https://etherscan.io',
+    rpcUrl: 'https://cloudflare-eth.com'
+  },
+  {
+    chainId: 42161,
+    name: 'Arbitrum',
+    currency: 'ETH',
+    explorerUrl: 'https://arbiscan.io',
+    rpcUrl: 'https://arb1.arbitrum.io/rpc'
+  }
+]
+
+const web3Config = defaultConfig({
+  metadata: {
+    name: 'Web3Modal',
+    description: 'Web3Modal Laboratory',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  },
+  defaultChainId: 1,
+  rpcUrl: 'https://cloudflare-eth.com'
+})
+
+// 3. Create modal
+const modal = createWeb3Modal({
+  web3Config,
+  chains,
+  projectId,
+  enableAnalytics: true,
+  themeMode: 'light',
+  themeVariables: {
+    '--w3m-color-mix': '#00DCFF',
+    '--w3m-color-mix-strength': 20
+  }
+})
+
+
+// 4. Trigger modal programaticaly
+const openConnectModalBtn = document.getElementById('open-connect-modal')
+const openNetworkModalBtn = document.getElementById('open-network-modal')
+
+openConnectModalBtn.addEventListener('click', () => modal.open())
+openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
+
+// 5. Alternatively use w3m component buttons (see index.html)
