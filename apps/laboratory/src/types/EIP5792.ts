169c1a92b61742ab6f84b0f906e75e69d01d7644
Karandeep Singh <90941366+KannuSingh@users.noreply.github.com>
2024-05-17 13:37:08 -0400

    added test for sendCalls and getCallsStatus (#2227)
    Added test component in laboratory for ethers and wagmi for EIP5792 methods sendCalls and getCallsStatus

---
+++ apps/laboratory/src/types/EIP5792.ts
@@ -0,0 +1,8 @@
+export type CapabilityName = 'atomicBatch' | 'paymasterService' | 'sessionKey'
+export type Capabilities = {
+  [K in CapabilityName]?: {
+    supported: boolean
+  }
+}
+export type GetCapabilitiesResult = Record<string, Capabilities>
+export type GetCallsStatusParams = `0x${string}`
