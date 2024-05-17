e173f935bf89430d0f69a2b522ee863aed86e3c8
Glitch <glitch.txs@gmail.com>
2024-05-17 16:02:39 +0500

    prettier

--- apps/laboratory/src/layout/CustomWallet.tsx
+++ apps/laboratory/src/layout/CustomWallet.tsx
@@ -21,22 +21,22 @@ interface Props {
 
 export function CustomWallet({ controls }: Props) {
   const [customWallet, setCustomWallet] = useState({
-    id: "custom",
+    id: 'custom',
     name: '',
     image_url: '',
     mobile_link: '',
     desktop_link: '',
     webapp_link: ''
-})
+  })
 
-function handleCustomWallet(){
-  localStorage.setItem(CUSTOM_WALLET, JSON.stringify(customWallet))
-  location.reload()
-}
+  function handleCustomWallet() {
+    localStorage.setItem(CUSTOM_WALLET, JSON.stringify(customWallet))
+    location.reload()
+  }
 
-function handleChange(e: ChangeEvent<HTMLInputElement>){
-  setCustomWallet(prev => ({ ...prev, [e.target.name]: e.target.value }))
-}
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setCustomWallet(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
 
   const { isOpen, onClose } = controls
 
@@ -47,22 +47,59 @@ function handleChange(e: ChangeEvent<HTMLInputElement>){
         <DrawerCloseButton />
         <DrawerHeader>Add a Custom Wallet</DrawerHeader>
         <Text pt="2" fontSize="sm" padding={6}>
-          Add a custom wallets to the modal. This feature is for wallets that are under the Explorer submission process. {" "} 
-          <Link isExternal color='blue.500' href='https://docs.walletconnect.com/cloud/explorer-submission#how-do-we-test-wallets' >Learn more.</Link>
+          Add a custom wallets to the modal. This feature is for wallets that are under the Explorer
+          submission process.{' '}
+          <Link
+            isExternal
+            color="blue.500"
+            href="https://docs.walletconnect.com/cloud/explorer-submission#how-do-we-test-wallets"
+          >
+            Learn more.
+          </Link>
         </Text>
         <DrawerBody>
           <Stack spacing={4}>
             Name
-            <Input name="name" value={customWallet.name} onChange={handleChange} variant='outline' placeholder='Name' />
+            <Input
+              name="name"
+              value={customWallet.name}
+              onChange={handleChange}
+              variant="outline"
+              placeholder="Name"
+            />
             Image URL
-            <Input name="image_url"  value={customWallet.image} onChange={handleChange} variant='outline' placeholder='Image URL' />
+            <Input
+              name="image_url"
+              value={customWallet.image}
+              onChange={handleChange}
+              variant="outline"
+              placeholder="Image URL"
+            />
             Mobile Linking (Optional)
-            <Input name="mobile_link"  value={customWallet.mobile} onChange={handleChange} variant='outline' placeholder='Mobile Linking' />
+            <Input
+              name="mobile_link"
+              value={customWallet.mobile}
+              onChange={handleChange}
+              variant="outline"
+              placeholder="Mobile Linking"
+            />
             Desktop Linking (Optional)
-            <Input name="desktop_link"  value={customWallet.desktop} onChange={handleChange} variant='outline' placeholder='Desktop Linking' />
+            <Input
+              name="desktop_link"
+              value={customWallet.desktop}
+              onChange={handleChange}
+              variant="outline"
+              placeholder="Desktop Linking"
+            />
             Webapp Linking (Optional)
-            <Input name="webapp_link"  value={customWallet.webapp} onChange={handleChange} variant='outline' placeholder='Webapp Linking' />
-            <Button onClick={handleCustomWallet} >Add Wallet</Button>
+            <Input
+              name="webapp_link"
+              value={customWallet.webapp}
+              onChange={handleChange}
+              variant="outline"
+              placeholder="Webapp Linking"
+            />
+            <Button onClick={handleCustomWallet}>Add Wallet</Button>
           </Stack>
         </DrawerBody>
       </DrawerContent>
