cc7ac008f94e8c93075f4397552970af782861a7
enesozturk <enesozturk.d@gmail.com>
2024-05-20 13:04:08 +0300

    Revert "added test for sendCalls and getCallsStatus (#2227)"
    This reverts commit 169c1a92b61742ab6f84b0f906e75e69d01d7644.

--- apps/laboratory/src/utils/ChainsUtil.ts
+++ apps/laboratory/src/utils/ChainsUtil.ts
@@ -1,5 +1,3 @@
-import * as viemChains from 'viem/chains'
-
 export const mainnet = {
   chainId: 1,
   name: 'Ethereum',
@@ -126,10 +124,4 @@ export const solanaDevnet = {
   currency: 'SOL',
   explorerUrl: 'https://explorer.solana.com/?cluster=devnet',
   rpcUrl: 'https://api.devnet.solana.com'
-}
-
-export function getChain(id: number) {
-  const chains = Object.values(viemChains) as viemChains.Chain[]
-
-  return chains.find(x => x.id === id)
 }
