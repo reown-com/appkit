a2c5b427dd191e0caf915df1e5987843f8883cf3
tomiir <rocchitomas@gmail.com>
2024-05-21 16:10:05 -0600

    fix: auth connector not showing in wagmi (#2290)

--- apps/laboratory/src/pages/library/wagmi-siwe.tsx
+++ apps/laboratory/src/pages/library/wagmi-siwe.tsx
@@ -1,23 +1,17 @@
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 import { Web3ModalButtons } from '../../components/Web3ModalButtons'
 import { WagmiTests } from '../../components/Wagmi/WagmiTests'
 import { ThemeStore } from '../../utils/StoreUtil'
-import { WagmiConstantsUtil } from '../../utils/WagmiConstants'
+import { CONFIGS } from '../../utils/WagmiConstants'
 import { SiweData } from '../../components/Siwe/SiweData'
 import { ConstantsUtil } from '../../utils/ConstantsUtil'
 import { siweConfig } from '../../utils/SiweUtils'
 
 const queryClient = new QueryClient()
 
-export const wagmiConfig = defaultWagmiConfig({
-  chains: WagmiConstantsUtil.chains,
-  projectId: ConstantsUtil.ProjectId,
-  metadata: ConstantsUtil.Metadata,
-  ssr: true
-})
+const wagmiConfig = CONFIGS.default
 const modal = createWeb3Modal({
   wagmiConfig,
   projectId: ConstantsUtil.ProjectId,
