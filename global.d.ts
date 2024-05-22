2a6455fafa72d0f9d778eb0d4f6d91a3099bf3a6
enesozturk <enesozturk.d@gmail.com>
2024-05-22 05:08:33 +0300

    chore: add plugin types to global

--- global.d.ts
+++ global.d.ts
@@ -3,3 +3,10 @@ type ControllerType<T> = {
   subscribe: (callback: (newState: T) => void) => () => void
   initialize: () => void
 }
+
+declare module '@web3modal/plugin-activity' {
+  import { ActivityPlugin } from './packages/core/src/types/ActivityPlugin'
+
+  const plugin: ActivityPlugin
+  export default plugin
+}
