efc9ddb2366b01fc9f4a4c2022287ea99340c668
enesozturk <enesozturk.d@gmail.com>
2024-06-05 13:49:29 +0300

    refactor: disable disconnected call for siwe if not connected

--- packages/scaffold/src/modal/w3m-modal/index.ts
+++ packages/scaffold/src/modal/w3m-modal/index.ts
@@ -35,7 +35,7 @@ export class W3mModal extends LitElement {
 
   @state() private isSiweEnabled = OptionsController.state.isSiweEnabled
 
-  @state() private connected = AccountController.state.isConnected
+  @state() private isConnected = AccountController.state.isConnected
 
   @state() private loading = ModalController.state.loading
 
@@ -49,7 +49,7 @@ export class W3mModal extends LitElement {
         this.loading = val
         this.onNewAddress(AccountController.state.caipAddress)
       }),
-      AccountController.subscribeKey('isConnected', val => (this.connected = val)),
+      AccountController.subscribeKey('isConnected', val => (this.isConnected = val)),
       AccountController.subscribeKey('caipAddress', val => this.onNewAddress(val)),
       OptionsController.subscribeKey('isSiweEnabled', val => (this.isSiweEnabled = val))
     )
@@ -88,7 +88,7 @@ export class W3mModal extends LitElement {
     if (this.isSiweEnabled) {
       const { SIWEController } = await import('@web3modal/siwe')
 
-      if (SIWEController.state.status !== 'success') {
+      if (SIWEController.state.status !== 'success' && this.isConnected) {
         await ConnectionController.disconnect()
       }
     }
@@ -165,7 +165,7 @@ export class W3mModal extends LitElement {
   }
 
   private async onNewAddress(caipAddress?: CaipAddress) {
-    if (!this.connected || this.loading) {
+    if (!this.isConnected || this.loading) {
       return
     }
 
