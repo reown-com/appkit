7bb27f2b7a82db18bbe5d1f68d3904dd7e2843a6
Sven <fr.sven.fr@gmail.com>
2024-06-10 15:34:03 +0200

    fix tests

--- apps/laboratory/tests/shared/validators/ModalValidator.ts
+++ apps/laboratory/tests/shared/validators/ModalValidator.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test'
-import type { Locator, Page } from '@playwright/test'
+import type { Page } from '@playwright/test'
 import { ConstantsUtil } from '../../../src/utils/ConstantsUtil'
 import { getMaximumWaitConnections } from '../utils/timeouts'
 import { verifySignature } from '../../../src/utils/SignatureUtil'
