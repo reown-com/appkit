83b083bf318a908f531c44608f2e4eba82d88cdc
Ben Kremer <contact@bkrem.dev>
2024-06-10 21:51:09 +0200

    chore: dogfood CSP in report-only mode

--- apps/laboratory/next.config.js
+++ apps/laboratory/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  img-src 'self' data: https://walletconnect.org;
+  font-src 'self' https://fonts.gstatic.com;
+  connect-src 'self' https://rpc.walletconnect.com https://rpc.walletconnect.org https://explorer.walletconnect.com https://explorer.walletconnect.org https://relay.walletconnect.com https://relay.walletconnect.org wss://relay.walletconnect.com wss://relay.walletconnect.org https://pulse.walletconnect.com https://pulse.walletconnect.org https://api.web3modal.com;
+  frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+`
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
@@ -6,6 +20,15 @@ const nextConfig = {
   cleanDistDir: true,
   async headers() {
     return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy-Report-Only',
+            value: cspHeader.replace(/\n/g, ' ').trim()
+          }
+        ]
+      },
       {
         source: '/.well-known/apple-app-site-association',
         headers: [{ key: 'content-type', value: 'application/json' }]
