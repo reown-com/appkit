7317367ec59e94859580ca612cd1b55221f31ad4
tomiir <rocchitomas@gmail.com>
2024-06-10 11:59:56 -0600

    chore: use walletconnect.org (#2360)
    Co-authored-by: Enes <enesozturk.d@gmail.com>

--- packages/common/src/utils/ConstantsUtil.ts
+++ packages/common/src/utils/ConstantsUtil.ts
@@ -1,4 +1,7 @@
 export const ConstantsUtil = {
   WC_NAME_SUFFIX: '.wcn.id',
-  WC_NAMES_ALLOWED_DOMAINS: ['walletconnect.com']
+  WC_NAMES_ALLOWED_DOMAINS: ['walletconnect.com'],
+  BLOCKCHAIN_API_RPC_URL: 'https://rpc.walletconnect.org',
+  PULSE_API_URL: 'https://pulse.walletconnect.org',
+  W3M_API_URL: 'https://api.web3modal.org'
 }
