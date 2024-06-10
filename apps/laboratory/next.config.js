775d56d442f69ecb54bf954b11ffadd2e7a604eb
Chris Smith <chris@walletconnect.com>
2024-06-10 15:10:47 -0700

    fix: tweak CSP

--- apps/laboratory/next.config.js
+++ apps/laboratory/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  script-src 'self' 'unsafe-inline';
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: https://walletconnect.org;
+  img-src 'self' 'data:' 'blob:' https://walletconnect.org https://walletconnect.com;
   font-src 'self' https://fonts.gstatic.com;
-  connect-src 'self' https://rpc.walletconnect.com https://rpc.walletconnect.org https://explorer.walletconnect.com https://explorer.walletconnect.org https://relay.walletconnect.com https://relay.walletconnect.org wss://relay.walletconnect.com wss://relay.walletconnect.org https://pulse.walletconnect.com https://pulse.walletconnect.org https://api.web3modal.com;
-  frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org;
+  connect-src 'self' https://rpc.walletconnect.com https://rpc.walletconnect.org https://explorer.walletconnect.com https://explorer.walletconnect.org https://relay.walletconnect.com https://relay.walletconnect.org wss://relay.walletconnect.com wss://relay.walletconnect.org https://pulse.walletconnect.com https://pulse.walletconnect.org https://api.web3modal.com https://api.web3modal.org https://o1095249.ingest.sentry.io;
+  frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org https://secure.walletconnect.com https://secure.walletconnect.org;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
