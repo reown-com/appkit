092a328c726a91f0638901e35911df52b344b9cc
tomiir <rocchitomas@gmail.com>
2024-06-11 11:17:37 -0600

    feat: make onramp enabled by default. remove flag from lab pages (#2384)

--- apps/laboratory/src/pages/library/wagmi-email.tsx
+++ apps/laboratory/src/pages/library/wagmi-email.tsx
@@ -19,7 +19,6 @@ const modal = createWeb3Modal({
   metadata: ConstantsUtil.Metadata,
   termsConditionsUrl: 'https://walletconnect.com/terms',
   privacyPolicyUrl: 'https://walletconnect.com/privacy',
-  enableOnramp: true,
   customWallets: ConstantsUtil.CustomWallets
 })
 
