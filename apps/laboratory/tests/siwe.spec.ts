9052321240551f0d81688c6bbec222efa396560c
tomiir <rocchitomas@gmail.com>
2024-06-13 14:02:28 -0600

    chore: add test

--- apps/laboratory/tests/siwe.spec.ts
+++ apps/laboratory/tests/siwe.spec.ts
@@ -10,28 +10,28 @@ testMWSiwe.afterEach(async ({ modalValidator, walletValidator, browserName }) =>
   await walletValidator.expectDisconnected()
 })
 
-// testMWSiwe(
-//   'it should sign in with ethereum',
-//   async ({ modalPage, walletPage, modalValidator, walletValidator }) => {
-//     const uri = await modalPage.getConnectUri()
-//     await walletPage.connectWithUri(uri)
-//     await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
-//     await modalValidator.expectAuthenticated()
-//     await modalValidator.expectConnected()
-//     await walletValidator.expectConnected()
-//     await modalPage.disconnect()
-//   }
-// )
+testMWSiwe(
+  'it should sign in with ethereum',
+  async ({ modalPage, walletPage, modalValidator, walletValidator }) => {
+    const uri = await modalPage.getConnectUri()
+    await walletPage.connectWithUri(uri)
+    await walletPage.handleSessionProposal(DEFAULT_SESSION_PARAMS)
+    await modalValidator.expectAuthenticated()
+    await modalValidator.expectConnected()
+    await walletValidator.expectConnected()
+    await modalPage.disconnect()
+  }
+)
 
-// testMWSiwe(
-//   'it should reject sign in with ethereum',
-//   async ({ modalPage, walletPage, modalValidator }) => {
-//     const uri = await modalPage.getConnectUri()
-//     await walletPage.connectWithUri(uri)
-//     await walletPage.handleRequest({ accept: false })
-//     await modalValidator.expectUnauthenticated()
-//   }
-// )
+testMWSiwe(
+  'it should reject sign in with ethereum',
+  async ({ modalPage, walletPage, modalValidator }) => {
+    const uri = await modalPage.getConnectUri()
+    await walletPage.connectWithUri(uri)
+    await walletPage.handleRequest({ accept: false })
+    await modalValidator.expectUnauthenticated()
+  }
+)
 
 testMWSiwe(
   'it should require re-authentication when switching networks',
@@ -46,10 +46,7 @@ testMWSiwe(
 
     // Re-authentication required
     await modalValidator.expectUnauthenticated()
-    await modalPage.promptSiwe()
-    await walletPage.handleRequest({ accept: true })
-    await modalValidator.expectAuthenticated()
     await modalPage.closeModal()
-    await modalPage.disconnect()
+    await modalValidator.expectDisconnected()
   }
 )
