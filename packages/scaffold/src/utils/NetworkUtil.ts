75c6c0c27c0512ec97719259d0453fd10e4d1b32
tomiir <rocchitomas@gmail.com>
2024-06-14 09:56:40 -0600

    fix: import only if siwe is enabled

--- packages/scaffold/src/utils/NetworkUtil.ts
+++ packages/scaffold/src/utils/NetworkUtil.ts
@@ -2,8 +2,8 @@ import { OptionsController, RouterUtil } from '@web3modal/core'
 
 export const NetworkUtil = {
   onNetworkChange: async () => {
-    const { SIWEController } = await import('@web3modal/siwe')
     if (OptionsController.state.isSiweEnabled) {
+      const { SIWEController } = await import('@web3modal/siwe')
       if (SIWEController.state._client?.options?.signOutOnNetworkChange) {
         await SIWEController.signOut()
       }
