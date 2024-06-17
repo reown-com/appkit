d2966bccb4b787fd2926ae69583e50b27ff52847
enesozturk <enesozturk.d@gmail.com>
2024-06-17 14:38:04 +0300

    fix: linter issues

--- packages/common/src/utils/NetworkUtil.ts
+++ packages/common/src/utils/NetworkUtil.ts
@@ -1,4 +1,4 @@
-import type { CaipNetworkId } from "./TypeUtil"
+import type { CaipNetworkId } from './TypeUtil.js'
 
 export const NetworkUtil = {
   caipNetworkIdToNumber(caipnetworkId?: CaipNetworkId) {
