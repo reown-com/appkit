eb588856a5a4d5892fe38c133d9d83e2d7761e77
Chris Smith <chris@walletconnect.com>
2024-06-27 12:03:44 -0700

    fix: lint error

--- apps/laboratory/src/components/RandomLink.tsx
+++ apps/laboratory/src/components/RandomLink.tsx
@@ -3,7 +3,8 @@
 import Link from "next/link";
 
 export function RandomLink({ hrefs, children }: { hrefs: string[], children: React.ReactNode }) {
-  const href = hrefs[Math.floor(Math.random() * hrefs.length)]!;
+  const href = hrefs[Math.floor(Math.random() * hrefs.length)];
+  if (!href) throw new Error("No hrefs provided");
   return <Link href={href}>
     {children}
   </Link>
