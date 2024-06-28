ba94b77338d3cd4d48e13eca8c2b8f1de47432db
Chris Smith <chris@walletconnect.com>
2024-06-28 10:00:35 -0700

    fix: lint

--- apps/laboratory/src/components/RandomLink.tsx
+++ apps/laboratory/src/components/RandomLink.tsx
@@ -6,11 +6,11 @@ import { useState, useEffect } from 'react'
 export function RandomLink({ hrefs, children }: { hrefs: string[]; children: React.ReactNode }) {
   const [href, setHref] = useState<string>()
   useEffect(() => {
-    const href = hrefs[Math.floor(Math.random() * hrefs.length)]
+    const newHref = hrefs[Math.floor(Math.random() * hrefs.length)]
     if (!href) {
       throw new Error('No hrefs provided')
     }
-    setHref(href)
+    setHref(newHref)
   }, [hrefs])
 
   return href ? <Link href={href}>{children}</Link> : <></>
