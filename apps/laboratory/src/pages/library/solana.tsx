c4ad0cd454b510bdf67d2d7bb8757ca0ffdd3def
Valentyn <valentin.vad.sh@gmail.com>
2024-07-01 12:42:55 +0300

    feat: removed wallet adapters per wallet, added wallet standard support (#2474)

--- apps/laboratory/src/pages/library/solana.tsx
+++ apps/laboratory/src/pages/library/solana.tsx
@@ -5,6 +5,13 @@ import { solana, solanaDevnet, solanaTestnet } from '../../utils/ChainsUtil'
 import { Web3ModalButtons } from '../../components/Web3ModalButtons'
 import { ConstantsUtil } from '../../utils/ConstantsUtil'
 import { SolanaTests } from '../../components/Solana/SolanaTests'
+import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack'
+import {
+  PhantomWalletAdapter,
+  HuobiWalletAdapter,
+  SolflareWalletAdapter,
+  TrustWalletAdapter
+} from '@solana/wallet-adapter-wallets'
 
 const chains = [solana, solanaTestnet, solanaDevnet]
 
@@ -22,7 +29,14 @@ const modal = createWeb3Modal({
   enableAnalytics: false,
   termsConditionsUrl: 'https://walletconnect.com/terms',
   privacyPolicyUrl: 'https://walletconnect.com/privacy',
-  customWallets: ConstantsUtil.CustomWallets
+  customWallets: ConstantsUtil.CustomWallets,
+  wallets: [
+    new BackpackWalletAdapter(),
+    new HuobiWalletAdapter(),
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new TrustWalletAdapter()
+  ]
 })
 
 ThemeStore.setModal(modal)
