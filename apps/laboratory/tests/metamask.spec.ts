a4e9bd3adf2c9768172f29d5e62ece2f11b666a1
Luka Isailovic <lukaisailovic@gmail.com>
2024-07-10 15:43:51 +0200

    chore: add sign and reject sign tests

--- apps/laboratory/tests/metamask.spec.ts
+++ apps/laboratory/tests/metamask.spec.ts
@@ -1,20 +1,27 @@
 import { testMetamask } from './shared/fixtures/w3m-metamask-fixture'
 
-testMetamask.beforeEach(({ browserName }) => {
+testMetamask.beforeEach(async ({ browserName, modalPage, wallet, modalValidator }) => {
   if (browserName !== 'chromium') {
     testMetamask.skip()
   }
+  await modalPage.connectMetamask()
+  await wallet.approve()
+  await modalValidator.expectConnected()
 })
 
 testMetamask.afterEach(async ({ modalPage, modalValidator }) => {
   await modalPage.disconnect()
   await modalValidator.expectDisconnected()
 })
-testMetamask(
-  'it should be able to connect with metamask',
-  async ({ modalPage, wallet, modalValidator }) => {
-    await modalPage.connectMetamask()
-    await wallet.approve()
-    await modalValidator.expectConnected()
-  }
-)
+
+testMetamask('it should sign', async ({ modalPage, wallet, modalValidator }) => {
+  await modalPage.sign()
+  await wallet.confirmTransaction()
+  await modalValidator.expectAcceptedSign()
+})
+
+testMetamask('it should reject sign', async ({ modalPage, wallet, modalValidator }) => {
+  await modalPage.sign()
+  await wallet.reject()
+  await modalValidator.expectRejectedSign()
+})
