54689acfcd2d22a155bccdf1354c71ebbd0bf7b3
Chris Smith <chris@walletconnect.com>
2024-07-19 11:33:44 -0700

    chore: ID allocation service

---
+++ apps/laboratory/tests/id-allocation-service/src/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from 'hono'
+import type { KVNamespace } from '@cloudflare/workers-types'
+
+export type Bindings = {
+  ID_ALLOCATION: KVNamespace
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+type Ids = { [key: number]: number }
+
+app.get('/allocate', async (c) => {
+  const now = Date.now()
+  const ids: Ids = JSON.parse(await c.env.ID_ALLOCATION.get('ids') || '{}')
+  for (let id = 0; ; id++) {
+    const isFree = ids[id] === undefined || ids[id] < now
+    if (isFree) {
+      ids[id] = now + 1000 * 5
+      await c.env.ID_ALLOCATION.put('ids', JSON.stringify(ids))
+      return c.json({ id: id })
+    }
+  }
+})
+
+export default app
