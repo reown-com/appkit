105c7ceb4ffabb7ae9d54bde6b28490176faf002
enesozturk <enesozturk.d@gmail.com>
2024-07-23 12:03:16 +0300

    chore: add social tests comment, update scripts

--- apps/laboratory/tests/social.spec.ts
+++ apps/laboratory/tests/social.spec.ts
@@ -1,3 +1,9 @@
+/**
+ * Social tests are still blocked because we are having authorization issues with them.
+ * Some options like Google, Discord have human verification and passing them with the Playwright environment is not an easy pick (or not quite possible).
+ * GitHub doesn't have human verification but when running the tests in parallel, it's getting stuck too much and not passing.
+ * We are going to keep this test file here for future reference and when we have a better solution for this, we can enable them.
+ */
 import { expect } from '@playwright/test'
 import { testMSocial } from './shared/fixtures/w3m-social-fixture'
 import type { ModalWalletPage } from './shared/pages/ModalWalletPage'
