d574a39e82b4fcd9b282e777fe74614bcfee956e
Gancho Radkov <ganchoradkov93@gmail.com>
2024-08-01 18:12:48 +0300

    chore: shows network state in ethers lab

--- apps/laboratory/src/pages/library/ethers.tsx
+++ apps/laboratory/src/pages/library/ethers.tsx
@@ -5,6 +5,9 @@ import { ThemeStore } from '../../utils/StoreUtil'
 import { EthersConstants } from '../../utils/EthersConstants'
 import { ConstantsUtil } from '../../utils/ConstantsUtil'
 import { EthersModalInfo } from '../../components/Ethers/EthersModalInfo'
+import { useChakraToast } from '../../components/Toast'
+
+import React from 'react'
 
 const modal = createWeb3Modal({
   ethersConfig: defaultConfig({
@@ -23,8 +26,36 @@ const modal = createWeb3Modal({
 })
 
 ThemeStore.setModal(modal)
-
+let loaded = false
 export default function Ethers() {
+  const toast = useChakraToast()
+
+  React.useEffect(() => {
+    if (loaded) {
+      return
+    }
+    loaded = true
+    // @ts-expect-error - walletConnectProviderInitPromise is private
+    modal.walletConnectProviderInitPromise.then(() => {
+      // @ts-expect-error - walletConnectProvider is private
+      const relayer = modal.walletConnectProvider.signer.client.core.relayer
+      relayer.on('relayer_connect', () => {
+        toast({
+          title: 'Connected!',
+          description: 'wss connection established',
+          type: 'success'
+        })
+      })
+      relayer.on('relayer_disconnect', () => {
+        toast({
+          title: 'Disconnected!',
+          description: 'wss connection lost',
+          type: 'error'
+        })
+      })
+    })
+  }, [])
+
   return (
     <>
       <Web3ModalButtons />
