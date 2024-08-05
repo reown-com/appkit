30584c7f87cff792a207f1b25099150c64137a46
Chris Smith <chris@walletconnect.com>
2024-08-05 11:43:17 -0400

    chore: switch to durable objects

--- services/id-allocation-service/src/index.ts
+++ services/id-allocation-service/src/index.ts
@@ -1,26 +1,36 @@
 import { Hono } from 'hono'
-import type { KVNamespace } from '@cloudflare/workers-types'
+import type { DurableObjectNamespace } from '@cloudflare/workers-types'
+import { DurableObject } from 'cloudflare:workers'
 
 export type Bindings = {
-  ID_ALLOCATION: KVNamespace
+  ID_ALLOCATION: DurableObjectNamespace<IdAllocation>,
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
 
 type Ids = { [key: number]: number }
 
-app.get('/allocate', async c => {
-  const now = Date.now()
-  const ids: Ids = JSON.parse((await c.env.ID_ALLOCATION.get('ids')) || '{}')
-  for (let id = 0; ; id++) {
-    const exp = ids[id]
-    const isFree = exp === undefined || exp < now
-    if (isFree) {
-      ids[id] = now + 1000 * 60 * 5
-      await c.env.ID_ALLOCATION.put('ids', JSON.stringify(ids))
-      return c.json({ id: id })
+export class IdAllocation extends DurableObject {
+  async allocateId(): Promise<number> {
+    const now = Date.now()
+    const ids: Ids = JSON.parse((await this.ctx.storage.get('ids')) || '{}')
+    for (let id = 0; ; id++) {
+      const exp = ids[id]
+      const isFree = exp === undefined || exp < now
+      if (isFree) {
+        ids[id] = now + 1000 * 60 * 5
+        await this.ctx.storage.put('ids', JSON.stringify(ids))
+        return id
+      }
     }
   }
+}
+
+app.get('/allocate', async c => {
+  const objId = c.env.ID_ALLOCATION.idFromName('id-allocation')
+  const obj = c.env.ID_ALLOCATION.get(objId)
+  const id = await obj.allocateId()
+  return c.json({ id: id })
 })
 
 export default app
