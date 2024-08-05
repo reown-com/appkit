1a696fb4b97c11a5685e5e4d928eea11d636fdee
Chris Smith <chris@walletconnect.com>
2024-08-05 09:59:26 -0400

    chore: lint

--- apps/laboratory/tests/shared/utils/email.ts
+++ apps/laboratory/tests/shared/utils/email.ts
@@ -83,7 +83,11 @@ export class Email {
     )
     const { id } = await response.json()
 
-    return `w3m-w${id}@${domain}`
+    const email = `w3m-w${id}@${domain}`
+    // eslint-disable-next-line no-console
+    console.log(`allocating email: ${email}`)
+
+    return email
   }
 
   getSmartAccountEnabledEmail(): string {
