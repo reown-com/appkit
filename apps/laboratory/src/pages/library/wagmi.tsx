b43bbf50e913da0247bf0be71b00fc3954d50002
Gancho Radkov <ganchoradkov93@gmail.com>
2024-08-07 11:09:38 +0300

    feat: adds network toast for wagmi as well

--- apps/laboratory/src/pages/library/wagmi.tsx
+++ apps/laboratory/src/pages/library/wagmi.tsx
@@ -7,6 +7,9 @@ import { ThemeStore } from '../../utils/StoreUtil'
 import { getWagmiConfig } from '../../utils/WagmiConstants'
 import { ConstantsUtil } from '../../utils/ConstantsUtil'
 import { WagmiModalInfo } from '../../components/Wagmi/WagmiModalInfo'
+import React from 'react'
+import { useChakraToast } from '../../components/Toast'
+import { EthereumProvider } from '@walletconnect/ethereum-provider'
 
 const queryClient = new QueryClient()
 
@@ -23,8 +26,41 @@ const modal = createWeb3Modal({
 })
 
 ThemeStore.setModal(modal)
-
+let loaded = false
 export default function Wagmi() {
+  const toast = useChakraToast()
+
+  React.useEffect(() => {
+    if (loaded) {
+      return
+    }
+    loaded = true
+    // @ts-expect-error - wagmiConfig is private
+    const connector = modal?.wagmiConfig?.connectors?.find(c => c.id === 'walletConnect')
+    if (!connector) {
+      return
+    }
+
+    connector.getProvider().then((provider: typeof EthereumProvider) => {
+      // @ts-expect-error - walletConnectProvider is private
+      const relayer = provider.signer.client.core.relayer
+      relayer.on('relayer_connect', () => {
+        toast({
+          title: 'Connected!',
+          description: 'wss connection established',
+          type: 'success'
+        })
+      })
+      relayer.on('relayer_disconnect', () => {
+        toast({
+          title: 'Disconnected!',
+          description: 'wss connection lost',
+          type: 'error'
+        })
+      })
+    })
+  }, [])
+
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
