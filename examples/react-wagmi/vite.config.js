3745718af17cecd9f4790bd0cdacb068153ab7b2
Luka Isailovic <lukaisailovic@gmail.com>
2024-08-13 11:47:24 +0200

    fix: build error

--- examples/react-wagmi/vite.config.js
+++ examples/react-wagmi/vite.config.js
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    preserveSymlinks: true
+  }
 })
