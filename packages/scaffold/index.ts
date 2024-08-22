a167993ab95c1a14ed3e05b16b27c0c2990be136
enesozturk <enesozturk.d@gmail.com>
2024-08-22 12:10:08 +0300

    chore: remove scaffold packages

--- packages/scaffold/index.ts
+++
@@ -1,7 +0,0 @@
-export * from '@web3modal/scaffold-ui'
-
-export { Web3ModalScaffold } from './src/client.js'
-export type { LibraryOptions, ScaffoldOptions } from './src/client.js'
-
-export type * from '@web3modal/core'
-export { CoreHelperUtil } from '@web3modal/core'
