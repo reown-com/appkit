0ae6a5b4f9a451614e0cdb598bc0c49b3ac3cdcf
tomiir <rocchitomas@gmail.com>
2024-08-23 11:15:58 +0200

    chore: add hook test interactions to lab

--- apps/laboratory/src/components/Ethers/EthersTests.tsx
+++ apps/laboratory/src/components/Ethers/EthersTests.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react'
-import { useWeb3ModalAccount } from '@web3modal/ethers/react'
+import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react'
 import { EthersSignMessageTest } from './EthersSignMessageTest'
 import { EthersSignTypedDataTest } from './EthersSignTypedDataTest'
-import { StackDivider, Card, CardHeader, Heading, CardBody, Box, Stack } from '@chakra-ui/react'
+import {
+  StackDivider,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  Box,
+  Stack,
+  Button
+} from '@chakra-ui/react'
 import { EthersTransactionTest } from './EthersTransactionTest'
 import { EthersWriteContractTest } from './EthersWriteContractTest'
 import { EthersSendCallsTest } from './EthersSendCallsTest'
@@ -12,6 +21,7 @@ import { EthersSendCallsWithPaymasterServiceTest } from './EthersSendCallsWithPa
 export function EthersTests() {
   const [ready, setReady] = React.useState(false)
   const { isConnected } = useWeb3ModalAccount()
+  const { open } = useWeb3Modal()
 
   React.useEffect(() => {
     setReady(true)
@@ -29,6 +39,12 @@ export function EthersTests() {
 
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase" pb="2">
+              Hook Interactions
+            </Heading>
+            <Button onClick={() => open()}>Open</Button>
+          </Box>
           <Box>
             <Heading size="xs" textTransform="uppercase" pb="2">
               Sign Message
