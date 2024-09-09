316e9c152c6008bc3751aef7bd0cae44017aaa1b
enesozturk <enesozturk.d@gmail.com>
2024-09-09 14:17:32 +0300

    fix: ethers tests

--- packages/adapters/ethers/src/tests/client.test.ts
+++ packages/adapters/ethers/src/tests/client.test.ts
@@ -318,20 +318,18 @@ describe('EVMEthersClient', () => {
           { type: '@w3m-frame/SWITCH_NETWORK_SUCCESS', payload: { chainId: '137' } },
           { method: 'eth_accounts' }
         )
-        expect(mockAppKit.popTransactionStack).toHaveBeenCalledWith(true)
+        expect(mockAppKit.popTransactionStack).toHaveBeenCalledWith()
       })
 
       it('should handle auth not connected', () => {
         client['handleAuthNotConnected']()
         expect(mockAppKit.setIsConnected).toHaveBeenCalledWith(false, 'eip155')
-        expect(mockAppKit.setLoading).toHaveBeenCalledWith(false)
       })
 
       it('should handle auth is connected', () => {
         const preferredAccountType = 'eoa'
         client['handleAuthIsConnected'](preferredAccountType)
         expect(mockAppKit.setIsConnected).toHaveBeenCalledWith(true, 'eip155')
-        expect(mockAppKit.setLoading).toHaveBeenCalledWith(false)
         expect(mockAppKit.setPreferredAccountType).toHaveBeenCalledWith(
           preferredAccountType,
           'eip155'
