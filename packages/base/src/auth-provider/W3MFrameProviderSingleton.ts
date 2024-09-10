7b84d490df7def7a80597c7d173897c8121d3e4a
tomiir <rocchitomas@gmail.com>
2024-09-10 12:08:36 -0600

    chore: fix empty function linter issue

--- packages/base/src/auth-provider/W3MFrameProviderSingleton.ts
+++ packages/base/src/auth-provider/W3MFrameProviderSingleton.ts
@@ -4,12 +4,14 @@ import { W3mFrameProvider } from '@rerock/wallet'
 export class W3mFrameProviderSingleton {
   private static instance: W3mFrameProvider
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function -- This is a singleton
   private constructor() {}
 
   public static getInstance(projectId: string, chainId?: number | CaipNetworkId): W3mFrameProvider {
     if (!W3mFrameProviderSingleton.instance) {
       W3mFrameProviderSingleton.instance = new W3mFrameProvider(projectId, chainId)
     }
+
     return W3mFrameProviderSingleton.instance
   }
 }
