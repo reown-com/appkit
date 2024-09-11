4ff28290d4178e84cca29420966181804432cb15
tomiir <rocchitomas@gmail.com>
2024-09-11 10:10:23 -0600

    fix: namesace

--- scripts/generate-npmrc.sh
+++ scripts/generate-npmrc.sh
@@ -2,6 +2,6 @@
 
 # This script generates a .npmrc file for the current project.
 rm .npmrc
-echo @namespace:registry=https://npm.pkg.github.com  >> .npmrc
+echo @rerock:registry=https://npm.pkg.github.com  >> .npmrc
 echo //npm.pkg.github.com/:_authToken=$GITHUB_TOKEN >> .npmrc
 echo 'GENERATED_NPMRC'
