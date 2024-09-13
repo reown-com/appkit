42f6fbb3581adf92f210b0901a44925f6a63429a
tomiir <rocchitomas@gmail.com>
2024-09-13 12:20:21 -0600

    chore: fix canary docker url

--- apps/laboratory/docker-canary.sh
+++ apps/laboratory/docker-canary.sh
@@ -2,6 +2,6 @@
 
 pnpm playwright:test:canary
 
-destination="s3://$TEST_RESULTS_BUCKET/appkit-canary/$(date --iso-8601=seconds)/test-results/"
+destination="s3://$TEST_RESULTS_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/"
 echo "Uploading test results to $destination"
 aws s3 cp ./test-results/ $destination --recursive
