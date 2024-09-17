c32ff46bbf2a93cb28035da54e7cd04cd1f0cea8
Sven <fr.sven.fr@gmail.com>
2024-09-17 15:53:56 +0200

    add reown secure site

--- packages/wallet/src/W3mFrameConstants.ts
+++ packages/wallet/src/W3mFrameConstants.ts
@@ -1,5 +1,5 @@
 export const SECURE_SITE_SDK =
-  process.env['NEXT_PUBLIC_SECURE_SITE_SDK_URL'] || 'https://secure.walletconnect.org/sdk'
+  process.env['NEXT_PUBLIC_SECURE_SITE_SDK_URL'] || 'https://secure.reown.org/sdk'
 
 export const DEFAULT_LOG_LEVEL = process.env['NEXT_PUBLIC_DEFAULT_LOG_LEVEL'] || 'error'
 
