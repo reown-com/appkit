df1624f7718eef8b7ec263e2f87dd4c6a266d772
tomiir <rocchitomas@gmail.com>
2024-09-24 09:19:25 -0300

    chore: add scaffold-utils tests

--- packages/scaffold-ui/src/utils/WalletUtil.ts
+++ packages/scaffold-ui/src/utils/WalletUtil.ts
@@ -2,9 +2,9 @@ import {
   ConnectorController,
   CoreHelperUtil,
   OptionsController,
-  StorageUtil,
-  type WcWallet
+  StorageUtil
 } from '@reown/appkit-core'
+import type { WcWallet } from '@reown/appkit-core'
 
 export const WalletUtil = {
   filterOutDuplicatesByRDNS(wallets: WcWallet[]) {
@@ -50,5 +50,30 @@ export const WalletUtil = {
     const uniqueWallets = this.filterOutDuplicatesByIds(uniqueByRDNS)
 
     return uniqueWallets
+  },
+
+  markWalletsAsInstalled(wallets: WcWallet[]) {
+    const { connectors } = ConnectorController.state
+    const installedConnectors = connectors
+      .filter(c => c.type === 'ANNOUNCED')
+      .reduce<Record<string, boolean>>((acum, val) => {
+        if (!val.info?.rdns) {
+          return acum
+        }
+        acum[val.info.rdns] = true
+
+        return acum
+      }, {})
+
+    const walletsWithInstalled: (WcWallet & { installed: boolean })[] = wallets.map(wallet => ({
+      ...wallet,
+      installed: Boolean(wallet.rdns) && Boolean(installedConnectors[wallet.rdns ?? ''])
+    }))
+
+    const sortedWallets = walletsWithInstalled.sort(
+      (a, b) => Number(b.installed) - Number(a.installed)
+    )
+
+    return sortedWallets
   }
 }
