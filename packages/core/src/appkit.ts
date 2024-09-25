5e4a290b506ddcf614c48a7688b6bf77e5a0932c
Felipe Mendes <zo.fmendes@gmail.com>
2024-09-25 10:15:25 -0300

    wip: add universal provider example

--- packages/core/src/appkit.ts
+++ packages/core/src/appkit.ts
@@ -1,11 +1,12 @@
 import type { ChainNamespace } from '@reown/appkit-common'
 import type { AdapterBlueprint } from './chain-adapters/ChainAdapterBlueprint.js'
 import type UnviersalProvider from '@walletconnect/universal-provider'
+import { UniversalAdapter } from './chain-adapters/UniversalAdapter.js'
 
 type Adapters = Record<ChainNamespace, AdapterBlueprint>
 
 export class AppKit {
-  private adapters: Adapters;
+  private adapters: Adapters
 
   private activeChainNamespace?: ChainNamespace
 
@@ -15,24 +16,23 @@ export class AppKit {
     this.adapters = adapters
   }
 
-
-
-
-
   private getAdapter(namespace: ChainNamespace) {
     return this.adapters.find(adapter => adapter.namespace === namespace)
   }
 
   private createAdapters(blueprints: AdapterBlueprint[]): Adapters {
+    const universalAdapter = new UniversalAdapter({ universalProvider: this.universalProvider })
+
     return (['eip155', 'solana', 'polkadot'] as const).reduce((adapters, namespace) => {
       const blueprint = blueprints.find(b => b.namespace === namespace)
       if (blueprint) {
         adapters[namespace] = blueprint
       } else {
-        adapters[]
+        adapters[namespace] = universalAdapter
       }
+
       return adapters
-    }, {} as Adapters) 
+    }, {} as Adapters)
   }
 }
 
