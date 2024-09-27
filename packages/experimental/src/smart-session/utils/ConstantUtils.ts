4db86b28bfe1d988ee84753882e7ffa1a486afe4
Karandeep Singh <karandeepsinghcaj@outlook.com>
2024-09-27 01:05:01 -0400

    chore: remove unused values

--- packages/experimental/src/smart-session/utils/ConstantUtils.ts
+++ packages/experimental/src/smart-session/utils/ConstantUtils.ts
@@ -1,4 +1,3 @@
 export const ConstantsUtil = {
-  WC_COSIGNER_BASE_URL: 'https://rpc.walletconnect.org/v1/sessions',
-  WALLET_SERVICE_BASE_URL: 'https://react-wallet.walletconnect.com/api'
+  WC_COSIGNER_BASE_URL: 'https://rpc.walletconnect.org/v1/sessions'
 }
