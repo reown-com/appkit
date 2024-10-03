b2d781b2df95f059d85ccd1934f2ad5fe3e53963
enesozturk <enesozturk.d@gmail.com>
2024-10-03 03:07:46 +0300

    chore: format files

--- examples/next-wagmi-app-router/next.config.mjs
+++ examples/next-wagmi-app-router/next.config.mjs
@@ -6,4 +6,4 @@ const nextConfig = {
   }
 }
 
-export default nextConfig;
+export default nextConfig
--- examples/next-wagmi-app-router/src/app/layout.tsx
+++ examples/next-wagmi-app-router/src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
-import { headers } from "next/headers"; // added
+import { headers } from 'next/headers' // added
 import ContextProvider from '@/context'
 
 export const metadata: Metadata = {
-  title: "AppKit Example App",
-  description: "Powered by WalletConnect"
-};
+  title: 'AppKit Example App',
+  description: 'Powered by WalletConnect'
+}
 
 export default function RootLayout({
   children
