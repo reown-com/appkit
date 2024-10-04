571644fee3b2dd61a381ef32fc5468bc2a3f1e59
Luka Isailovic <lukaisailovic@gmail.com>
2024-10-04 11:34:51 +0200

    chore: add testing configuration and modal version to sentry

--- apps/laboratory/src/utils/SentryUtil.ts
+++ apps/laboratory/src/utils/SentryUtil.ts
@@ -1,14 +1,11 @@
 import * as Sentry from '@sentry/react'
 import { BrowserTracing } from '@sentry/browser'
-import packageJson from '../../package.json' assert { type: 'json' }
 import type { AppKit } from '@reown/appkit'
 
 const TESTING_CONFIGURATION_TAG_KEY = 'testing-configuration'
+const FULL_VERSION_TAG_KEY = 'full-version'
 
 export function bootstrapSentry() {
-  // eslint-disable-next-line no-console
-  console.log('Version', packageJson.dependencies['@reown/appkit'])
-
   const dsn = process.env['NEXT_PUBLIC_SENTRY_DSN']
   if (dsn) {
     Sentry.init({
@@ -22,15 +19,16 @@ export function bootstrapSentry() {
 export function setSdkVersion(version: string) {
   Sentry.withScope(scope => {
     scope.addEventProcessor(event => {
-      event.release = version
+      event.release = version.split('-')[1]
 
       return event
     })
+    scope.setTag(FULL_VERSION_TAG_KEY, version)
   })
 }
 
 export function setTestingConfiguration(value: string) {
-  setTag(TESTING_CONFIGURATION_TAG_KEY, value)
+  setTag(TESTING_CONFIGURATION_TAG_KEY, value.replace('/library/', '').replace('/', ''))
 }
 
 function setTag(key: string, value: string) {
@@ -40,7 +38,9 @@ function setTag(key: string, value: string) {
 }
 
 export function updateSentryEnvironment(modal: AppKit) {
-  if (typeof window === 'undefined') {
+  setSdkVersion(modal.version)
+
+  if (typeof window !== 'undefined') {
     setTestingConfiguration(window.location.pathname)
   }
 }
