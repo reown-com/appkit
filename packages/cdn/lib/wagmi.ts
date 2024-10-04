90b04e612e9af99ff23361dec9cf879c2d133fcb
enesozturk <enesozturk.d@gmail.com>
2024-10-04 20:07:36 +0300

    chore: update cdn exports

--- packages/cdn/lib/wagmi.ts
+++ packages/cdn/lib/wagmi.ts
@@ -1,6 +1,6 @@
 import '@reown/appkit-polyfills'
-export * as AppKit from '@reown/appkit-wagmi'
+export * as AppKit from '@reown/appkit'
+export * as AppKitWagmi from '@reown/appkit-adapter-wagmi'
 export * as Viem from 'viem'
-export * as Chains from 'viem/chains'
 export * as Connectors from '@wagmi/connectors'
 export * as WagmiCore from '@wagmi/core'
