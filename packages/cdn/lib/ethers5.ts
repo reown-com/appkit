3b3b124ba9138b47aeae193491b36f495bd7da89
Enes <enesozturk.d@gmail.com>
2024-10-09 15:32:59 +0300

    refactor: appkit packages cdn workaround (#3046)

--- packages/cdn/lib/ethers5.ts
+++
@@ -1,3 +0,0 @@
-import '@reown/appkit-polyfills'
-export * as AppKit from '@reown/appkit'
-export * as AppKitEthers5 from '@reown/appkit-adapter-ethers5'
--- packages/cdn/lib/wagmi.ts
+++
@@ -1,6 +0,0 @@
-import '@reown/appkit-polyfills'
-export * as AppKit from '@reown/appkit'
-export * as AppKitWagmi from '@reown/appkit-adapter-wagmi'
-export * as Viem from 'viem'
-export * as Connectors from '@wagmi/connectors'
-export * as WagmiCore from '@wagmi/core'
