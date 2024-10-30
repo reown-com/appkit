3d40e7c4df068f074367eda2b682345f9441fcf8
tomiir <rocchitomas@gmail.com>
2024-10-30 12:20:06 +0100

    chore: remove intermediate select screen account on auth connector (#2827)

--- packages/core/src/controllers/RouterController.ts
+++ packages/core/src/controllers/RouterController.ts
@@ -32,7 +32,6 @@ export interface RouterControllerState {
   view:
     | 'Account'
     | 'AccountSettings'
-    | 'SelectAddresses'
     | 'AllWallets'
     | 'ApproveTransaction'
     | 'BuyInProgress'
