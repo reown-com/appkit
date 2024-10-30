cd7480d40b465cf8eca718032694fabfa5d1d3a5
enesozturk <enesozturk.d@gmail.com>
2024-10-30 18:09:55 +0300

    refactor: wagmi vue example configs

--- examples/vue-wagmi/vite.config.js
+++ examples/vue-wagmi/vite.config.js
@@ -2,5 +2,10 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['@tanstack/vue-query']
+    }
+  }
 })
