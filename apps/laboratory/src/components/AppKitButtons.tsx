5a9685940e2d97c7c7420cf16774ce686e674cc1
enesozturk <enesozturk.d@gmail.com>
2024-10-31 14:42:28 +0300

    chore: appkit hook hydration issue

--- apps/laboratory/src/components/AppKitButtons.tsx
+++ apps/laboratory/src/components/AppKitButtons.tsx
@@ -1,40 +1,7 @@
-import { useAppKit, useDisconnect, useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
-import { polygon, mainnet, solana, solanaTestnet, type AppKitNetwork } from '@reown/appkit/networks'
-import {
-  Stack,
-  Card,
-  CardHeader,
-  Heading,
-  CardBody,
-  Box,
-  StackDivider,
-  Button
-} from '@chakra-ui/react'
+import { Stack, Card, CardHeader, Heading, CardBody, Box, StackDivider } from '@chakra-ui/react'
+import { AppKitHooks } from './AppKitHooks'
 
 export function AppKitButtons() {
-  const { open } = useAppKit()
-  const { isConnected } = useAppKitAccount()
-  const { caipNetwork, switchNetwork } = useAppKitNetwork()
-  const { disconnect } = useDisconnect()
-
-  const isEIPNamespace = caipNetwork?.chainNamespace === 'eip155'
-  // eslint-disable-next-line no-nested-ternary
-  const networkToSwitch: AppKitNetwork = isEIPNamespace
-    ? caipNetwork?.id === polygon.id
-      ? mainnet
-      : polygon
-    : caipNetwork?.id === solana.id
-      ? solanaTestnet
-      : solana
-
-  function handleSwitchNetwork() {
-    if (isEIPNamespace) {
-      switchNetwork(networkToSwitch)
-    } else {
-      switchNetwork(networkToSwitch)
-    }
-  }
-
   return (
     <Card marginTop={10}>
       <CardHeader>
@@ -55,30 +22,7 @@ export function AppKitButtons() {
             </Heading>
             <w3m-network-button />
           </Box>
-          <Box>
-            <Heading size="xs" textTransform="uppercase" pb="2">
-              Hooks Interactions
-            </Heading>
-            <Box display="flex" alignItems="center" columnGap={3}>
-              <Button data-testid="w3m-open-hook-button" onClick={() => open()}>
-                Open
-              </Button>
-
-              <Button
-                data-testid="disconnect-hook-button"
-                isDisabled={!isConnected}
-                onClick={() => disconnect()}
-              >
-                Disconnect
-              </Button>
-
-              {caipNetwork ? (
-                <Button data-testid="switch-network-hook-button" onClick={handleSwitchNetwork}>
-                  Switch Network to {networkToSwitch?.name}
-                </Button>
-              ) : null}
-            </Box>
-          </Box>
+          <AppKitHooks />
         </Stack>
       </CardBody>
     </Card>
---
+++ apps/laboratory/src/components/AppKitHooks.tsx
@@ -0,0 +1,59 @@
+import { useAppKit, useDisconnect, useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
+import { polygon, mainnet, solana, solanaTestnet, type AppKitNetwork } from '@reown/appkit/networks'
+import { Heading, Box, Button } from '@chakra-ui/react'
+
+export function AppKitHooks() {
+  const { open } = useAppKit()
+  const { isConnected } = useAppKitAccount()
+  const { caipNetwork, switchNetwork } = useAppKitNetwork()
+  const { disconnect } = useDisconnect()
+
+  if (!caipNetwork) {
+    return null
+  }
+
+  const isEIPNamespace = caipNetwork?.chainNamespace === 'eip155'
+  // eslint-disable-next-line no-nested-ternary
+  const networkToSwitch: AppKitNetwork = isEIPNamespace
+    ? caipNetwork?.id === polygon.id
+      ? mainnet
+      : polygon
+    : caipNetwork?.id === solana.id
+      ? solanaTestnet
+      : solana
+
+  function handleSwitchNetwork() {
+    if (isEIPNamespace) {
+      switchNetwork(networkToSwitch)
+    } else {
+      switchNetwork(networkToSwitch)
+    }
+  }
+
+  return (
+    <Box>
+      <Heading size="xs" textTransform="uppercase" pb="2">
+        Hooks Interactions
+      </Heading>
+      <Box display="flex" alignItems="center" columnGap={3}>
+        <Button data-testid="w3m-open-hook-button" onClick={() => open()}>
+          Open
+        </Button>
+
+        <Button
+          data-testid="disconnect-hook-button"
+          isDisabled={!isConnected}
+          onClick={() => disconnect()}
+        >
+          Disconnect
+        </Button>
+
+        {caipNetwork ? (
+          <Button data-testid="switch-network-hook-button" onClick={handleSwitchNetwork}>
+            Switch Network to {networkToSwitch?.name}
+          </Button>
+        ) : null}
+      </Box>
+    </Box>
+  )
+}
