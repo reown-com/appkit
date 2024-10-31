35169120bf1ea407fe66d91d0ae181614140870c
Enes <enesozturk.d@gmail.com>
2024-10-31 09:28:10 +0300

    refactor: wagmi vue example configs (#3168)

--- examples/vue-wagmi/src/main.js
+++ examples/vue-wagmi/src/main.js
@@ -1,5 +1,12 @@
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
+import { WagmiPlugin } from '@wagmi/vue'
 import { createApp } from 'vue'
+import { wagmiAdapter } from './config'
 import App from './App.vue'
 
-const app = createApp(App)
-app.mount('#app')
+const queryClient = new QueryClient()
+
+createApp(App)
+  .use(WagmiPlugin, { config: wagmiAdapter.wagmiConfig })
+  .use(VueQueryPlugin, { queryClient })
+  .mount('#app')
