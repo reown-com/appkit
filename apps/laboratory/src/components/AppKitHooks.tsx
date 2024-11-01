f020cad4db41f00fa7c828cb4dad4623bc5c4266
enesozturk <enesozturk.d@gmail.com>
2024-11-01 16:41:19 +0300

    chore: remove new hook button

--- apps/laboratory/src/components/AppKitHooks.tsx
+++ apps/laboratory/src/components/AppKitHooks.tsx
@@ -1,26 +1,10 @@
-import { useAppKit, useDisconnect, useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
-import { polygon, mainnet, solana, solanaTestnet, type AppKitNetwork } from '@reown/appkit/networks'
+import { useAppKit, useDisconnect, useAppKitAccount } from '@reown/appkit/react'
 import { Heading, Box, Button } from '@chakra-ui/react'
 
 export function AppKitHooks() {
   const { open } = useAppKit()
   const { isConnected } = useAppKitAccount()
-  const { caipNetwork, switchNetwork } = useAppKitNetwork()
   const { disconnect } = useDisconnect()
-
-  function handleSwitchNetwork() {
-    const isEIPNamespace = caipNetwork?.chainNamespace === 'eip155'
-    // eslint-disable-next-line no-nested-ternary
-    const networkToSwitch: AppKitNetwork = isEIPNamespace
-      ? caipNetwork?.id === polygon.id
-        ? mainnet
-        : polygon
-      : caipNetwork?.id === solana.id
-        ? solanaTestnet
-        : solana
-
-    switchNetwork(networkToSwitch)
-  }
 
   return (
     <Box>
@@ -38,10 +22,6 @@ export function AppKitHooks() {
           onClick={() => disconnect()}
         >
           Disconnect
-        </Button>
-
-        <Button data-testid="switch-network-hook-button" onClick={handleSwitchNetwork}>
-          Switch Network
         </Button>
       </Box>
     </Box>
