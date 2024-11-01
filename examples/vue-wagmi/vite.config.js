74d6e2ace94e3d00844bc03ca3c850cb7da526d2
enesozturk <enesozturk.d@gmail.com>
2024-11-01 04:06:51 +0300

    chore: update vite app configs for vue-wagmi example

--- examples/vue-wagmi/vite.config.js
+++ examples/vue-wagmi/vite.config.js
@@ -1,11 +1,15 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vercel()],
   build: {
     rollupOptions: {
       external: ['@tanstack/vue-query']
     }
+  },
+  define: {
+    VITE_PROJECT_ID: process.env.VITE_PROJECT_ID
   }
 })
