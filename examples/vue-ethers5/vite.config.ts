a93bcfabc07108794a5ad38025db83b009fcccfb
enesozturk <enesozturk.d@gmail.com>
2024-11-01 19:27:32 +0300

    refactor: remove env configs from vite settings, update solana example

--- examples/vue-ethers5/vite.config.ts
+++ examples/vue-ethers5/vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  define: {
-    'import.meta.env.VITE_PROJECT_ID': JSON.stringify(process.env.VITE_PROJECT_ID)
-  }
+  plugins: [vue()]
 })
