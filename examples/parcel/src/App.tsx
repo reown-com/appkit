ae90da74bd7161e0193439fae5cc63788abbd98f
enesozturk <enesozturk.d@gmail.com>
2024-11-20 15:28:37 +0300

    chore: parcel app updates

--- examples/parcel/src/App.tsx
+++ examples/parcel/src/App.tsx
@@ -1,3 +1,27 @@
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+import { mainnet } from 'wagmi/chains'
+
+const wagmiConfig = defaultWagmiConfig({
+  chains: [mainnet],
+  projectId: '3bdbc796b351092d40d5d08e987f4eca',
+  metadata: {
+    name: 'Base',
+    description: 'Base',
+    url: 'https://base.org',
+    icons: ['https://base.org/logo.png']
+  }
+})
+
+createWeb3Modal({
+  wagmiConfig,
+  projectId: '3bdbc796b351092d40d5d08e987f4eca'
+})
+
 export function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <h1>Hello world1!</h1>
+      <w3m-button />
+    </div>
+  )
 }
--- examples/parcel/src/index.js
+++ examples/parcel/src/index.js
@@ -1,7 +1,4 @@
-export * from './App'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { App } from './App'
 
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
