29bc14cfcba591145e09587902cb8b2ab3ea711a
Chris Smith <1979423+chris13524@users.noreply.github.com>
2024-11-21 15:42:01 -0500

    chore: test ethers verify API + AppKit in iframe verify test case (#2831)
    Co-authored-by: Chris Smith <chris@reown.com>

---
+++ apps/laboratory/src/pages/library/ethers-verify-evil.tsx
@@ -0,0 +1,34 @@
+import { createAppKit } from '@reown/appkit/react'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { EthersTests } from '../../components/Ethers/EthersTests'
+import { AppKitButtons } from '../../components/AppKitButtons'
+import { ThemeStore } from '../../utils/StoreUtil'
+import { ConstantsUtil } from '../../utils/ConstantsUtil'
+import { EthersModalInfo } from '../../components/Ethers/EthersModalInfo'
+import { mainnet } from '@reown/appkit/networks'
+
+// Special project ID with https://malicious-app-verify-simulation.vercel.app/ as the verified domain and this domain is marked as a scam
+const projectId = '9d176efa3150a1df0a76c8c138b6b657'
+
+const modal = createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: ConstantsUtil.EvmNetworks,
+  defaultNetwork: mainnet,
+  projectId,
+  features: {
+    analytics: true
+  },
+  customWallets: ConstantsUtil.CustomWallets
+})
+
+ThemeStore.setModal(modal)
+
+export default function Ethers() {
+  return (
+    <>
+      <AppKitButtons />
+      <EthersModalInfo />
+      <EthersTests />
+    </>
+  )
+}
--- apps/laboratory/tests/shared/fixtures/w3m-wagmi-verify-valid-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/w3m-wagmi-verify-valid-fixture.ts
@@ -2,10 +2,10 @@ import type { ModalFixture } from './w3m-fixture'
 import { ModalPage } from '../pages/ModalPage'
 import { timingFixture } from './timing-fixture'
 
-export const testMVerifyDomainMismatch = timingFixture.extend<ModalFixture>({
+export const testMWagmiVerifyValid = timingFixture.extend<ModalFixture>({
   library: ['wagmi', { option: true }],
   modalPage: async ({ page, library }, use) => {
-    const modalPage = new ModalPage(page, library, 'verify-domain-mismatch')
+    const modalPage = new ModalPage(page, library, 'wagmi-verify-valid')
     await modalPage.load()
     await use(modalPage)
   }
