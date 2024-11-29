9319554368b397cd2fe83a50c19f37108fcb5195
MK <mago.khamidov@gmail.com>
2024-11-29 14:55:43 +0000

    chore: remove useAccount hook

--- examples/next-solana-app-router/src/components/ConnectButton.tsx
+++ examples/next-solana-app-router/src/components/ConnectButton.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useAppKitAccount } from '@reown/appkit/react'
-import { useAccount } from 'wagmi'
 
 const compactHash = (hash: string) => {
   return hash.slice(0, 7) + '...' + hash.slice(-5)
