099898598a371307a88d40d0a995e81b0674e9b6
MK <mago.khamidov@gmail.com>
2024-12-02 16:09:37 +0000

    chore: cleanup

--- apps/browser-extension/src/sleep.ts
+++
@@ -1,3 +0,0 @@
-export function sleep(ms = 1000) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
--- apps/browser-extension/src/toHex.ts
+++
@@ -1,7 +0,0 @@
-import { BigNumber } from '@ethersproject/bignumber'
-import { hexValue } from '@ethersproject/bytes'
-
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-export function toHex(stringToConvert: string | number | BigNumber) {
-  return hexValue(BigNumber.from(stringToConvert).toHexString())
-}
