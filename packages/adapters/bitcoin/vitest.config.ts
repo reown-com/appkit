db53c4aac310358ccfe93e7fd53a12ce02379c18
Luka Isailovic <lukaisailovic@gmail.com>
2024-12-03 13:22:54 +0100

    chore: add vitest.config.ts to projects

--- packages/adapters/bitcoin/vitest.config.ts
+++ packages/adapters/bitcoin/vitest.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    ...configDefaults,
+    globals: true,
     environment: 'jsdom'
   }
 })
