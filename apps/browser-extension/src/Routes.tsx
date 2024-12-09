096c2c57d595f0da3004105d0b94d7d29240d60d
MK <mago.khamidov@gmail.com>
2024-12-09 16:53:39 +0000

    fix: build errors

--- apps/browser-extension/src/Routes.tsx
+++ apps/browser-extension/src/Routes.tsx
@@ -1,33 +1,5 @@
-import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { useSnapshot } from 'valtio'
-import { PageController } from './controllers/PageController'
-import { ChooseNetwork } from './components/ChooseNetwork'
-import { ReactNode } from 'react'
-
-function HomeRoute({ children }: { children: ReactNode }) {
-  const { page } = useSnapshot(PageController.state)
-
-  if (!page) {
-    return <ChooseNetwork />
-  }
-
-  return children
-}
-
-const ROUTE_DATA = [
-  {
-    path: '/',
-    element: (
-      <HomeRoute>
-        <Home />
-      </HomeRoute>
-    )
-  }
-]
-
-const router = createHashRouter(ROUTE_DATA)
 
 export function Routes() {
-  return <RouterProvider router={router} />
+  return <Home />
 }
--- apps/browser-extension/src/pages/Home/index.tsx
+++ apps/browser-extension/src/pages/Home/index.tsx
@@ -6,12 +6,7 @@ import { privateKeyToAccount } from 'viem/accounts'
 import { AccountUtil } from '../../utils/AccountUtil'
 import { Keypair } from '@solana/web3.js'
 import { HelperUtil } from '../../utils/HelperUtil'
-import { useMemo, useState } from 'react'
-import { IconButton, IconButtonIconKey } from '../../components/IconButton'
 import { useSnapshot } from 'valtio'
-import { Token } from '../../components/Token'
-import { useBalance } from '../../hooks/useBalance'
-import Big from 'big.js'
 
 // EVM
 const { address } = privateKeyToAccount(AccountUtil.privateKeyEvm)
@@ -21,47 +16,11 @@ const keypair = Keypair.fromSecretKey(AccountUtil.privateKeySolana)
 const publicKey = keypair.publicKey
 
 export function Home() {
-  const [copied, setCopied] = useState(false)
   const { page } = useSnapshot(PageController.state)
 
   const isEVM = page === 'ethereum'
 
   const account = isEVM ? address : publicKey.toString()
-
-  const balance = useBalance(page ?? 'ethereum', account)
-  // eslint-disable-next-line new-cap
-  const formattedBalance = Big(balance).round(4).toString()
-
-  const iconOptions = useMemo(
-    () => ({
-      [copied ? 'checkmark' : 'copy']: {
-        text: 'Copy',
-        onClick: () => {
-          setCopied(true)
-          navigator.clipboard.writeText(account)
-          setTimeout(() => setCopied(false), 1500)
-        }
-      },
-      switch: {
-        text: 'Switch',
-        onClick: () => {
-          PageController.setPage(isEVM ? 'solana' : 'ethereum')
-        }
-      },
-      arrowRightUp: {
-        text: 'View',
-        onClick: () => {
-          window.open(
-            isEVM
-              ? `https://etherscan.io/address/${account}`
-              : `https://explorer.solana.com/address/${account}`,
-            '_blank'
-          )
-        }
-      }
-    }),
-    [isEVM, copied]
-  )
 
   return (
     <Box
@@ -93,19 +52,7 @@ export function Home() {
           <Text as="h1" textAlign="center" fontSize="18" color="accent010">
             {HelperUtil.shortenAddress(account)}
           </Text>
-          <Box display="flex" alignItems="center" gap="12">
-            {Object.entries(iconOptions).map(([icon, { text, onClick }]) => (
-              <IconButton
-                key={icon}
-                icon={icon as IconButtonIconKey}
-                text={text}
-                onClick={onClick}
-              />
-            ))}
-          </Box>
         </Box>
-
-        <Token token={isEVM ? 'ethereum' : 'solana'} balance={formattedBalance} />
       </Box>
     </Box>
   )
