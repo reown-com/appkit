e2f435a6184a32f69cd281cd190e5ab8efcbd20e
enesozturk <enesozturk.d@gmail.com>
2024-12-11 14:23:19 +0300

    chore: nuxt wagmi example

---
+++ examples/nuxt-app/app.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { createAppKit } from '@reown/appkit/vue'
+import { mainnet, polygon, base } from '@reown/appkit/networks'
+import { wagmiAdapter, projectId } from './config/wagmi'
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks: [mainnet, polygon, base],
+  projectId,
+  metadata: {
+    name: 'AppKit Nuxt Example',
+    description: 'AppKit Nuxt Example',
+    url: 'https://reown.com/appkit',
+    icons: ['https://avatars.githubusercontent.com/u/179229932?s=200&v=4']
+  }
+})
+</script>
+
+<template>
+  <div>
+    <appkit-button />
+  </div>
+</template>
---
+++ examples/nuxt-app/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      projectId: process.env.NUXT_PROJECT_ID
+    }
+  }
+})
