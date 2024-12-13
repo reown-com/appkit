a4a3e2406d84562b2f9b9eb600a4ca41b91ba9ad
tomiir <rocchitomas@gmail.com>
2024-12-12 18:23:01 -0600

    chore: add buy in progress tests

--- packages/scaffold-ui/src/views/w3m-buy-in-progress-view/index.ts
+++ packages/scaffold-ui/src/views/w3m-buy-in-progress-view/index.ts
@@ -106,7 +106,11 @@ export class W3mBuyInProgressView extends LitElement {
         </wui-flex>
 
         <wui-flex flexDirection="column" alignItems="center" gap="xs">
-          <wui-text variant="paragraph-500" color=${this.error ? 'error-100' : 'fg-100'}>
+          <wui-text
+            variant="paragraph-500"
+            color=${this.error ? 'error-100' : 'fg-100'}
+            data-testid="w3m-buy-in-progress-label"
+          >
             ${label}
           </wui-text>
           <wui-text align="center" variant="small-500" color="fg-200">${subLabel}</wui-text>
