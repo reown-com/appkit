5a2bdd91196c4c84ea2ec1f7205db717d74eb75e
enesozturk <enesozturk.d@gmail.com>
2024-12-16 13:40:14 +0300

    chore: format files

--- examples/sveltekit-ethers/src/lib/appkit.ts
+++ examples/sveltekit-ethers/src/lib/appkit.ts
@@ -1,39 +1,39 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { createAppKit } from '@reown/appkit';
-import { EthersAdapter } from '@reown/appkit-adapter-ethers';
-import { arbitrum, mainnet, optimism, polygon } from '@reown/appkit/networks';
-import { browser } from '$app/environment';
+import { createAppKit } from '@reown/appkit'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { arbitrum, mainnet, optimism, polygon } from '@reown/appkit/networks'
+import { browser } from '$app/environment'
 
 // Only initialize in browser environment
-let modal: ReturnType<typeof createAppKit> | undefined;
+let modal: ReturnType<typeof createAppKit> | undefined
 
 if (browser) {
-	const projectId = import.meta.env.VITE_PROJECT_ID;
-	if (!projectId) {
-		throw new Error('VITE_PROJECT_ID is not set');
-	}
+  const projectId = import.meta.env.VITE_PROJECT_ID
+  if (!projectId) {
+    throw new Error('VITE_PROJECT_ID is not set')
+  }
 
-	// Create adapter
-	const ethersAdapter = new EthersAdapter();
+  // Create adapter
+  const ethersAdapter = new EthersAdapter()
 
-	// Initialize AppKit
-	modal = createAppKit({
-		adapters: [ethersAdapter],
-		networks: [arbitrum, mainnet, optimism, polygon],
-		defaultNetwork: arbitrum,
-		projectId,
-		features: {
-			email: false,
-			socials: false
-		},
-		themeMode: 'light',
-		metadata: {
-			name: 'SvelteKit Example',
-			description: 'SvelteKit Example using Ethers adapter',
-			url: 'https://reown.com/appkit',
-			icons: ['https://avatars.githubusercontent.com/u/179229932?s=200&v=4']
-		}
-	});
+  // Initialize AppKit
+  modal = createAppKit({
+    adapters: [ethersAdapter],
+    networks: [arbitrum, mainnet, optimism, polygon],
+    defaultNetwork: arbitrum,
+    projectId,
+    features: {
+      email: false,
+      socials: false
+    },
+    themeMode: 'light',
+    metadata: {
+      name: 'SvelteKit Example',
+      description: 'SvelteKit Example using Ethers adapter',
+      url: 'https://reown.com/appkit',
+      icons: ['https://avatars.githubusercontent.com/u/179229932?s=200&v=4']
+    }
+  })
 }
 
-export { modal };
+export { modal }
