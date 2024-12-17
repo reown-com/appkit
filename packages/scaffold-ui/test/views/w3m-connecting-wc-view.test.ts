ab30b0a2efc6153a874816cfc8feb07813e35477
MK <mago.khamidov@gmail.com>
2024-12-17 18:38:29 +0000

    chore: add test for mobile

--- packages/scaffold-ui/test/views/w3m-connecting-wc-view.test.ts
+++ packages/scaffold-ui/test/views/w3m-connecting-wc-view.test.ts
@@ -5,6 +5,7 @@ import { elementUpdated, fixture } from '@open-wc/testing'
 import { html } from 'lit'
 import type { WcWallet } from '@reown/appkit-core'
 import { HelpersUtil } from '../utils/HelpersUtil'
+import type { WuiTabs } from '@reown/appkit-ui'
 
 // --- Constants ---------------------------------------------------- //
 
@@ -14,10 +15,6 @@ const WALLET = {
 } as WcWallet
 
 describe('W3mConnectingWcView - Render', () => {
-  beforeAll(() => {
-    vi.spyOn(CoreHelperUtil, 'isMobile').mockReturnValue(false)
-  })
-
   test.skip('should render walletConnect widget if no wallet is give', async () => {
     const element: W3mConnectingWcView = await fixture(
       html`<w3m-connecting-wc-view></w3m-connecting-wc-view>`
@@ -26,14 +23,15 @@ describe('W3mConnectingWcView - Render', () => {
     expect(HelpersUtil.querySelect(element, 'w3m-connecting-wc-qrcode')).not.toBeNull()
   })
 
-  test('should render browser widget if rdns or injected ids are present', async () => {
+  test('should render web and browser platforms if rdns and webapp_link is present', async () => {
+    vi.spyOn(CoreHelperUtil, 'isMobile').mockReturnValue(false)
+
     vi.spyOn(RouterController, 'state', 'get').mockReturnValue({
       ...RouterController.state,
       data: {
         wallet: {
           ...WALLET,
           rdns: 'metamask.io',
-          injected: [{ injected_id: 'isMetamask' }],
           webapp_link: 'https://metamask.io'
         }
       }
@@ -44,6 +42,53 @@ describe('W3mConnectingWcView - Render', () => {
     )
 
     const w3mConnectingHeader = HelpersUtil.querySelect(element, 'w3m-connecting-header')
+
     expect(w3mConnectingHeader).not.toBeNull()
+
+    const wuiTabs = HelpersUtil.querySelect(w3mConnectingHeader, 'wui-tabs') as WuiTabs
+
+    expect(wuiTabs).not.toBeNull()
+
+    const tabsProperty = wuiTabs.tabs
+
+    expect(tabsProperty).toStrictEqual([
+      { label: 'Webapp', icon: 'browser', platform: 'web' },
+      { label: 'Browser', icon: 'extension', platform: 'unsupported' }
+    ])
+  })
+
+  test('should render web,mo browser platforms if rdns and webapp_link is present', async () => {
+    vi.spyOn(CoreHelperUtil, 'isMobile').mockReturnValue(true)
+
+    vi.spyOn(RouterController, 'state', 'get').mockReturnValue({
+      ...RouterController.state,
+      data: {
+        wallet: {
+          ...WALLET,
+          webapp_link: 'https://metamask.io',
+          mobile_link: 'https://metamask.io'
+        }
+      }
+    })
+
+    const element: W3mConnectingWcView = await fixture(
+      html`<w3m-connecting-wc-view></w3m-connecting-wc-view>`
+    )
+
+    const w3mConnectingHeader = HelpersUtil.querySelect(element, 'w3m-connecting-header')
+
+    expect(w3mConnectingHeader).not.toBeNull()
+
+    const wuiTabs = HelpersUtil.querySelect(w3mConnectingHeader, 'wui-tabs') as WuiTabs
+
+    expect(wuiTabs).not.toBeNull()
+
+    const tabsProperty = wuiTabs.tabs
+
+    expect(tabsProperty).toStrictEqual([
+      { label: 'Mobile', icon: 'mobile', platform: 'mobile' },
+      { label: 'Browser', icon: 'extension', platform: 'unsupported' },
+      { label: 'Webapp', icon: 'browser', platform: 'web' }
+    ])
   })
 })
