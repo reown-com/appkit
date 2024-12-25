aee4b667ce91b14c181d8f38aa3befbd715db485
enesozturk <enesozturk.d@gmail.com>
2024-12-25 21:25:08 +0300

    chore: update styles

--- examples/vue-wagmi-solana/src/App.vue
+++ examples/vue-wagmi-solana/src/App.vue
@@ -4,9 +4,9 @@
       <img
         :src="themeState.themeMode === 'dark' ? '/reown-logo-white.png' : '/reown-logo.png'"
         alt="Reown"
-        width="200"
+        width="150"
       />
-      <img src="/appkit-logo.png" alt="Reown" width="200" />
+      <img src="/appkit-logo.png" alt="Reown" width="150" />
     </div>
 
     <h1 class="page-title">Vue Wagmi + Solana Example</h1>
@@ -17,14 +17,21 @@
     </div>
 
     <ActionButtonList />
-    <div className="advice">
-      <p>
-        This projectId only works on localhost. Go to
-        <a href="https://cloud.reown.com" target="_blank" rel="Reown Cloud">Reown Cloud</a> to get
-        your own.
-      </p>
-    </div>
+
     <InfoList />
+
+    <div className="footer">
+      <div class="footer-links">
+        <a href="https://reown.com" target="_blank" rel="noreferrer">Reown</a>
+        •
+        <a href="https://docs.reown.com" target="_blank" rel="noreferrer">Docs</a>
+        •
+        <a href="https://github.com/reown-com/appkit" target="_blank" rel="noreferrer">GitHub</a>
+        •
+        <a href="https://cloud.reown.com" target="_blank" rel="noreferrer">Cloud</a>
+      </div>
+      <p class="warning">This project ID only works on localhost. Go to Cloud to get your own.</p>
+    </div>
   </div>
 </template>
 
