066819dc0984ce6e80928dc85ce9984d64e5cc8b
Enes <enesozturk.d@gmail.com>
2024-12-27 16:21:41 +0300

    refactor: unify examples with ui and functionality (#3538)

--- examples/react-solana/src/App.tsx
+++ examples/react-solana/src/App.tsx
@@ -1,76 +1,34 @@
-import {
-  createAppKit,
-  useAppKit,
-  useAppKitEvents,
-  useAppKitState,
-  useAppKitTheme
-} from '@reown/appkit/react'
-import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
-import {
-  PhantomWalletAdapter,
-  HuobiWalletAdapter,
-  SolflareWalletAdapter,
-  TrustWalletAdapter
-} from '@solana/wallet-adapter-wallets'
-import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
+import { useAppKitTheme } from './config'
 
-// @ts-expect-error 1. Get projectId
-const projectId = import.meta.env.VITE_PROJECT_ID
-if (!projectId) {
-  throw new Error('VITE_PROJECT_ID is not set')
-}
-
-const networks = [solana, solanaTestnet, solanaDevnet]
-
-// 3. Create modal
-createAppKit({
-  adapters: [
-    new SolanaAdapter({
-      wallets: [
-        new HuobiWalletAdapter(),
-        new PhantomWalletAdapter(),
-        new SolflareWalletAdapter(),
-        new TrustWalletAdapter()
-      ]
-    })
-  ],
-  networks,
-  metadata: {
-    name: 'AppKit React Example',
-    description: 'AppKit React Example',
-    url: '',
-    icons: []
-  },
-  projectId,
-  themeMode: 'light',
-  themeVariables: {
-    '--w3m-color-mix': '#00DCFF',
-    '--w3m-color-mix-strength': 20
-  }
-})
+import ActionButtonList from './components/ActionButton'
+import InfoList from './components/InfoList'
+import Footer from './components/Footer'
 
 export default function App() {
-  // 4. Use modal hook
-  const modal = useAppKit()
-  const state = useAppKitState()
-  const { themeMode, themeVariables, setThemeMode } = useAppKitTheme()
-  const events = useAppKitEvents()
+  const { themeMode } = useAppKitTheme()
+  document.documentElement.className = themeMode
 
   return (
-    <>
-      <appkit-button />
-      <appkit-network-button />
-      <appkit-connect-button />
-      <appkit-account-button />
+    <div className="page-container">
+      <div className="logo-container">
+        <img
+          src={themeMode === 'dark' ? '/reown-logo-white.png' : '/reown-logo.png'}
+          alt="Reown"
+          width="150"
+        />
+        <img src="/appkit-logo.png" alt="Reown" width="150" />
+      </div>
 
-      <button onClick={() => modal.open()}>Connect Wallet</button>
-      <button onClick={() => modal.open({ view: 'Networks' })}>Choose Network</button>
-      <button onClick={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}>
-        Toggle Theme Mode
-      </button>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-      <pre>{JSON.stringify({ themeMode, themeVariables }, null, 2)}</pre>
-      <pre>{JSON.stringify(events, null, 2)}</pre>
-    </>
+      <h1 className="page-title">React Solana Example</h1>
+
+      <div className="appkit-buttons-container">
+        <appkit-button />
+        <appkit-network-button />
+      </div>
+
+      <ActionButtonList />
+      <InfoList />
+      <Footer />
+    </div>
   )
 }
---
+++ examples/sveltekit-ethers/src/lib/components/InfoList.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { accountState, networkState, appKitState, themeState, events, walletInfo } from '$lib/store';
+</script>
+
+<div class="code-container-wrapper">
+  <section class="code-container">
+    <h2 class="code-container-title">useAppKitAccount()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($accountState, null, 2)}</pre>
+    </div>
+  </section>
+
+  <section class="code-container">
+    <h2 class="code-container-title">useAppKitNetwork()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($networkState, null, 2)}</pre>
+    </div>
+  </section>
+
+  <section class="code-container">
+    <h2 class="code-container-title">useAppKitTheme()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($themeState, null, 2)}</pre>
+    </div>
+  </section>
+
+  <section class="code-container">
+    <h2 class="code-container-title">useAppKitState()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($appKitState, null, 2)}</pre>
+    </div>
+  </section>
+
+  <section class="code-container">
+    <h2 class="code-container-title">useAppKitEvents()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($events, null, 2)}</pre>
+    </div>
+  </section>
+
+  <section class="code-container">
+    <h2 class="code-container-title">useWalletInfo()</h2>
+    <div class="code-container-content">
+      <pre>{JSON.stringify($walletInfo, null, 2)}</pre>
+    </div>
+  </section>
+</div>
