35ba2f20a33ec6493d61d8f8ea71977723d8115f
MK <mago.khamidov@gmail.com>
2024-12-30 17:20:09 +0000

    chore: add changeset

---
+++ .changeset/weak-buses-raise.md
@@ -0,0 +1,35 @@
+---
+'@reown/appkit': patch
+'@reown/appkit-wallet': patch
+'@reown/appkit-core': patch
+'@reown/appkit-adapter-bitcoin': patch
+'@reown/appkit-adapter-ethers': patch
+'@reown/appkit-adapter-ethers5': patch
+'@reown/appkit-adapter-solana': patch
+'@reown/appkit-adapter-wagmi': patch
+'@reown/appkit-utils': patch
+'@reown/appkit-cdn': patch
+'@reown/appkit-cli': patch
+'@reown/appkit-common': patch
+'@reown/appkit-experimental': patch
+'@reown/appkit-polyfills': patch
+'@reown/appkit-scaffold-ui': patch
+'@reown/appkit-siwe': patch
+'@reown/appkit-siwx': patch
+'@reown/appkit-ui': patch
+'@reown/appkit-wallet-button': patch
+---
+
+Added email and other social info to `useAppKitAccount` hook.
+
+**Example usage**
+
+```tsx
+import { useAppKitAccount } from '@reown/appkit/react'
+
+export function YourApp() {
+  const { isConnected, user } = useAppKitAccount()
+
+  return <div>{isConnected ? user.email : 'Not connected'}</div>
+}
+```
--- apps/laboratory/src/components/AppKitNetworkInfo.tsx
+++ apps/laboratory/src/components/AppKitNetworkInfo.tsx
@@ -1,43 +1,7 @@
-import * as React from 'react'
-import { useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
-import {
-  StackDivider,
-  Card,
-  CardHeader,
-  Heading,
-  CardBody,
-  Box,
-  Stack,
-  Text
-} from '@chakra-ui/react'
+import { useAppKitAccount } from '@reown/appkit/react'
 
-export function AppKitNetworkInfo() {
-  const { address } = useAppKitAccount()
-  const { chainId } = useAppKitNetwork()
+export function YourApp() {
+  const { isConnected, user } = useAppKitAccount()
 
-  return (
-    <Card marginTop={10} marginBottom={10}>
-      <CardHeader>
-        <Heading size="md">Account Information</Heading>
-      </CardHeader>
-
-      <CardBody>
-        <Stack divider={<StackDivider />} spacing="4">
-          <Box>
-            <Heading size="xs" textTransform="uppercase" pb="2">
-              Address
-            </Heading>
-            <Text data-testid="w3m-address">{address || '-'}</Text>
-          </Box>
-
-          <Box>
-            <Heading size="xs" textTransform="uppercase" pb="2">
-              Chain Id
-            </Heading>
-            <Text data-testid="w3m-chain-id">{chainId}</Text>
-          </Box>
-        </Stack>
-      </CardBody>
-    </Card>
-  )
+  return <div>{isConnected ? user.email : 'Not connected'}</div>
 }
