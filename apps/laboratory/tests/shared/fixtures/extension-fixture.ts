d9a25ee8455ea2214f49f5c5c99b0954162d0053
MK <mago.khamidov@gmail.com>
2025-01-13 14:08:23 +0000

    chore: cleanup

--- apps/laboratory/tests/shared/fixtures/extension-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/extension-fixture.ts
@@ -16,18 +16,12 @@ export const extensionFixture = base.extend<ExtensionFixture>({
     const pathToExtension = path.resolve(__dirname, '../../../../browser-extension/dist')
 
     const isHeadless = process.env['HEADLESS'] === 'true'
-    // eslint-disable-next-line no-console
-    console.log('isHeadless', isHeadless)
 
     const context = await chromium.launchPersistentContext('', {
       headless: isHeadless,
       args: [
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`,
-        '--no-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-gpu',
-        '--disable-setuid-sandbox',
         ...(isHeadless ? ['--headless=new'] : [])
       ]
     })
@@ -35,19 +29,11 @@ export const extensionFixture = base.extend<ExtensionFixture>({
     await use(context)
   },
   extensionId: async ({ context }, use) => {
-    // eslint-disable-next-line no-console
-    console.log('extensionId (1)')
     let [background] = context.serviceWorkers()
-    // eslint-disable-next-line no-console
-    console.log('extensionId (2)')
+
     if (!background) {
       background = await context.waitForEvent('serviceworker')
-      // eslint-disable-next-line no-console
-      console.log('extensionId (3)')
     }
-
-    // eslint-disable-next-line no-console
-    console.log('extensionId (4)', background)
 
     const extensionId = background.url().split('/')[2]
 
