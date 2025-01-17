144cf64c00befcbd1e30336a2b5d3b1990496db7
enesozturk <enesozturk.d@gmail.com>
2025-01-17 19:10:15 +0300

    chore: remove wagmi related things

--- apps/builder/providers/appkit-provider.tsx
+++ apps/builder/providers/appkit-provider.tsx
@@ -2,15 +2,11 @@
 
 import React, { type ReactNode } from 'react'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ref } from 'valtio/vanilla'
-import { type Config, WagmiProvider, cookieToInitialState } from 'wagmi'
 
-import { ConstantsUtil } from '@reown/appkit-core'
-import { mainnet } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
 
-import { networks, wagmiAdapter } from '@/lib/config'
+import { appKitConfigs, initialConfig } from '@/lib/config'
 import { ThemeStore } from '@/lib/theme-store'
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
