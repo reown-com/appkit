0760198226f7683e67adf77f9625a5691b34db6f
tomiir <rocchitomas@gmail.com>
2025-01-24 15:30:26 +0100

    chore: add solana and bitcoin

--- apps/laboratory/src/pages/library/multichain-basic.tsx
+++ apps/laboratory/src/pages/library/multichain-basic.tsx
@@ -1,4 +1,4 @@
-import { type AppKitNetwork, defineChain, mainnet } from '@reown/appkit/networks'
+import { type AppKitNetwork, mainnet } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
 
 import { AppKitButtons } from '../../components/AppKitButtons'
@@ -7,19 +7,7 @@ import { UpaTests } from '../../components/UPA/UpaTests'
 import { ConstantsUtil } from '../../utils/ConstantsUtil'
 import { ThemeStore } from '../../utils/StoreUtil'
 
-const polkadot = defineChain({
-  id: '91b171bb158e2d3848fa23a9f1c25182',
-  name: 'Polkadot',
-  network: 'polkadot',
-  nativeCurrency: { name: 'Polkadot', symbol: 'DOT', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.polkadot.io'] }
-  },
-  chainNamespace: 'polkadot',
-  caipNetworkId: 'polkadot:mainnet'
-})
-
-const networks = [...ConstantsUtil.EvmNetworks, polkadot]
+const networks = [...ConstantsUtil.AllNetworks, ...ConstantsUtil.BitcoinNetworks]
 
 const modal = createAppKit({
   networks: networks as [AppKitNetwork, ...AppKitNetwork[]],
