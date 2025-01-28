d22ff0bcc282edcc3528827d627889ee6118eb20
tomiir <rocchitomas@gmail.com>
2025-01-28 17:23:54 +0100

    chore: add bundle analyzer to wagmi example

--- examples/react-wagmi/vite.config.js
+++ examples/react-wagmi/vite.config.js
@@ -1,6 +1,14 @@
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'bundle-analysis.html',
+      gzipSize: true,
+      title: 'Bundle Analysis'
+    })
+  ]
 })
