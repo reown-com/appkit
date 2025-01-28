8fe744027d2bb365918fb8affa2cff8c867e317a
MK <mago.khamidov@gmail.com>
2025-01-28 14:05:04 +0000

    chore: cleanup

--- examples/react-wagmi/src/App.tsx
+++ examples/react-wagmi/src/App.tsx
@@ -1,23 +1,12 @@
 import { createAppKit } from '@reown/appkit-basic'
-import {
-  type AppKitNetwork,
-  arbitrum,
-  mainnet,
-  optimism,
-  polygon
-} from '@reown/appkit-basic/networks'
+import type { AppKitNetwork } from '@reown/appkit-basic/networks'
+import * as networks from '@reown/appkit-basic/networks'
 
 export const projectId = 'b56e18d47c72ab683b10814fe9495694' // this is a public projectId only to use on localhost
 
-const networks = [mainnet, polygon, arbitrum, optimism] as unknown as unknown as [
-  AppKitNetwork,
-  ...AppKitNetwork[]
-]
-
 // Create modal
-createAppKit({
-  adapters: [],
-  networks,
+const appkit = createAppKit({
+  networks: networks as unknown as [AppKitNetwork, ...AppKitNetwork[]],
   metadata: {
     name: 'AppKit React Example',
     description: 'AppKit React Wagmi Example',
@@ -32,5 +21,10 @@ createAppKit({
   }
 })
 export default function App() {
-  return null
+  return (
+    <>
+      <h2>hey</h2>
+      <button onClick={() => appkit.open()}>appkit</button>
+    </>
+  )
 }
--- packages/appkit-basic/exports/index.ts
+++ packages/appkit-basic/exports/index.ts
@@ -6,9 +6,7 @@ import { PACKAGE_VERSION } from './constants.js'
 
 // -- Utils & Other -----------------------------------------------------
 export * from '../src/utils/index.js'
-export type * from '@reown/appkit-core'
 export type { CaipNetwork, CaipAddress, CaipNetworkId } from '@reown/appkit-common'
-export { CoreHelperUtil, AccountController } from '@reown/appkit-core'
 
 export type CreateAppKit = Omit<AppKitOptions, 'sdkType' | 'sdkVersion'>
 
