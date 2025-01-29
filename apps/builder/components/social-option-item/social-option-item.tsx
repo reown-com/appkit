6c1d049c9e8aece29a7543f4790b26f3b6c5889d
tomiir <rocchitomas@gmail.com>
2025-01-29 15:41:53 +0100

    chore: use new export

--- apps/builder/components/social-option-item/social-option-item.tsx
+++ apps/builder/components/social-option-item/social-option-item.tsx
@@ -5,7 +5,7 @@ import type { Transform } from '@dnd-kit/utilities'
 import classNames from 'classnames'
 
 import { SocialProvider } from '@reown/appkit-core'
-import '@reown/appkit-ui'
+import '@reown/appkit-ui/jsx'
 
 import { useAppKitContext } from '@/hooks/use-appkit'
 
