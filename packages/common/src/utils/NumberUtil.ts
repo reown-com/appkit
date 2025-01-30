22efd0a5d67cd5e26f08a63dc4dbb7b3e8dff091
tomiir <rocchitomas@gmail.com>
2025-01-30 16:06:39 +0100

    chore: initial replace of bignumber for big.js

--- packages/common/src/utils/NumberUtil.ts
+++ packages/common/src/utils/NumberUtil.ts
@@ -1,27 +1,48 @@
-import * as BigNumber from 'bignumber.js'
+import Big from 'big.js'
 
 export const NumberUtil = {
-  bigNumber(value: BigNumber.BigNumber.Value) {
-    return new BigNumber.BigNumber(value)
+  bigNumber(value: string | number) {
+    return new Big(value)
   },
 
   /**
-   * Multiply two numbers represented as strings with BigNumber to handle decimals correctly
+   * Multiply two numbers represented as strings with Big to handle decimals correctly
    * @param a string
    * @param b string
    * @returns
    */
-  multiply(a: BigNumber.BigNumber.Value | undefined, b: BigNumber.BigNumber.Value | undefined) {
+  multiply(a: string | number | undefined, b: string | number | undefined) {
     if (a === undefined || b === undefined) {
-      // eslint-disable-next-line new-cap
-      return BigNumber.BigNumber(0)
+      return new Big(0)
     }
 
-    const aBigNumber = new BigNumber.BigNumber(a)
-    const bBigNumber = new BigNumber.BigNumber(b)
+    const aBig = new Big(a)
+    const bBig = new Big(b)
 
-    return aBigNumber.multipliedBy(bBigNumber)
+    return aBig.times(bBig)
   },
+
+  /**
+   * Divide two numbers using Big to handle decimals correctly
+   * @param a string
+   * @param b string
+   * @returns
+   */
+  divide(a: string | number | undefined, b: string | number | undefined) {
+    if (a === undefined || b === undefined) {
+      return new Big(0)
+    }
+
+    const aBig = new Big(a)
+    const bBig = new Big(b)
+
+    if (bBig.eq(0)) {
+      throw new Error('Division by zero')
+    }
+
+    return aBig.div(bBig)
+  },
+
   /**
    * Format the given number or string to human readable numbers with the given number of decimals
    * @param value - The value to format. It could be a number or string. If it's a string, it will be parsed to a float then formatted.
@@ -34,17 +55,12 @@ export const NumberUtil = {
     }
 
     if (typeof value === 'number') {
-      return value.toLocaleString('en-US', {
-        maximumFractionDigits: decimals,
-        minimumFractionDigits: decimals
-      })
+      return value.toFixed(decimals)
     }
 
-    return parseFloat(value).toLocaleString('en-US', {
-      maximumFractionDigits: decimals,
-      minimumFractionDigits: decimals
-    })
+    return new Big(value).toFixed(decimals)
   },
+
   /**
    * Parse a formatted local string back to a number
    * @param value - The formatted string to parse
@@ -56,6 +72,6 @@ export const NumberUtil = {
     }
 
     // Remove any commas used as thousand separators and parse the float
-    return parseFloat(value.replace(/,/gu, ''))
+    return Number(new Big(value.replace(/,/gu, '')))
   }
 }
