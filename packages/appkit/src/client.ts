8edd53b8b27886543e6b314909f3dd44a560902e
enesozturk <enesozturk.d@gmail.com>
2025-01-30 12:37:28 +0300

    fix: handle connector controller state changes while updating account data for `subscribeAccount`

--- packages/appkit/src/client.ts
+++ packages/appkit/src/client.ts
@@ -290,8 +290,9 @@ export class AppKit {
   }
 
   public subscribeAccount(callback: (newState: UseAppKitAccountReturn) => void) {
-    const authConnector = ConnectorController.getAuthConnector()
     function updateVal() {
+      const authConnector = ConnectorController.getAuthConnector()
+
       callback({
         allAccounts: AccountController.state.allAccounts,
         caipAddress: ChainController.state.activeCaipAddress,
@@ -311,6 +312,7 @@ export class AppKit {
 
     ChainController.subscribe(updateVal)
     AccountController.subscribe(updateVal)
+    ConnectorController.subscribe(updateVal)
   }
 
   public subscribeNetwork(
