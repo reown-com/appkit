7190370523aeb9a0a05e8a13356f09d66c6d21d3
tomiir <rocchitomas@gmail.com>
2025-01-31 13:43:27 +0100

    fix: sign client example

--- examples/html-ak-basic-sign-client/src/main.js
+++ examples/html-ak-basic-sign-client/src/main.js
@@ -6,7 +6,7 @@ import { bitcoin, mainnet, polygon, solana } from '@reown/appkit/networks'
 
 // Constants
 const PROJECT_ID = import.meta.env.VITE_PROJECT_ID || 'b56e18d47c72ab683b10814fe9495694'
-const REQUIRED_NAMESPACES = {
+const OPTIONAL_NAMESPACES = {
   eip155: {
     methods: [
       'eth_sendTransaction',
@@ -37,31 +37,12 @@ let session
 let account
 let network
 
-let networkState = {}
-let accountState = {}
-
 const networks = [mainnet, polygon, solana, bitcoin]
 async function initialize() {
   signClient = await SignClient.init({ projectId: PROJECT_ID })
   modal = createAppKit({
     projectId: PROJECT_ID,
     networks
-  })
-
-  modal.subscribeAccount(state => {
-    accountState = state
-  })
-
-  modal.subscribeNetwork(state => {
-    networkState = state
-  })
-
-  modal.subscribeProviders(state => {
-    providers = state
-  })
-
-  document.getElementById('switch-to-ethereum')?.addEventListener('click', () => {
-    modal.switchNetwork(mainnet)
   })
 
   document.getElementById('toggle-theme')?.addEventListener('click', () => {
@@ -71,20 +52,28 @@ async function initialize() {
     updateTheme(newTheme)
   })
 
-  document.getElementById('switch-network-eth')?.addEventListener('click', network => {
+  document.getElementById('switch-network-eth')?.addEventListener('click', () => {
     modal.switchNetwork(mainnet)
+    network = 'eip155:1'
+    account = session?.namespaces?.eip155?.accounts?.[0]?.split(':')[2]
   })
 
-  document.getElementById('switch-network-polygon')?.addEventListener('click', network => {
+  document.getElementById('switch-network-polygon')?.addEventListener('click', () => {
     modal.switchNetwork(polygon)
+    network = 'eip155:137'
+    account = session?.namespaces?.eip155?.accounts?.[0]?.split(':')[2]
   })
 
-  document.getElementById('switch-network-solana')?.addEventListener('click', network => {
+  document.getElementById('switch-network-solana')?.addEventListener('click', () => {
     modal.switchNetwork(solana)
+    network = solana.caipNetworkId
+    account = session?.namespaces?.solana?.accounts?.[0].split(':')[2]
   })
 
-  document.getElementById('switch-network-bitcoin')?.addEventListener('click', network => {
+  document.getElementById('switch-network-bitcoin')?.addEventListener('click', () => {
     modal.switchNetwork(bitcoin)
+    network = bitcoin.caipNetworkId
+    account = session?.namespaces?.bip122?.accounts?.[0].split(':')[2]
   })
 
   // Get last session if exists
@@ -157,7 +146,7 @@ function clearState() {
 function setupEventListeners() {
   document.getElementById('connect')?.addEventListener('click', async () => {
     const { uri, approval } = await signClient.connect({
-      requiredNamespaces: REQUIRED_NAMESPACES
+      optionalNamespaces: OPTIONAL_NAMESPACES
     })
 
     if (uri) {
@@ -187,18 +176,18 @@ async function getPayload() {
       method: 'solana_signMessage',
       params: {
         message: base58.encode(new TextEncoder().encode('Hello Appkit!')),
-        pubkey: accountState.address
+        pubkey: account
       }
     },
     eip155: {
       method: 'personal_sign',
-      params: [accountState.address, 'Hello AppKit!']
+      params: [account, 'Hello AppKit!']
     },
     bip122: {
       method: 'signMessage',
       params: {
         message: 'Hello AppKit!',
-        account: accountState.address
+        account: account
       }
     },
     polkadot: {
@@ -207,7 +196,7 @@ async function getPayload() {
         transactionPayload: {
           specVersion: '0x00002468',
           transactionVersion: '0x0000000e',
-          address: `${accountState.address}`,
+          address: `${account}`,
           blockHash: '0x554d682a74099d05e8b7852d19c93b527b5fae1e9e1969f6e1b82a2f09a14cc9',
           blockNumber: '0x00cb539c',
           era: '0xc501',
@@ -227,19 +216,20 @@ async function getPayload() {
           tip: '0x00000000000000000000000000000000',
           version: 4
         },
-        address: accountState.address
+        address: account
       }
     }
   }
 
-  const payload = map[networkState?.caipNetwork?.chainNamespace || '']
+  const [namespace] = network.split(':')
+  const payload = map[namespace || '']
 
   return payload
 }
 
 async function signMessage() {
   try {
-    if (!signClient || !accountState.address) {
+    if (!signClient || !account) {
       throw Error('User is disconnected')
     }
 
@@ -249,7 +239,11 @@ async function signMessage() {
       throw Error('Chain not supported by laboratory')
     }
 
-    const signature = await signClient.request(payload, networkState?.caipNetwork?.caipNetworkId)
+    const sig = await signClient.request({
+      topic: session.topic,
+      chainId: network,
+      request: payload
+    })
 
     console.log({
       title: 'Signed successfully',
