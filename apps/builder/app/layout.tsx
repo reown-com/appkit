374540f54d28cbbf418b4406774e7e53081fa4f5
enesozturk <enesozturk.d@gmail.com>
2025-02-04 16:56:31 +0300

    chore: add google analytics

--- apps/builder/app/layout.tsx
+++ apps/builder/app/layout.tsx
@@ -1,7 +1,7 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
 import { headers } from 'next/headers'
-import { Toaster } from 'sonner'
 
 import { khTeka } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
@@ -76,6 +76,7 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
             <ContextProvider>{children}</ContextProvider>
           </AppKitProvider>
         </ThemeProvider>
+        <GoogleAnalytics gaId="G-X4S1RDVEGE" />
       </body>
     </html>
   )
