5900a70dcb6508ecc27dfd629dc942224c3b1a56
tomiir <rocchitomas@gmail.com>
2025-02-05 17:39:01 +0100

    fix:  deeplink handling on all wallets list and recent wallets (#3803)

--- packages/appkit-utils/exports/index.ts
+++ packages/appkit-utils/exports/index.ts
@@ -7,7 +7,6 @@ export { PresetsUtil } from '../src/PresetsUtil.js'
 export { HelpersUtil } from '../src/HelpersUtil.js'
 export { ErrorUtil } from '../src/ErrorUtil.js'
 export { LoggerUtil } from '../src/LoggerUtil.js'
-export { MobileWalletUtil } from '../src/MobileWallet.js'
 export { CaipNetworksUtil, getBlockchainApiRpcUrl } from '../src/CaipNetworkUtil.js'
 export type { SocialProvider } from '../src/TypeUtil.js'
 
