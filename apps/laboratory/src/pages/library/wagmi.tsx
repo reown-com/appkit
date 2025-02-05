afa12f7bdc26237406c8349e62189fc383787d10
MK <mago.khamidov@gmail.com>
2025-02-05 13:44:08 +0000

    chore: remove excludedWalletIds

--- apps/laboratory/src/pages/library/wagmi.tsx
+++ apps/laboratory/src/pages/library/wagmi.tsx
@@ -22,11 +22,6 @@ const modal = createAppKit({
   adapters: [wagmiAdapter],
   networks: ConstantsUtil.EvmNetworks,
   projectId: ConstantsUtil.ProjectId,
-  excludeWalletIds: [
-    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369'
-  ],
   features: {
     analytics: true
   },
