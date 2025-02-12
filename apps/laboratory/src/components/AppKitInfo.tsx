d07bc59072a976a7db3665f15b74a9155eeaccb1
Luka Isailovic <lukaisailovic@gmail.com>
2025-02-12 13:05:56 +0100

    chore: migrate convertCaipToErc3770 utility to experimental package and remove old implementation

--- apps/laboratory/src/components/AppKitInfo.tsx
+++ apps/laboratory/src/components/AppKitInfo.tsx
@@ -11,9 +11,9 @@ import {
   Text
 } from '@chakra-ui/react'
 
+import { convertCaipToErc3770 } from '@reown/appkit-experimental/erc3770'
 import { useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
 
-import { convertCaipToErc3770 } from '../utils/ERC3770Utils'
 import { EmbeddedWalletInfo } from './EmbeddedWalletInfo'
 
 type AppKitInfoProps = {
