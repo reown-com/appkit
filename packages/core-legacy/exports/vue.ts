4c22ce592b2dcc587e188a416ca44ea68abaa1ab
tomiir <rocchitomas@gmail.com>
2025-02-19 10:48:30 +0100

    chore: added warning mesasge

--- packages/core-legacy/exports/vue.ts
+++ packages/core-legacy/exports/vue.ts
@@ -1,1 +1,6 @@
+if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  console.warn(
+    'Importing from @reown/appkit-core is deprecated. Please import from @reown/appkit-controllers instead.'
+  )
+}
 export * from '@reown/appkit-controllers/vue'
