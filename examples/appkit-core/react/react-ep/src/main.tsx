f0d03280eb8e32a0573288e4e355d17399f6a45e
tomiir <rocchitomas@gmail.com>
2025-02-20 11:22:07 +0100

    chore: organize examples for core

--- examples/appkit-core/react/react-ep/src/main.tsx
+++ examples/appkit-core/react/react-ep/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App.js'
+import App from './App.jsx'
 import './assets/main.css'
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
