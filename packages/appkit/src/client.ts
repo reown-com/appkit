7b3a9094dde6daa29cef9e6ce4980cceddcf969d
enesozturk <enesozturk.d@gmail.com>
2025-02-20 13:33:40 +0300

    chore: e2e test cases

--- packages/appkit/src/client.ts
+++ packages/appkit/src/client.ts
@@ -1783,7 +1783,7 @@ export class AppKit {
   }: Pick<AdapterBlueprint.ConnectResult, 'address' | 'chainId'> & {
     chainNamespace: ChainNamespace
   }) {
-    const caipNetworkId = `${chainNamespace}:${chainId}`
+    const caipNetworkId = `${chainNamespace}:${chainId}` as CaipNetworkId
     const activeCaipNetwork = this.caipNetworks?.find(n => n.caipNetworkId === caipNetworkId)
 
     if (chainNamespace !== ConstantsUtil.CHAIN.EVM || activeCaipNetwork?.testnet) {
