63f37f2d534c9e11dc0efdf281a29d76fc1ab039
enesozturk <enesozturk.d@gmail.com>
2025-02-21 17:17:05 +0300

    fix: multichain basic upa tests

--- apps/laboratory/tests/multichain/multichain-basic.spec.ts
+++ apps/laboratory/tests/multichain/multichain-basic.spec.ts
@@ -45,13 +45,12 @@ test('it should switch networks and sign', async () => {
     }
 
     const chainName = chains[index] ?? DEFAULT_CHAIN_NAME
-    const namespace = chainName === 'Solana' ? 'solana' : 'eip155'
     await modalPage.switchNetwork(chainName)
     await modalValidator.expectSwitchedNetwork(chainName)
     await modalPage.closeModal()
 
     // -- Sign ------------------------------------------------------------------
-    await modalPage.sign(namespace)
+    await modalPage.sign('upa')
     await walletValidator.expectReceivedSign({ chainName })
     await walletPage.handleRequest({ accept: true })
     await modalValidator.expectAcceptedSign()
