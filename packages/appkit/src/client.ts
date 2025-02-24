1f319cd5e5fb014d66725d2f9b975ec2ed08f21e
Enes <enesozturk.d@gmail.com>
2025-02-24 13:12:28 +0300

    fix: identity call where if the network is not supported by wallet and switched to another one (#3881)
    Co-authored-by: tomiir <rocchitomas@gmail.com>

--- packages/appkit/src/client.ts
+++ packages/appkit/src/client.ts
@@ -1788,9 +1788,8 @@ export class AppKit {
   }: Pick<AdapterBlueprint.ConnectResult, 'address' | 'chainId'> & {
     chainNamespace: ChainNamespace
   }) {
-    const activeCaipNetwork = this.caipNetworks?.find(
-      n => n.caipNetworkId === `${chainNamespace}:${chainId}`
-    )
+    const caipNetworkId: CaipNetworkId = `${chainNamespace}:${chainId}`
+    const activeCaipNetwork = this.caipNetworks?.find(n => n.caipNetworkId === caipNetworkId)
 
     if (chainNamespace !== ConstantsUtil.CHAIN.EVM || activeCaipNetwork?.testnet) {
       return
@@ -1798,7 +1797,8 @@ export class AppKit {
 
     try {
       const { name, avatar } = await this.fetchIdentity({
-        address
+        address,
+        caipNetworkId
       })
 
       this.setProfileName(name, chainNamespace)
