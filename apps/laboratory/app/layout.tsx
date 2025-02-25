6537b4b694d739dbab607d23c2f58244ccf8f877
Sven <fr.sven.fr@gmail.com>
2025-02-25 11:59:15 +0100

    correct metadata

--- apps/laboratory/app/layout.tsx
+++ apps/laboratory/app/layout.tsx
@@ -15,8 +15,7 @@ export default async function App({ children }: { children: React.ReactNode }) {
       <meta name="description" content="AppKit Lab" />
       <meta property="og:title" content="AppKit Lab" />
       <meta property="og:description" content="AppKit Lab" />
-      <meta property="og:image" content="https://appkit.reown.com/og-image.png" />
-      <meta property="og:url" content="https://appkit.reown.com" />
+      <meta property="og:url" content="https://appkit-lab.reown.com/" />
       <meta property="og:type" content="website" />
       <link rel="icon" href="/favicon.svg" />
       <body>
