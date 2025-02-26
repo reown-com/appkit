d870b1f88eda61363d5a8bb47e0c01a588a4ffe9
Sven <fr.sven.fr@gmail.com>
2025-02-26 09:44:58 +0100

    use nextjs static metadata

--- apps/laboratory/app/layout.tsx
+++ apps/laboratory/app/layout.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react'
 
+import type { Metadata } from 'next'
+
 import Layout from '@/src/layout'
 import { bootstrapSentry } from '@/src/utils/SentryUtil'
 import { getSession } from '@/src/utils/auth'
 
 bootstrapSentry()
 
+export const metadata: Metadata = {
+  title: 'AppKit Lab',
+  description: "AppKit Lab is the test environment for Reown's AppKit",
+  openGraph: {
+    title: 'AppKit Lab',
+    description: "AppKit Lab is the test environment for Reown's AppKit",
+    url: 'https://appkit-lab.reown.com/',
+    type: 'website'
+  },
+  icons: {
+    icon: '/favicon.svg'
+  }
+}
+
 export default async function App({ children }: { children: React.ReactNode }) {
   const session = await getSession()
 
   return (
     <html>
-      <title>AppKit Lab</title>
-      <meta name="description" content="AppKit Lab" />
-      <meta property="og:title" content="AppKit Lab" />
-      <meta property="og:description" content="AppKit Lab" />
-      <meta property="og:url" content="https://appkit-lab.reown.com/" />
-      <meta property="og:type" content="website" />
-      <link rel="icon" href="/favicon.svg" />
       <body>
         <Layout session={session}>{children}</Layout>
       </body>
