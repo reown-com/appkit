26a4234940e3038e5ce73447cf44203912472d64
Enes <enesozturk.d@gmail.com>
2025-02-27 19:09:15 +0300

    chore: update metadata links (#3933)

--- examples/next-ak-basic-app-router/src/context/index.tsx
+++ examples/next-ak-basic-app-router/src/context/index.tsx
@@ -19,7 +19,7 @@ if (!projectId) {
 export const appKitMetadata = {
   name: 'AppKit Next.js AppKit Basic',
   description: 'AppKit Next.js AppKit Basic Example',
-  url: 'https://github.com/0xonerb/next-reown-appkit-ssr', // origin must match your domain & subdomain
+  url: 'https://appkit-lab.reown.com', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
 
