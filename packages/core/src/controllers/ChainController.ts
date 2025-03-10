a18e5b70c4246afa0fe79d4eaf13fdc682b1446d
enesozturk <enesozturk.d@gmail.com>
2025-03-10 12:48:05 +0300

    chore: remove connector id from local storage when disconnect from he wallet

--- packages/core/src/controllers/ChainController.ts
+++ packages/core/src/controllers/ChainController.ts
@@ -624,6 +624,7 @@ export const ChainController = {
       user: undefined,
       status: 'disconnected'
     })
+    StorageUtil.deleteConnectedConnectorId(chain)
   },
 
   async disconnect() {
@@ -638,7 +639,6 @@ export const ChainController = {
             }
             this.resetAccount(namespace)
             this.resetNetwork(namespace)
-            StorageUtil.deleteConnectedConnectorId(namespace)
           } catch (error) {
             throw new Error(`Failed to disconnect chain ${namespace}: ${(error as Error).message}`)
           }
