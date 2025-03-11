4c8c485fa99c061a322fcf7c120d89ded3f6f0fb
tomiir <rocchitomas@gmail.com>
2025-03-11 08:37:25 +0100

    chore: fix broken import

--- packages/ui/src/composites/wui-profile-button-v2/index.ts
+++ packages/ui/src/composites/wui-profile-button-v2/index.ts
@@ -1,9 +1,8 @@
-import { ChainController, ConnectorController } from '@@reown/appkit-controllers'
-
 import { LitElement, html } from 'lit'
 import { property } from 'lit/decorators.js'
 
 import { type ChainNamespace, ConstantsUtil } from '@reown/appkit-common'
+import { ChainController, ConnectorController } from '@reown/appkit-controllers'
 
 import '../../components/wui-icon/index.js'
 import '../../components/wui-image/index.js'
