736ba2b3ce6b6d476c2b06083ad096bb4663ccb9
MK <mago.khamidov@gmail.com>
2025-03-12 16:27:30 +0000

    chore: override sdkVersion to use cdn

--- packages/common/src/utils/TypeUtil.ts
+++ packages/common/src/utils/TypeUtil.ts
@@ -133,7 +133,7 @@ type BalanceQuantity = {
 
 export type SIWEStatus = 'uninitialized' | 'ready' | 'loading' | 'success' | 'rejected' | 'error'
 
-export type SdkFramework = 'html' | 'react' | 'vue'
+export type SdkFramework = 'html' | 'react' | 'vue' | 'cdn'
 
 export type SdkVersion = `${SdkFramework}-${AdapterType}-${string}`
 
