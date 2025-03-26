4dd3db16d8a78e0592c87edca762a46013b05670
tomiir <rocchitomas@gmail.com>
2025-03-26 16:36:04 +0100

    chore: filter by namespace when fetching instead of via setFilterByNamespace

--- packages/controllers/src/controllers/ApiController.ts
+++ packages/controllers/src/controllers/ApiController.ts
@@ -236,10 +236,12 @@ export const ApiController = {
       .filter(Boolean)
     await Promise.allSettled((images as string[]).map(id => ApiController._fetchWalletImage(id)))
 
+
     state.wallets = CoreHelperUtil.uniqueBy(
       [...state.wallets, ...ApiController._filterOutExtensions(data)],
       'id'
-    )
+    ).filter(w => w.chains?.some(chain => caipNetworkIds.includes(chain)))
+
     state.count = count > state.count ? count : state.count
     state.page = page
   },
@@ -361,10 +363,6 @@ export const ApiController = {
     )
 
     state.recommended = state.allRecommended.filter(wallet =>
-      wallet.chains?.some(chain => caipNetworkIds.includes(chain))
-    )
-
-    state.wallets = state.wallets.filter(wallet =>
       wallet.chains?.some(chain => caipNetworkIds.includes(chain))
     )
   }
