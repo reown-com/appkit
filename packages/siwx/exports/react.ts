2f4659f649195eb8c2600808675102b0ff38a011
Felipe Mendes <zo.fmendes@gmail.com>
2025-03-28 15:10:53 -0300

    chore: add react-cloud-auth example

--- packages/siwx/exports/react.ts
+++ packages/siwx/exports/react.ts
@@ -3,5 +3,8 @@ import { useMemo } from 'react'
 import { OptionsController, type SIWXConfig } from '@reown/appkit-controllers'
 
 export function useAppKitSIWX<Config extends SIWXConfig = SIWXConfig>() {
-  return useMemo(() => OptionsController.state.siwx as Config, [OptionsController.state.siwx])
+  return useMemo(
+    () => OptionsController.state.siwx as Config | undefined,
+    [OptionsController.state.siwx]
+  )
 }
