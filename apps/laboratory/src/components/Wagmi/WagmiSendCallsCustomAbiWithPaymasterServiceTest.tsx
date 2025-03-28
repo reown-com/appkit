1e77a1b9b09b212b22f546ec61b21e91a2babdd9
Derek Rein <alexanderderekrein@gmail.com>
2025-03-28 13:25:13 +0400

    fix: prettier

--- apps/laboratory/src/components/Wagmi/WagmiSendCallsCustomAbiWithPaymasterServiceTest.tsx
+++ apps/laboratory/src/components/Wagmi/WagmiSendCallsCustomAbiWithPaymasterServiceTest.tsx
@@ -292,7 +292,9 @@ function AvailableTestContent() {
             switchChain({ chainId: 8453 })
           }
           setContractAddress('0x833589fcd6edb6e08f4c7c32d4f71b54bda02913')
-          setContractAbi('[{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]')
+          setContractAbi(
+            '[{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'
+          )
           setMethod('transfer')
           setMethodArgs(`["${address}", 10000]`)
         }}
