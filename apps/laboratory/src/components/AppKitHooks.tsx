5c35a56471b06f79e169bd0b9890c9749d37911b
Derek Rein <alexanderderekrein@gmail.com>
2025-03-30 21:39:40 +0400

    fix: lint

--- apps/laboratory/src/components/AppKitHooks.tsx
+++ apps/laboratory/src/components/AppKitHooks.tsx
@@ -43,7 +43,7 @@ export function AppKitHooks() {
   function handleSwitchNetwork() {
     if (isMultichainPage) {
       switchNetwork(getNetworkToSwitch(caipNetwork))
-      
+
       return
     }
 
