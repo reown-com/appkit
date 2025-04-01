54090b93f5f469ff29ee42b12c4193c90c610026
Karandeep Singh <karandeepsinghcaj@outlook.com>
2025-04-01 17:44:51 +0530

    chore: disable gasEstimation by default when preparingTransaction, 'nonce' indicate just to prepare nonce.

--- packages/adapters/wagmi/src/client.ts
+++ packages/adapters/wagmi/src/client.ts
@@ -329,7 +329,8 @@ export class WagmiAdapter extends AdapterBlueprint {
       gasPrice: params.gasPrice as bigint,
       data: params.data as Hex,
       chainId,
-      type: 'legacy' as const
+      type: 'legacy' as const,
+      parameters: ['nonce'] as const
     }
 
     await prepareTransactionRequest(this.wagmiConfig, txParams)
