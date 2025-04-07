d5d6f3c3c609b888170edc33a0b420d73755a8b8
tomiir <rocchitomas@gmail.com>
2025-04-07 17:44:48 +0200

    chore: remove matcher

--- apps/laboratory/middleware.ts
+++ apps/laboratory/middleware.ts
@@ -40,22 +40,3 @@ export function middleware(request: NextRequest) {
 
   return response
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' }
-      ]
-    }
-  ]
-}
