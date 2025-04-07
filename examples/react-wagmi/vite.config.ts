48750dca1b132843977afdef905d23641aec7653
tomiir <rocchitomas@gmail.com>
2025-04-07 17:57:19 +0200

    chore: linter

--- examples/react-wagmi/vite.config.ts
+++ examples/react-wagmi/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3002
   }
-}) 
+})
