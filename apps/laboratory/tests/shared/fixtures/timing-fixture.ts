41ff9c8b757fc5ddd154eb5ee613ff662c019185
Sven <fr.sven.fr@gmail.com>
2025-04-09 13:53:09 +0200

    add comment

--- apps/laboratory/tests/shared/fixtures/timing-fixture.ts
+++ apps/laboratory/tests/shared/fixtures/timing-fixture.ts
@@ -7,6 +7,11 @@ export interface TimingFixture {
 }
 
 export const timingFixture = base.extend<TimingFixture>({
+  /*
+   * Fixes intermittent timingRecords.push is not a function error by properly defining
+   * the timingRecords fixture using a fixture function. Ensures each test gets a fresh
+   * array instance.
+   */
   // eslint-disable-next-line no-empty-pattern
   timingRecords: async ({}, use) => {
     await use([])
