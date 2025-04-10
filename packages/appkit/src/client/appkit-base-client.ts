690ea1a91bd38bc5c05ae127da5588406d9604ff
MK <53529533+magiziz@users.noreply.github.com>
2025-04-10 03:05:14 +0100

    chore: removed preferred account switching (#4180)
    Co-authored-by: Gancho Radkov <ganchoradkov93@gmail.com>
    Co-authored-by: Enes <enesozturk.d@gmail.com>
    Co-authored-by: Gancho Radkov <43912948+ganchoradkov@users.noreply.github.com>
    Co-authored-by: tomiir <rocchitomas@gmail.com>
    Co-authored-by: Sven <fr.sven.fr@gmail.com>

--- packages/appkit/src/client/appkit-base-client.ts
+++ packages/appkit/src/client/appkit-base-client.ts
@@ -103,7 +103,6 @@ export abstract class AppKitBaseClient {
   protected universalProviderInitPromise?: Promise<void>
   protected caipNetworks?: [CaipNetwork, ...CaipNetwork[]]
   protected defaultCaipNetwork?: CaipNetwork
-  protected hasSwitchedToPreferredAccountTypeOnConnect = false
 
   public chainAdapters?: Adapters
   public chainNamespaces: ChainNamespace[] = []
@@ -413,7 +412,6 @@ export abstract class AppKitBaseClient {
         ProviderUtil.resetChain(namespace)
         this.setUser(undefined, namespace)
         this.setStatus('disconnected', namespace)
-        this.hasSwitchedToPreferredAccountTypeOnConnect = false
       },
       checkInstalled: (ids?: string[]) => {
         if (!ids) {
