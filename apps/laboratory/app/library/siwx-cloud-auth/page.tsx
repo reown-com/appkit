96835e342d5503ea4de608c14f8f5445631bfd92
tomiir <rocchitomas@gmail.com>
2025-04-16 13:52:10 +0200

    chore: more pages

--- apps/laboratory/app/library/siwx-cloud-auth/page.tsx
+++ apps/laboratory/app/library/siwx-cloud-auth/page.tsx
@@ -3,23 +3,20 @@
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 import { CloudAuthSIWX } from '@reown/appkit-siwx'
 import { mainnet } from '@reown/appkit/networks'
-import { createAppKit } from '@reown/appkit/react'
 
 import { AppKitButtons } from '@/src/components/AppKitButtons'
 import { AppKitInfo } from '@/src/components/AppKitInfo'
 import { EthersTests } from '@/src/components/Ethers/EthersTests'
-import InitializeBoundary from '@/src/components/InitializeBoundary'
 import { SolanaTests } from '@/src/components/Solana/SolanaTests'
+import { AppKitProvider } from '@/src/context/AppKitContext'
 import { ConstantsUtil } from '@/src/utils/ConstantsUtil'
-import { ThemeStore } from '@/src/utils/StoreUtil'
 
 const networks = ConstantsUtil.EvmNetworks
 
 const etherAdapter = new EthersAdapter()
 
-const modal = createAppKit({
+const config = {
   adapters: [etherAdapter],
-  projectId: ConstantsUtil.ProjectId,
   networks,
   defaultNetwork: mainnet,
   features: {
@@ -28,17 +25,15 @@ const modal = createAppKit({
   termsConditionsUrl: 'https://reown.com/terms-of-service',
   privacyPolicyUrl: 'https://reown.com/privacy-policy',
   siwx: new CloudAuthSIWX()
-})
-
-ThemeStore.setModal(modal)
+}
 
 export default function SIWXCloudAuth() {
   return (
-    <InitializeBoundary>
+    <AppKitProvider config={config}>
       <AppKitButtons />
       <AppKitInfo />
       <EthersTests />
       <SolanaTests />
-    </InitializeBoundary>
+    </AppKitProvider>
   )
 }
