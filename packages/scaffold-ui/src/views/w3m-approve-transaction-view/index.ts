6eb8fe000d97135ffdeeef66c941d89417ef4360
Enes <enesozturk.d@gmail.com>
2025-04-16 23:13:46 +0300

    chore: linter issue

--- packages/scaffold-ui/src/views/w3m-approve-transaction-view/index.ts
+++ packages/scaffold-ui/src/views/w3m-approve-transaction-view/index.ts
@@ -2,12 +2,7 @@ import { LitElement, html } from 'lit'
 import { state } from 'lit/decorators.js'
 
 import { getW3mThemeVariables } from '@reown/appkit-common'
-import {
-  ConnectorController,
-  ModalController,
-  RouterController,
-  ThemeController
-} from '@reown/appkit-controllers'
+import { ConnectorController, ModalController, ThemeController } from '@reown/appkit-controllers'
 import { customElement } from '@reown/appkit-ui'
 
 import styles from './styles.js'
