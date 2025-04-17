8e5ee3ad25755e43b114f6c6e0696add333be3ed
Felipe Mendes <zo.fmendes@gmail.com>
2025-04-17 12:39:24 -0300

    refactor: state handler for react useAppKitSIWX

--- packages/siwx/exports/react.ts
+++ packages/siwx/exports/react.ts
@@ -1,10 +1,17 @@
-import { useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 import { OptionsController, type SIWXConfig } from '@reown/appkit-controllers'
 
 export function useAppKitSIWX<Config extends SIWXConfig = SIWXConfig>() {
-  return useMemo(
-    () => OptionsController.state.siwx as Config | undefined,
-    [OptionsController.state.siwx]
+  const [siwx, setSiwx] = useState(OptionsController.state.siwx as Config | undefined)
+
+  useEffect(
+    () =>
+      OptionsController.subscribeKey('siwx', val => {
+        setSiwx(val as Config | undefined)
+      }),
+    []
   )
+
+  return siwx
 }
