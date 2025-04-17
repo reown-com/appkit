f2f1b72387a7658abd6e8c3061c51e9811e8ce69
Felipe Mendes <zo.fmendes@gmail.com>
2025-04-17 12:41:44 -0300

    feat: cloud auth solana integration (#4220)

--- apps/laboratory/app/library/siwx-cloud-auth/page.tsx
+++ apps/laboratory/app/library/siwx-cloud-auth/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { SolanaAdapter } from '@reown/appkit-adapter-solana'
 import { CloudAuthSIWX } from '@reown/appkit-siwx'
 import { mainnet } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
@@ -13,14 +14,13 @@ import { SolanaTests } from '@/src/components/Solana/SolanaTests'
 import { ConstantsUtil } from '@/src/utils/ConstantsUtil'
 import { ThemeStore } from '@/src/utils/StoreUtil'
 
-const networks = ConstantsUtil.EvmNetworks
-
 const etherAdapter = new EthersAdapter()
+const solanaAdapter = new SolanaAdapter()
 
 const modal = createAppKit({
-  adapters: [etherAdapter],
+  adapters: [etherAdapter, solanaAdapter],
   projectId: ConstantsUtil.ProjectId,
-  networks,
+  networks: [...ConstantsUtil.EvmNetworks, ...ConstantsUtil.SolanaNetworks],
   defaultNetwork: mainnet,
   features: {
     analytics: true
