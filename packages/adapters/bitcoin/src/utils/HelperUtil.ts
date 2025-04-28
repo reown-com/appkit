f67c1e6d8abeef4dc459e6e333de1bc5ff2a6d12
Enes <enesozturk.d@gmail.com>
2025-04-28 09:39:46 +0300

    chore: use testnet4 for bitcoin testnet

--- packages/adapters/bitcoin/src/utils/HelperUtil.ts
+++ packages/adapters/bitcoin/src/utils/HelperUtil.ts
@@ -1,11 +1,13 @@
 import { BitcoinNetworkType } from 'sats-connect'
 
+import { bitcoin, bitcoinTestnet } from '@reown/appkit/networks'
+
 export function mapCaipNetworkToXverseName(caipNetworkId: string): BitcoinNetworkType {
   switch (caipNetworkId) {
-    case 'bip122:000000000019d6689c085ae165831e93':
+    case bitcoin.caipNetworkId:
       return BitcoinNetworkType.Mainnet
-    case 'bip122:000000000933ea01ad0ee984209779ba':
-      return BitcoinNetworkType.Signet
+    case bitcoinTestnet.caipNetworkId:
+      return BitcoinNetworkType.Testnet4
     default:
       throw new Error(`Network ${caipNetworkId} not supported by Xverse wallet`)
   }
@@ -14,9 +16,9 @@ export function mapCaipNetworkToXverseName(caipNetworkId: string): BitcoinNetwor
 export function mapXverseNameToCaipNetwork(network: BitcoinNetworkType): string {
   switch (network) {
     case BitcoinNetworkType.Mainnet:
-      return 'bip122:000000000019d6689c085ae165831e93'
-    case BitcoinNetworkType.Signet:
-      return 'bip122:000000000933ea01ad0ee984209779ba'
+      return bitcoin.caipNetworkId
+    case BitcoinNetworkType.Testnet4:
+      return bitcoinTestnet.caipNetworkId
     default:
       throw new Error(`Network ${network} not supported by Xverse wallet`)
   }
