314fe08c5dc1389670e4b10bb76be6f5418d9a85
Sven <fr.sven.fr@gmail.com>
2025-04-29 16:12:09 +0200

    fix tests

--- packages/scaffold-ui/test/partials/w3m-social-login-widget.test.ts
+++ packages/scaffold-ui/test/partials/w3m-social-login-widget.test.ts
@@ -52,7 +52,7 @@ describe('W3mSocialLoginWidget', () => {
     await googleButton.click()
 
     expect(CoreHelperUtil.returnOpenHref).toHaveBeenCalledWith(
-      '',
+      'https://secure.walletconnect.org/sdk/loading',
       'popupWindow',
       'width=600,height=800,scrollbars=yes'
     )
