4e49d0e75d912b74457080f38bc8ea7428c9d644
Luka Isailovic <lukaisailovic@gmail.com>
2025-05-09 10:56:15 +0200

    fix: test

--- packages/pay/tests/controllers/PayController.test.ts
+++ packages/pay/tests/controllers/PayController.test.ts
@@ -236,7 +236,7 @@ describe('PayController', () => {
       expect(ApiUtil.getPayUrl).toHaveBeenCalledWith({
         exchangeId: 'coinbase',
         asset: 'eip155:1/erc20:0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-        amount: 'a', // hex of 10
+        amount: '10',
         recipient: 'eip155:1:0x1234567890123456789012345678901234567890'
       })
       expect(result).toEqual(mockPayUrlResponse)
