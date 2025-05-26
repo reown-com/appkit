f0f391406895d851f7080afc65ecb5f4ca4f31db
tomiir <rocchitomas@gmail.com>
2025-05-26 17:12:33 +0200

    chore: remove server proxy

--- examples/vue-wagmi/vite.config.ts
+++ examples/vue-wagmi/vite.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    allowedHosts: ['localhost', '127.0.0.1', 'practice-scanned-trail-approx.trycloudflare.com']
-  }
+  plugins: [vue()]
 })
