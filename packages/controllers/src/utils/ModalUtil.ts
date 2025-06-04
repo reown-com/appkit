31c4f129f46b1b4306662676e7a213a320cf9012
Felipe Mendes <zo.fmendes@gmail.com>
2025-06-04 16:02:14 -0300

    fix: disconnect if skip data capture views

--- packages/controllers/src/utils/ModalUtil.ts
+++ packages/controllers/src/utils/ModalUtil.ts
@@ -1,3 +1,4 @@
+import { ConnectionController } from '../controllers/ConnectionController.js'
 import { ModalController } from '../controllers/ModalController.js'
 import { RouterController } from '../controllers/RouterController.js'
 import { SIWXUtil } from './SIWXUtil.js'
@@ -23,6 +24,13 @@ export const ModalUtil = {
       ModalController.shake()
 
       return
+    }
+
+    if (
+      RouterController.state.view === 'DataCapture' ||
+      RouterController.state.view === 'DataCaptureOtpConfirm'
+    ) {
+      ConnectionController.disconnect()
     }
 
     ModalController.close()
