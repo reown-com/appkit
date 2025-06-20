e5198d6f616ca9fd302a7c3fad73d38f7dcbf9b3
Sven <fr.sven.fr@gmail.com>
2025-06-20 12:55:48 +0200

    add featured wallet ids

--- apps/laboratory/app/library/wagmi/page.tsx
+++ apps/laboratory/app/library/wagmi/page.tsx
@@ -17,12 +17,21 @@ const queryClient = new QueryClient()
 const wagmiAdapter = new WagmiAdapter({
   ssr: true,
   networks: ConstantsUtil.EvmNetworks,
+
   projectId: ConstantsUtil.ProjectId
 })
 
 const config = {
   adapters: [wagmiAdapter],
   networks: ConstantsUtil.EvmNetworks,
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+    '18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1',
+    '0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150',
+    '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
+    '9ce87712b99b3eb57396cc8621db8900ac983c712236f48fb70ad28760be3f6a',
+    '0cb0c532b518aa842786d5167e13df22046bc9301b6677808d7134c3d7366a9d'
+  ],
   projectId: ConstantsUtil.ProjectId,
   customWallets: ConstantsUtil.CustomWallets
 }
