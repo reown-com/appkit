4dc9eb4318b42ef4ce0f9394ead144e2f1172f7d
Enes <enesozturk.d@gmail.com>
2025-06-24 19:14:07 +0300

    chore: remove throw on utxos fetch

--- packages/adapters/bitcoin/src/connectors/WalletStandardConnector.ts
+++ packages/adapters/bitcoin/src/connectors/WalletStandardConnector.ts
@@ -64,10 +64,9 @@ export class WalletStandardConnector extends ProviderEventEmitter implements Bit
 
   async connect() {
     const connectFeature = this.getWalletFeature('bitcoin:connect')
-
     const response = await connectFeature.connect({ purposes: ['payment', 'ordinals'] })
-
     const account = response.accounts[0]
+
     if (!account) {
       throw new Error('No account found')
     }
