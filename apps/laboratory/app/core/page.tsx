fed6bc32fd39cbedf190bd89b876125b8dced8aa
Enes <enesozturk.d@gmail.com>
2025-06-30 23:30:46 +0300

    fix: use ref when setting modal to theme store, unit tests

--- apps/laboratory/app/core/page.tsx
+++ apps/laboratory/app/core/page.tsx
@@ -1,32 +1,33 @@
 'use client'
 
 import { type AppKitNetwork, mainnet } from '@reown/appkit/networks'
+import { createAppKit } from '@reown/appkit/react-core'
 
 import { AppKitCoreHooks } from '@/src/components/AppKitCoreHooks'
 import { AppKitInfo } from '@/src/components/AppKitInfo'
 import { MultiChainInfo } from '@/src/components/MultiChainInfo'
 import { UpaTests } from '@/src/components/UPA/UpaTests'
-import { AppKitProvider } from '@/src/context/AppKitContext'
 import { ConstantsUtil } from '@/src/utils/ConstantsUtil'
+import { ThemeStore } from '@/src/utils/StoreUtil'
 
 const networks = ConstantsUtil.AllNetworks
 
-const config = {
+const modal = createAppKit({
   networks: networks as [AppKitNetwork, ...AppKitNetwork[]],
   defaultNetwork: mainnet,
   projectId: ConstantsUtil.ProjectId,
   metadata: ConstantsUtil.Metadata
-}
+})
+
+ThemeStore.setModal(modal)
 
 export default function MultiChainWagmiAdapterOnly() {
   return (
     <>
-      <AppKitProvider config={config}>
-        <AppKitCoreHooks />
-        <AppKitInfo />
-        <MultiChainInfo />
-        <UpaTests />
-      </AppKitProvider>
+      <AppKitCoreHooks />
+      <AppKitInfo />
+      <MultiChainInfo />
+      <UpaTests />
     </>
   )
 }
