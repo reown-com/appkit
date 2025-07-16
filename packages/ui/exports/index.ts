72b2c4aa590f7d89b2cdb855a11d370df6910090
enesozturk <enes@reown.com>
2025-07-16 14:42:02 +0300

    chore: ts issues

--- packages/ui/exports/index.ts
+++ packages/ui/exports/index.ts
@@ -3,5 +3,6 @@ export { initializeTheming, setColorTheme, setThemeVariables } from '../src/util
 export { UiHelperUtil } from '../src/utils/UiHelperUtil.js'
 export { TransactionUtil } from '../src/utils/TransactionUtil.js'
 export { customElement } from '../src/utils/WebComponentsUtil.js'
+export { borderRadius, colors, spacing } from '../src/utils/ThemeConstantsUtil.js'
 
 export type { IconType, VisualType, ColorType, TransactionType } from '../src/utils/TypeUtil.js'
