3917075cefbb30eeccc438ee095a1c4dcd2465c4
Luka Isailovic <lukaisailovic@gmail.com>
2025-07-23 15:20:50 +0200

    chore: next setup

--- apps/pay-test-exchange/app/layout.tsx
+++ apps/pay-test-exchange/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { khTeka } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={cn(khTeka.className)}>
+    <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+          >
+             {children}
+          </ThemeProvider>
+       
       </body>
     </html>
   );
