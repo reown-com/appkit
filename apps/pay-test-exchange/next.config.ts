e25181b12c10e76e95bcdce4830d4cbd9730cf1c
Luka Isailovic <lukaisailovic@gmail.com>
2025-07-23 13:45:32 +0200

    chore: init test exchange project

---
+++ apps/pay-test-exchange/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+initOpenNextCloudflareForDev();
