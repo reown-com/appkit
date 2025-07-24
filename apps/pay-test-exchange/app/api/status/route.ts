bcce373c1cdb40dd162c53a29d1f616b32b09543
Luka Isailovic <lukaisailovic@gmail.com>
2025-07-24 18:23:08 +0300

    chore: remove KV get cache (#4711)

--- apps/pay-test-exchange/app/api/status/route.ts
+++ apps/pay-test-exchange/app/api/status/route.ts
@@ -14,7 +14,10 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: 'No sessionId provided' }, { status: 400 })
   }
 
-  const session = await env.SESSIONID_STORAGE.get(sessionId)
+  const session = await env.SESSIONID_STORAGE.get(sessionId, {
+    // Default cache ttl is 60s, we put 0 to get latest data on request
+    cacheTtl: 0
+  })
 
   if (!session) {
     return NextResponse.json({ error: 'Session not found' }, { status: 404 })
