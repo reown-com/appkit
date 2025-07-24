7c7b93140fc96aa1cc8e22413500ed78717fda81
Luka Isailovic <lukaisailovic@gmail.com>
2025-07-24 15:16:24 +0200

    chore: add key expiry

--- apps/pay-test-exchange/app/api/update/route.ts
+++ apps/pay-test-exchange/app/api/update/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { Session } from '@/lib/types'
 
+const EXPIRY_SECONDS = 60 * 60 * 72 // 72 hours
+
 interface UpdateSessionRequest {
   status: Session['status']
 }
@@ -24,7 +26,10 @@ export async function POST(request: NextRequest) {
       JSON.stringify({
         status: 'pending',
         createdAt: new Date().toISOString()
-      } as Session)
+      } as Session),
+      {
+        expirationTtl: EXPIRY_SECONDS
+      }
     )
 
     return NextResponse.json({ message: 'Session created' }, { status: 200 })
@@ -45,7 +50,9 @@ export async function POST(request: NextRequest) {
     session.txid = '0x9b629147b75dc0b275d478fa34d97c5d4a26926457540b15a5ce871df36c23fd'
   }
 
-  await env.SESSIONID_STORAGE.put(sessionId, JSON.stringify(session))
+  await env.SESSIONID_STORAGE.put(sessionId, JSON.stringify(session), {
+    expirationTtl: EXPIRY_SECONDS
+  })
 
   return NextResponse.json({ message: 'Session updated' }, { status: 200 })
 }
