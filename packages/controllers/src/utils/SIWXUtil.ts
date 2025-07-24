7d41aa6a9e647150c08caa65995339effbc5497d
Felipe Mendes <zo.fmendes@gmail.com>
2025-07-24 18:38:36 -0300

    fix: keep authentication flow when email capture is enabled (#4709)

--- packages/controllers/src/utils/SIWXUtil.ts
+++ packages/controllers/src/utils/SIWXUtil.ts
@@ -41,16 +41,6 @@ export const SIWXUtil = {
     }
 
     try {
-      if (addEmbeddedWalletSessionPromise) {
-        await addEmbeddedWalletSessionPromise
-      }
-
-      const sessions = await siwx.getSessions(`${namespace}:${chainId}`, address)
-
-      if (sessions.length) {
-        return
-      }
-
       if (OptionsController.state.remoteFeatures?.emailCapture) {
         const user = ChainController.getAccountData(namespace)?.user
 
@@ -61,6 +51,16 @@ export const SIWXUtil = {
           }
         })
 
+        return
+      }
+
+      if (addEmbeddedWalletSessionPromise) {
+        await addEmbeddedWalletSessionPromise
+      }
+
+      const sessions = await siwx.getSessions(`${namespace}:${chainId}`, address)
+
+      if (sessions.length) {
         return
       }
 
@@ -258,7 +258,12 @@ export const SIWXUtil = {
   }) {
     const siwx = SIWXUtil.getSIWX()
 
-    if (!siwx || !chainNamespace.includes(CommonConstantsUtil.CHAIN.EVM)) {
+    if (
+      !siwx ||
+      !chainNamespace.includes(CommonConstantsUtil.CHAIN.EVM) ||
+      // Request to input email and sign message when email capture is enabled
+      OptionsController.state.remoteFeatures?.emailCapture
+    ) {
       const result = await authConnector.connect({
         chainId,
         socialUri,
