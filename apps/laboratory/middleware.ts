7ed06e2d30779c13f34219340577dbf5e33ff272
Sven <fr.sven.fr@gmail.com>
2025-07-25 16:18:57 +0200

    remove preview url

--- apps/laboratory/middleware.ts
+++ apps/laboratory/middleware.ts
@@ -26,11 +26,11 @@ export function middleware(request: NextRequest) {
     img-src * 'self' data: blob: https://walletconnect.org https://walletconnect.com https://secure.walletconnect.com https://secure.walletconnect.org https://tokens-data.1inch.io https://tokens.1inch.io https://ipfs.io https://cdn.zerion.io https://appkit-lab.reown.org;
     font-src 'self' https://fonts.gstatic.com https://res.cloudinary.com;
     connect-src  'self' https://react-wallet.walletconnect.com https://rpc.walletconnect.com https://rpc.walletconnect.org https://relay.walletconnect.com https://relay.walletconnect.org wss://relay.walletconnect.com wss://relay.walletconnect.org https://pulse.walletconnect.com https://pulse.walletconnect.org https://api.web3modal.com https://api.web3modal.org wss://www.walletlink.org https://o1095249.ingest.sentry.io https://quote-api.jup.ag https://mempool.space ${publicNodeDomains.join(' ')};
-    frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org https://secure.walletconnect.com https://secure.walletconnect.org https://secure.reown.com https://secure-appkit-git-feat-appkit-reskin-reown-com.vercel.app ${secureSiteDomain};
+    frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org https://secure.walletconnect.com https://secure.walletconnect.org https://secure.reown.com ${secureSiteDomain};
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors ${verifyApiNestedIframesTestOuterDomain} https://app.safe.global https://secure-appkit-git-feat-appkit-reskin-reown-com.vercel.app;
+    frame-ancestors ${verifyApiNestedIframesTestOuterDomain} https://app.safe.global;
     report-uri https://o1095249.ingest.sentry.io/api/4505685639364608/security/?sentry_key=36ff1e79c60877fce6c0273e94a8ed69;
     report-to csp-endpoint
 `
