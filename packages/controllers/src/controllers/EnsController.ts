4f3d88f0bf1116e2fa92c0b50aa29c6fe9857aec
Enes <enesozturk.d@gmail.com>
2025-07-29 16:12:13 +0300

    chore: update cache after setting ens name

--- packages/controllers/src/controllers/EnsController.ts
+++ packages/controllers/src/controllers/EnsController.ts
@@ -154,6 +154,19 @@ const controller = {
       })
 
       AccountController.setProfileName(name, network.chainNamespace)
+      StorageUtil.updateEnsCache({
+        address,
+        ens: [
+          {
+            name,
+            registered_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+            addresses: {},
+            attributes: []
+          }
+        ],
+        timestamp: Date.now()
+      })
       RouterController.replace('RegisterAccountNameSuccess')
     } catch (e) {
       const errorMessage = EnsController.parseEnsApiError(e, `Error registering name ${name}`)
