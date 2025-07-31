673829bfeff9934ab2233d5a14fcf6e45a9fd52b
MK <53529533+magiziz@users.noreply.github.com>
2025-07-31 17:24:18 +0100

    chore: fix missing `address` in WC success event (#4744)

--- packages/controllers/src/controllers/EventsController.ts
+++ packages/controllers/src/controllers/EventsController.ts
@@ -51,7 +51,12 @@ export const EventsController = {
 
   async _sendAnalyticsEvent(payload: EventsControllerState) {
     try {
-      const address = AccountController.state.address
+      let address = AccountController.state.address
+
+      if ('address' in payload.data && payload.data.address) {
+        address = payload.data.address
+      }
+
       if (excluded.includes(payload.data.event) || typeof window === 'undefined') {
         return
       }
