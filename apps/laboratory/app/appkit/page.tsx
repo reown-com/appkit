0e189abeaba8423bcb38ee195d3a500f8b1404b9
Enes <enesozturk.d@gmail.com>
2025-08-28 14:24:17 +0300

    chore: move utils to separate file

--- apps/laboratory/app/appkit/page.tsx
+++ apps/laboratory/app/appkit/page.tsx
@@ -7,62 +7,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Link from 'next/link'
 import { WagmiProvider } from 'wagmi'
 
-import type { ChainAdapter } from '@reown/appkit'
-import { BitcoinAdapter } from '@reown/appkit-adapter-bitcoin'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-import { Ethers5Adapter } from '@reown/appkit-adapter-ethers5'
-import { SolanaAdapter } from '@reown/appkit-adapter-solana'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 
 import DemoContent from '@/src/components/DemoContent'
-import { type Adapter, type WagmiConfig, appKitConfigs } from '@/src/constants/appKitConfigs'
 import { AppKitProvider } from '@/src/context/AppKitContext'
+import { getAppKitAdapters, getAppKitConfigByName } from '@/src/utils/AppKitConfigUtil'
 
 const queryClient = new QueryClient()
-
-const solanaAdapter = new SolanaAdapter()
-const ethersAdapter = new EthersAdapter()
-const ethers5Adapter = new Ethers5Adapter()
-const bitcoinAdapter = new BitcoinAdapter()
 
 type SearchParams = {
   name: string
 }
 
-function resolvePathFromName(name: string) {
-  return appKitConfigs[name] || undefined
-}
-
-function getAdapters(_adapters: Adapter[] | undefined, wagmiConfig: WagmiConfig | undefined) {
-  const adapters: ChainAdapter[] = []
-
-  _adapters?.forEach(adapter => {
-    if (adapter === 'wagmi' && wagmiConfig) {
-      const wagmiAdapter = new WagmiAdapter(wagmiConfig)
-
-      adapters.push(wagmiAdapter)
-    }
-    if (adapter === 'ethers') {
-      adapters.push(ethersAdapter)
-    }
-    if (adapter === 'ethers5') {
-      adapters.push(ethers5Adapter)
-    }
-    if (adapter === 'solana') {
-      adapters.push(solanaAdapter)
-    }
-    if (adapter === 'bitcoin') {
-      adapters.push(bitcoinAdapter)
-    }
-  })
-
-  return adapters
-}
-
-export default function DemoPage({ searchParams }: { searchParams: SearchParams }) {
+export default function Page({ searchParams }: { searchParams: SearchParams }) {
   const name = searchParams['name']
-  const config = resolvePathFromName(name || '')
-  const hasWagmi = config?.adapters?.includes('wagmi')
+  const config = getAppKitConfigByName(name || '')
 
   if (!config || !name) {
     return (
@@ -77,11 +36,12 @@ export default function DemoPage({ searchParams }: { searchParams: SearchParams 
     )
   }
 
-  if (hasWagmi && config.wagmiConfig) {
-    const adapters = getAdapters(config.adapters, config.wagmiConfig)
-    const wagmiAdapter = adapters.find(adapter => adapter.namespace === 'eip155') as WagmiAdapter
+  const adapters = getAppKitAdapters(config.adapters, config.wagmiConfig)
+  const appKitConfig = { ...config, adapters }
+  const hasWagmi = config?.adapters?.includes('wagmi')
 
-    const appKitConfig = { ...config, adapters }
+  if (hasWagmi && config.wagmiConfig) {
+    const wagmiAdapter = adapters.find(a => a.namespace === 'eip155') as WagmiAdapter
 
     return (
       <WagmiProvider config={wagmiAdapter.wagmiConfig}>
@@ -92,11 +52,6 @@ export default function DemoPage({ searchParams }: { searchParams: SearchParams 
         </QueryClientProvider>
       </WagmiProvider>
     )
-  }
-
-  const appKitConfig = {
-    ...config,
-    adapters: getAdapters(config.adapters, config.wagmiConfig)
   }
 
   return (
