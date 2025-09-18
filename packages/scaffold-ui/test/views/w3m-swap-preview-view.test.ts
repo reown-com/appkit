d2644de2a58940ed392b826c72defb1f24551462
Sven <38101365+svenvoskamp@users.noreply.github.com>
2025-09-18 18:14:57 +0200

    fix: switching tokens swap screen (#4937)

--- packages/scaffold-ui/test/views/w3m-swap-preview-view.test.ts
+++ packages/scaffold-ui/test/views/w3m-swap-preview-view.test.ts
@@ -89,6 +89,7 @@ const mockSwapState: SwapControllerState = {
   loadingApprovalTransaction: false,
   loadingBuildTransaction: false,
   fetchError: false,
+  switchingTokens: false,
   approvalTransaction: undefined,
   swapTransaction: {
     data: '0x123',
