0408edda5d51b9997775c80f6cdaf5f1405cb479
tomiir <tomas@walletconnect.com>
2025-10-03 13:21:05 +0200

    fix: rollback test change

--- apps/laboratory/tests/multichain/multichain-wagmi-solana.spec.ts
+++ apps/laboratory/tests/multichain/multichain-wagmi-solana.spec.ts
@@ -110,7 +110,7 @@ test('it should also connect wagmi and sign a message if connecting from a diffe
   await modalValidator.expectConnected()
 
   await modalPage.sign('eip155')
-  await walletValidator.expectReceivedSign({ chainName: 'Polygon' })
+  await walletValidator.expectReceivedSign({ chainName: 'Ethereum' })
   await walletPage.handleRequest({ accept: true })
   await modalValidator.expectAcceptedSign()
 
