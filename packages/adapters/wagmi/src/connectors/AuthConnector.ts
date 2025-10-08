67e87eb32d6b37b12815eb65171c4c134e635edf
tomiir <tomas@walletconnect.com>
2025-10-08 17:14:25 +0200

    fix: issue where requesting from non-evm network on wagmi caused it to reset to mainnet

--- packages/adapters/wagmi/src/connectors/AuthConnector.ts
+++ packages/adapters/wagmi/src/connectors/AuthConnector.ts
@@ -218,6 +218,16 @@ export function authConnector(parameters: AuthParameters) {
       const provider = await this.getProvider()
       const { chainId } = await provider.getChainId()
 
+      const networks = ChainController.getCaipNetworks('eip155')
+      if (!networks.some(network => network.id === chainId)) {
+        const currentChainId = ChainController.getActiveCaipNetwork('eip155')?.id
+        if (currentChainId) {
+          return currentChainId as number
+        }
+
+        return (networks[0]?.id as number) || 1
+      }
+
       return parseChainId(chainId)
     },
 
