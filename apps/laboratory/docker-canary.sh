47ffe0b965e6f58e3eb73817b333f9d25ae2029a
Derek Rein <alexanderderekrein@gmail.com>
2025-10-10 09:59:00 +0400

    chore(canary): fail container when Canary fails

--- apps/laboratory/docker-canary.sh
+++ apps/laboratory/docker-canary.sh
@@ -1,6 +1,7 @@
 # Not adding `set -e` so that S3 upload happens regardless
 
 pnpm playwright:test:canary
+TEST_EXIT_CODE=$?
 
 echo ""
 echo "✅ Playwright Test Results (json)"
@@ -12,3 +13,5 @@ echo ""
 destination="s3://$TEST_RESULTS_BUCKET/web3modal-canary/$(date --iso-8601=seconds)/test-results/"
 echo "Uploading test results to $destination"
 aws s3 cp ./test-results/ $destination --recursive
+
+exit $TEST_EXIT_CODE
