4b6c1f07d71d1f79a2b67c107bb30207cd624d12
Enes <enesozturk.d@gmail.com>
2025-11-10 18:43:53 +0300

    fix: wui-list-token text overflow issue

--- packages/ui/src/composites/wui-list-token/styles.ts
+++ packages/ui/src/composites/wui-list-token/styles.ts
@@ -8,6 +8,7 @@ export default css`
   button {
     padding: ${({ spacing }) => spacing[3]};
     display: flex;
+    gap: ${({ spacing }) => spacing[3]};
     justify-content: space-between;
     width: 100%;
     border-radius: ${({ borderRadius }) => borderRadius[4]};
@@ -38,5 +39,9 @@ export default css`
 
   wui-image {
     border-radius: ${({ borderRadius }) => borderRadius[16]};
+  }
+
+  .token-name-container {
+    flex: 1;
   }
 `
