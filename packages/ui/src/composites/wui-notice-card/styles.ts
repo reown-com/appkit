67e2b8500682a25635af77b4844f90cbef0da3b1
Enes <enesozturk.d@gmail.com>
2025-11-11 13:40:35 +0300

    chore: notice card fixes

--- packages/ui/src/composites/wui-notice-card/styles.ts
+++ packages/ui/src/composites/wui-notice-card/styles.ts
@@ -21,7 +21,7 @@ export default css`
     color: ${({ tokens }) => tokens.theme.textInvert};
     background-color: ${({ tokens }) => tokens.core.backgroundAccentPrimary};
     border-radius: ${({ borderRadius }) => borderRadius[2]};
-    align-items: normal;
+    align-items: center;
   }
 
   @media (hover: hover) {
