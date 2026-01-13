266533a00da4d0d6026cb41e584c1b7b9ca4400e
Enes <enesozturk.d@gmail.com>
2026-01-13 16:51:28 +0300

    chore: revert changes

--- packages/controllers/src/utils/ConnectUtil.ts
+++ packages/controllers/src/utils/ConnectUtil.ts
@@ -103,6 +103,36 @@ export const ConnectUtil = {
   },
 
   /**
+   * Maps the WalletGuide explorer wallet to a WalletItem.
+   * @param w - The WalletGuide explorer wallet.
+   * @returns The WalletItem for the WalletGuide explorer wallet.
+   */
+  mapWalletToWalletItem(w: WcWallet): WalletItem {
+    return {
+      id: w.id,
+      connectors: [],
+      name: w.name,
+      imageUrl: AssetUtil.getWalletImageUrl(w.image_id),
+      isInjected: false,
+      isRecent: false,
+      walletInfo: {
+        description: w.description,
+        supportedChains: w.chains,
+        website: w.homepage,
+        installationLinks: {
+          appStore: w.app_store,
+          playStore: w.play_store,
+          chromeStore: w.chrome_store,
+          desktopLink: w.desktop_link
+        },
+        deepLink: w.mobile_link,
+        linkMode: w.link_mode,
+        isCertified: w.badge_type === 'certified'
+      }
+    }
+  },
+
+  /**
    * Maps the WalletItem to a Wallet Guide Wallet.
    * @param wallet - The WalletItem to map to a Wallet Guide Wallet.
    * @returns The Wallet Guide Wallet for the WalletItem.
