d3cd1009baacfe2c7f4f6b293dc65052234e21c4
Armin Teimouri <armin@walletconnect.com>
2026-09-01 11:44:08 +0300

    chore: changing referenced to com domain
    Signed-off-by: Armin Teimouri <armin@walletconnect.com>

--- packages/scaffold-ui/src/views/w3m-get-wallet-view/index.ts
+++ packages/scaffold-ui/src/views/w3m-get-wallet-view/index.ts
@@ -13,7 +13,7 @@ import { customElement } from '@reown/appkit-ui'
 import '@reown/appkit-ui/wui-flex'
 import '@reown/appkit-ui/wui-list-item'
 
-const EXPLORER = 'https://walletguide.walletconnect.network'
+const EXPLORER = 'https://walletguide.walletconnect.com'
 
 @customElement('w3m-get-wallet-view')
 export class W3mGetWalletView extends LitElement {
@@ -29,7 +29,7 @@ export class W3mGetWalletView extends LitElement {
           icon="externalLink"
           size="sm"
           @click=${() => {
-            CoreHelperUtil.openHref('https://walletguide.walletconnect.network/', '_blank')
+            CoreHelperUtil.openHref('https://walletguide.walletconnect.com/', '_blank')
           }}
         ></w3m-list-wallet>
       </wui-flex>
